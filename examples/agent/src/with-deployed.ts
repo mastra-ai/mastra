09826a33020d7d0edeecb658b65383f05903a903
Abhi Aiyer <abhiaiyer91@gmail.com>
2024-12-06 09:09:48 -0800

    test cases

--- examples/agent/src/with-deployed.ts
+++ examples/agent/src/with-deployed.ts
@@ -1,10 +1,19 @@
 async function main() {
-    // Query 1: Basic pantry ingredients
+
+    const URI = `http://localhost:4111`
+
+    const CF = `https://mastra.abhiaiyer91.workers.dev`
+
+    const VERCEL = `https://mastra-70djhzend-kepler-ab1bf5ab.vercel.app`
+
+    const API_URL = VERCEL
+
+    // // Query 1: Basic pantry ingredients
     // const query1 =
     //     'In my kitchen I have: pasta, canned tomatoes, garlic, olive oil, and some dried herbs (basil and oregano). What can I make?';
     // console.log(`Query: ${query1}`);
 
-    // const d = await fetch(`https://mastra.abhiaiyer91.workers.dev/agent/${encodeURIComponent('Chef Agent')}/text`, {
+    // const d = await fetch(`${API_URL}/agent/${encodeURIComponent('Chef Agent')}/text`, {
     //     method: `POST`,
     //     headers: {
     //         'Content-Type': 'application/json',
@@ -21,13 +30,14 @@ async function main() {
     // console.log('\n👨‍🍳 Chef Michel:', pastaResponse.text);
     // console.log('\n-------------------\n');
 
+
     // Query 2: More ingredients
     const query2 =
         "Now I'm over at my friend's house, and they have: chicken thighs, coconut milk, sweet potatoes, and some curry powder.";
     console.log(`Query: ${query2}`);
 
 
-    const s = await fetch(`https://mastra.abhiaiyer91.workers.dev/agent/${encodeURIComponent('Chef Agent')}/stream`, {
+    const s = await fetch(`${API_URL}/agent/${encodeURIComponent('Chef Agent')}/stream`, {
         method: `POST`,
         headers: {
             'Content-Type': 'application/json',
