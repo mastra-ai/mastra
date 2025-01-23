67f2d537227d381ed6ac2eb844cde537e2bf031b
Ward Peeters <ward@coding-tech.com>
2025-01-23 15:50:17 -0800

    fix measure

--- packages/core/src/eval/metric.ts
+++ packages/core/src/eval/metric.ts
@@ -1,13 +1,8 @@
-export interface MeasureParams {
-  input: string;
-  output: string;
-}
-
 export interface MetricResult {
   score: number;
   info?: Record<string, any>;
 }
 
 export abstract class Metric {
-  abstract measure(args: MeasureParams): Promise<MetricResult>;
+  abstract measure(input: string, output: string): Promise<MetricResult>;
 }
