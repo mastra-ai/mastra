2103897008cf704e81e925dc7a01b67b25c40b78
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-01-22 16:51:21 -0800

    Remove old templates

--- packages/cli/src/templates/server.ts
+++
@@ -1,9 +0,0 @@
-import { createNodeServer } from '@mastra/deployer';
-import _path, { join } from 'path';
-import { fileURLToPath as _fileURLToPath } from 'url';
-import { pathToFileURL } from 'url';
-
-const mastraPath = pathToFileURL(join(process.cwd(), 'mastra.mjs')).href;
-const { mastra } = await import(mastraPath);
-
-await createNodeServer(mastra);
