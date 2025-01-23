8d9ee7b159c94fa26567f22f67b1b57c52ee4fcc
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-01-22 16:58:21 -0800

    Dane fixes

--- examples/agent/src/mastra/index.ts
+++ examples/agent/src/mastra/index.ts
@@ -1,26 +1,7 @@
 import { Mastra } from '@mastra/core';
-// import { VercelDeployer } from '@mastra/deployer-vercel';
-// import { CloudflareDeployer } from '@mastra/deployer-cloudflare';
-import { NetlifyDeployer } from '@mastra/deployer-netlify';
 
 import { chefAgent } from './agents/index';
 
-// const cf = new CloudflareDeployer({
-//   scope: '66d175c352385eecf41cf5ac9afdcc61',
-//   projectName: `mastra-netlify-test`,
-// })
-
-// const vercel = new VercelDeployer({
-//   scope: 'abhiaiyer91-gmailcom-s-team',
-//   projectName: `mastra-netlify-test`,
-// })
-
-const netlify = new NetlifyDeployer({
-  scope: 'abhiaiyer91',
-  projectName: `mastra-netlify-test`,
-})
-
 export const mastra = new Mastra({
   agents: { chefAgent },
-  deployer: netlify,
 });
