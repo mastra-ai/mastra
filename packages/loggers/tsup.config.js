a6c6b377765c1731b9a45a796a2b27f9f9709090
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2025-01-28 14:31:27 +0100

    update

--- packages/loggers/tsup.config.js
+++ packages/loggers/tsup.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/file/index.ts', 'src/upstash/index.ts'],
   treeshake: true,
   format: ['esm', 'cjs'],
   dts: true,
