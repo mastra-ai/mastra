c512c62fbca9ab6566cd16fb153bda0b4918b11c
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2025-01-28 15:09:49 +0100

    inline tsup config

--- deployers/cloudflare/tsup.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/index.ts', 'src/secrets-manager/index.ts'],
-  treeshake: true,
-  format: 'esm',
-  dts: true,
-  clean: true,
-});
--- deployers/vercel/tsup.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/index.ts'],
-  treeshake: true,
-  format: 'esm',
-  dts: true,
-  clean: true,
-});
