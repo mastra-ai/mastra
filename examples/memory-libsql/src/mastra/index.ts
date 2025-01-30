c76c573db28f07e66a87fcfa64bfcd23a61c29ae
tyler barnes <tylerbarnes@Mac.lan>
2025-01-30 10:40:04 -0800

    examples temp

---
+++ examples/memory-libsql/src/mastra/index.ts
@@ -0,0 +1,26 @@
+import { Mastra, MastraStorageLibSql } from '@mastra/core';
+import { Memory } from '@mastra/memory';
+
+import 'dotenv/config';
+
+import { chefAgent, memoryAgent } from './agents';
+
+const connectionString = process.env.POSTGRES_CONNECTION_STRING;
+
+if (!connectionString) {
+  throw new Error(`process.env.POSTGRES_CONNECTION_STRING is required for this example to work`);
+}
+
+const memory = new Memory({
+  storage: new PostgresStore({
+    connectionString,
+  }),
+  threads: {
+    injectRecentMessages: 10,
+  },
+});
+
+export const mastra = new Mastra({
+  agents: { chefAgent, memoryAgent },
+  memory,
+});
--- examples/memory-upstashkv/src/mastra/index.ts
+++ examples/memory-upstashkv/src/mastra/index.ts
@@ -1,12 +1,13 @@
 import { Mastra } from '@mastra/core';
-import { UpstashKVMemory } from '@mastra/memory';
+import { Memory } from '@mastra/memory';
+import { UpstashStore } from '@mastra/store-upstash';
 
 import { chefAgent } from './agents';
 
-const kvMemory = new UpstashKVMemory({
-  url: process.env.KV_REST_API_URL!,
-  token: process.env.KV_REST_API_TOKEN!,
-});
+// const kvMemory = new UpstashKVMemory({
+//   url: process.env.KV_REST_API_URL!,
+//   token: process.env.KV_REST_API_TOKEN!,
+// });
 
 export const mastra = new Mastra({
   memory: kvMemory,
