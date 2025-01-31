3c1b790bb15755434540802ebfa63fc702e4871c
Ward Peeters <ward@coding-tech.com>
2025-01-31 16:43:28 +0100

    setup deployer package

--- packages/deployer/src/build/bundle.ts
+++ packages/deployer/src/build/bundle.ts
@@ -1,253 +1,66 @@
-import { noopLogger, Logger, createLogger } from '@mastra/core/logger';
-import { Workflow, Step } from '@mastra/core/workflows';
-import * as esbuild from 'esbuild';
-import { type BuildOptions } from 'esbuild';
+import alias from '@rollup/plugin-alias';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { join } from 'path';
-import { z } from 'zod';
+import { rollup, watch, type InputOptions, type Plugin, type InputOption } from 'rollup';
+import esbuild from 'rollup-plugin-esbuild';
+import { fileURLToPath } from 'url';
 
-import { FileService } from './fs.js';
-import { upsertMastraDir } from './utils.js';
+import { FileService } from './fs';
+import { telemetryFix } from './plugins/telemetry-fix';
 
-const logger = createLogger({
-  name: 'Mastra CLI',
-  level: 'debug',
-});
+type NormalizedInputOptions = Omit<Partial<InputOptions>, 'plugins' | 'input'> & {
+  plugins?: Plugin[];
+  input: InputOption;
+};
 
-const buildWorkflow = new Workflow({
-  name: 'Build',
-  triggerSchema: z.object({
-    buildName: z.string(),
-    entry: z.string(),
-    outfile: z.string().optional(),
-    useBanner: z.boolean().optional(),
-    devMode: z.boolean().optional(),
-  }),
-});
+function getOptions(inputOptions: NormalizedInputOptions): InputOptions {
+  const fileService = new FileService();
+  const entry = fileService.getFirstExistingFile([
+    join(process.cwd(), 'src/mastra/index.ts'),
+    join(process.cwd(), 'src/mastra/index.js'),
+  ]);
 
-const ensureDir = new Step({
-  id: 'Ensure Directory',
-  execute: async () => {
-    // Ensure .mastra directory exists
-    upsertMastraDir({});
-  },
-});
-
-const bundleStep = new Step({
-  id: 'Bundle',
-  execute: async ({ context }) => {
-    const devMode = context.machineContext?.triggerData.devMode;
-    const buildName = context.machineContext?.triggerData.buildName;
-    const useBanner = context.machineContext?.triggerData.useBanner;
-
-    const entry = context.machineContext?.triggerData.entry;
-
-    const outfile = context.machineContext?.triggerData.outfile;
-
-    const fileService = new FileService();
-    const entryPoint = fileService.getFirstExistingFile([entry]);
-    const outfilePath = outfile || join(process.cwd(), '.mastra', 'mastra.mjs');
-
-    const plugins: any[] = [];
-
-    let missingMastraDependency = false;
-
-    const externalIfMissingPlugin = {
-      name: 'external-if-missing',
-      setup(build: any) {
-        build.onResolve(
-          { filter: /^.*$/ },
-          // @ts-ignore
-          (args: any) => {
-            if (!args.importer.endsWith('.mastra/index.mjs')) return;
-            try {
-              const resolvedPath = import.meta.resolve(args.path);
-              if (!resolvedPath || resolvedPath.includes('_npx/')) {
-                missingMastraDependency = true;
-                return { path: args.path, external: true };
-              }
-            } catch (e) {
-              missingMastraDependency = true;
-              return { path: args.path, external: true };
-            }
+  return {
+    logLevel: 'silent',
+    ...inputOptions,
+    treeshake: true,
+    plugins: [
+      ...(inputOptions.plugins ?? []),
+      telemetryFix(),
+      alias({
+        entries: [
+          {
+            find: /^\#server$/,
+            replacement: fileURLToPath(import.meta.resolve('@mastra/deployer/server')).replaceAll('\\', '/'),
           },
-        );
-      },
-    };
+          { find: /^\#mastra$/, replacement: entry.replaceAll('\\', '/') },
+        ],
+      }),
+      commonjs(),
+      nodeResolve({}),
+      json(),
+      esbuild({
+        target: 'node20',
+        platform: 'node',
+        minify: false,
+        define: {
+          'process.env.NODE_ENV': JSON.stringify('production'),
+        },
+      }),
+    ].filter(Boolean),
+  };
+}
 
-    if (devMode) {
-      plugins.push(externalIfMissingPlugin);
-    }
+export async function getBundler(inputOptions: NormalizedInputOptions) {
+  const bundle = await rollup(getOptions(inputOptions));
 
-    const esbuildConfig: BuildOptions = {
-      entryPoints: [entryPoint],
-      bundle: true,
-      platform: 'node',
-      format: 'esm',
-      outfile: outfilePath,
-      target: 'node20',
-      sourcemap: true,
-      logLevel: 'silent',
-      minify: true,
-      metafile: true,
-      mainFields: ['module', 'main'],
-      conditions: ['import', 'node'],
-      logOverride: {
-        'commonjs-variable-in-esm': 'silent',
-      },
-      external: [
-        'execa',
-        // Mark node built-ins as external
-        'assert',
-        'buffer',
-        'child_process',
-        'cluster',
-        'constants',
-        'crypto',
-        'dgram',
-        'dns',
-        'events',
-        'fs',
-        'http',
-        'http2',
-        'https',
-        'module',
-        'net',
-        'os',
-        'path',
-        'punycode',
-        'querystring',
-        'readline',
-        'repl',
-        'stream',
-        'string_decoder',
-        'sys',
-        'timers',
-        'tls',
-        'tty',
-        'url',
-        'util',
-        'v8',
-        'vm',
-        'wasi',
-        'worker_threads',
-        'zlib',
-        'chromium-bidi/lib/cjs/bidiMapper/BidiMapper',
-        'chromium-bidi/lib/cjs/cdp/CdpConnection',
+  return bundle;
+}
 
-        // Your packages
-        '@mastra/core',
-        '@mastra/memory',
-        '@mastra/engine',
-        '@mastra/rag',
-        '@mastra/evals',
-        '@mastra/mcp',
-        '@mastra/firecrawl',
-        '@mastra/github',
-        '@mastra/stabilityai',
+export async function getWatcher(inputOptions: NormalizedInputOptions) {
+  const watcher = watch(getOptions(inputOptions));
 
-        // Your deployer packages
-        '@mastra/deployer',
-        '@mastra/deployer-cloudflare',
-        '@mastra/deployer-netlify',
-        '@mastra/deployer-vercel',
-
-        // Your speech packages
-        '@mastra/speech-elevenlabs',
-        '@mastra/speech-openai',
-        '@mastra/speech-playai',
-        '@mastra/speech-azure',
-        '@mastra/speech-deepgram',
-        '@mastra/speech-google',
-        '@mastra/speech-ibm',
-        '@mastra/speech-murf',
-        '@mastra/speech-speechify',
-        '@mastra/speech-replicate',
-
-        // Your vector store packages
-        '@mastra/vector-astra',
-        '@mastra/vector-chroma',
-        '@mastra/vector-libsql',
-        '@mastra/vector-pg',
-        '@mastra/vector-pinecone',
-        '@mastra/vector-qdrant',
-        '@mastra/vector-upstash',
-        '@mastra/vector-vectorize',
-      ],
-      plugins,
-    };
-
-    if (useBanner) {
-      esbuildConfig.banner = {
-        js: `
-                import { createRequire } from "node:module";                
-                const require = createRequire(import.meta.url || 'file:///');
-              `,
-      };
-    }
-
-    const result = await esbuild.build(esbuildConfig);
-
-    if (devMode && missingMastraDependency) {
-      logger.error(
-        `Missing Mastra dependency. Please install the mastra package in your project or globally using npm i -g mastra`,
-      );
-      process.exit(1);
-    }
-
-    // Log build results
-    logger.info(`[${buildName}]: completed successfully`);
-
-    return result;
-  },
-});
-
-const analyzeStep = new Step({
-  id: 'Analyze',
-  execute: async ({ context }) => {
-    if (context?.machineContext?.stepResults?.Bundle?.status !== 'success') {
-      throw new Error('Bundle step failed');
-    }
-    const res = await esbuild.analyzeMetafile(context.machineContext?.stepResults.Bundle.payload.metafile);
-    return res;
-  },
-});
-
-buildWorkflow.step(ensureDir).then(bundleStep).then(analyzeStep).commit();
-
-export async function bundle(
-  entry: string,
-  {
-    useBanner = true,
-    buildName = 'Build',
-    outfile,
-    devMode,
-  }: { devMode?: boolean; outfile?: string; entryFile?: string; buildName?: string; useBanner?: boolean } = {},
-) {
-  const { start } = buildWorkflow.createRun();
-
-  buildWorkflow.__setLogger(noopLogger as unknown as Logger);
-
-  try {
-    const result = await start({
-      triggerData: {
-        buildName,
-        entry,
-        useBanner,
-        outfile,
-        devMode,
-      },
-    });
-
-    Object.entries(result.results).forEach(([stepName, stepResult]) => {
-      if ((stepResult as any).status !== 'success') {
-        logger.error(`${stepName}:`, stepResult);
-        throw new Error(`Step ${stepName} failed`);
-      }
-    });
-  } catch (error) {
-    if (error instanceof Error) {
-      logger.error(error.message, { stack: error.stack, message: error.message, name: error.name });
-    }
-    process.exit(1);
-  }
+  return watcher;
 }
