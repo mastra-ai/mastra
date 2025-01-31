1beb888a48c067fe39f6e949ee9d40d966a9a1b1
tyler barnes <tylerbarnes@Mac.lan>
2025-01-30 19:29:34 -0800

    fix upstash example

--- examples/memory-upstashkv/src/mastra/index.ts
+++ examples/memory-upstashkv/src/mastra/index.ts
@@ -2,14 +2,31 @@ import { Mastra } from '@mastra/core';
 import { Memory } from '@mastra/memory';
 import { UpstashStore } from '@mastra/store-upstash';
 
+import 'dotenv/config';
+
 import { chefAgent } from './agents';
 
-// const kvMemory = new UpstashKVMemory({
-//   url: process.env.KV_REST_API_URL!,
-//   token: process.env.KV_REST_API_TOKEN!,
-// });
+const memory = new Memory({
+  storage: new UpstashStore({
+    url: 'http://localhost:8089',
+    token: 'test_token',
+  }),
+  threads: {
+    injectRecentMessages: 10,
+    injectVectorHistorySearch: {
+      includeResults: 3,
+      includePrevious: 2,
+      includeNext: 2,
+    },
+  },
+  embeddingOptions: {
+    provider: 'OPEN_AI',
+    model: 'text-embedding-ada-002',
+    maxRetries: 3,
+  },
+});
 
 export const mastra = new Mastra({
-  memory: kvMemory,
   agents: { chefAgent },
+  memory,
 });
