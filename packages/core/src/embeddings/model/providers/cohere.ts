1abd47a60e31f199411a88a3db90e6a5177cea36
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-02-04 19:35:04 -0800

    Embedding Primitive

--- packages/core/src/embeddings/model/providers/cohere.ts
+++ packages/core/src/embeddings/model/providers/cohere.ts
@@ -1,5 +1,7 @@
 import { createCohere } from '@ai-sdk/cohere';
 
+import { MastraEmbedding } from '../base';
+
 export type CohereEmbeddingModelNames =
   | 'embed-english-v3.0'
   | 'embed-multilingual-v3.0'
@@ -10,24 +12,26 @@ export type CohereEmbeddingModelNames =
   | 'embed-multilingual-v2.0'
   | (string & {});
 
-export function createCohereEmbeddingModel({
-  apiKey,
-  model,
-  baseURL,
-  fetch,
-  headers,
-}: {
-  apiKey?: string;
-  model: CohereEmbeddingModelNames;
-  baseURL?: string;
-  fetch?: typeof globalThis.fetch;
-  headers?: Record<string, string>;
-}) {
-  const cohere = createCohere({
-    apiKey: apiKey || process.env.COHERE_API_KEY,
+export class CohereEmbeddingModel extends MastraEmbedding {
+  constructor({
+    apiKey = process.env.COHERE_API_KEY || '',
+    model,
     baseURL,
     fetch,
     headers,
-  });
-  return cohere.embedding(model);
+  }: {
+    apiKey?: string;
+    model: CohereEmbeddingModelNames;
+    baseURL?: string;
+    fetch?: typeof globalThis.fetch;
+    headers?: Record<string, string>;
+  }) {
+    const cohere = createCohere({
+      apiKey,
+      baseURL,
+      fetch,
+      headers,
+    });
+    super({ model: cohere.embedding(model) });
+  }
 }
