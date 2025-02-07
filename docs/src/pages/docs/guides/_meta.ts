7ac23e83d7bc791ba3f2cada4391cdda6fc726ae
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-02-07 13:57:26 -0800

    Fixes

--- docs/src/pages/docs/guides/_meta.ts
+++ docs/src/pages/docs/guides/_meta.ts
@@ -1,7 +1,7 @@
 const meta = {
-  "02-chef-michel": "Agents: Chef Michel",
-  "03-stock-agent": "Tools: Stock Agent",
-  "04-recruiter": "Workflows: AI Recruiter",
+  "01-chef-michel": "Agents: Chef Michel",
+  "02-stock-agent": "Tools: Stock Agent",
+  "03-recruiter": "Workflows: AI Recruiter",
 };
 
 export default meta;
