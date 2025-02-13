0f0cfd17778d6e8315c85c295477093b7a73b2a0
Tony Kovanen <tony.kovanen@gmail.com>
2025-02-13 12:37:20 +0200

    revert quickstart

--- examples/quick-start/src/mastra/index.ts
+++ examples/quick-start/src/mastra/index.ts
@@ -3,12 +3,7 @@ import { MastraStorageLibSql, createLogger, Mastra } from '@mastra/core';
 import { catOne, agentTwo } from './agents/agent';
 import { logCatWorkflow } from './workflow';
 
-const logger = createLogger({
-  level: 'debug',
-});
-
 export const mastra = new Mastra({
   agents: { catOne, agentTwo },
   workflows: { logCatWorkflow },
-  logger,
 });
