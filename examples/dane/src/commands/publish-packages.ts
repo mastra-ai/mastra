6892aa001655a317fffaa192cc402ea173b25410
Ward Peeters <ward@coding-tech.com>
2025-02-17 13:41:13 +0100

    revert dane for now

--- examples/dane/src/commands/publish-packages.ts
+++ examples/dane/src/commands/publish-packages.ts
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 
 import { mastra } from '../mastra/index.js';
 
-export async function publishPackages({ prerelease }: { prerelease: boolean }) {
+export async function publishPackages() {
   console.log(chalk.green("Hi! I'm Dane!"));
   console.log(chalk.green('Let me publish your packages..\n'));
 
