3c962232d9a60ce5a3d2866f178753b9cfa1dcc7
Ward Peeters <ward@coding-tech.com>
2025-02-18 16:03:11 +0100

    lint memory

--- packages/memory/eslint.config.js
+++ packages/memory/eslint.config.js
@@ -3,4 +3,10 @@ import { createConfig } from '@internal/lint/eslint';
 const config = await createConfig();
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [...config];
+export default [
+  ...config,
+  {
+    files: ['integration-tests/**/*'],
+    ...(await import('typescript-eslint')).configs.disableTypeChecked,
+  },
+];
