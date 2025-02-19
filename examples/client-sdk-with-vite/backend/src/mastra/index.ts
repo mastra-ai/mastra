88a4da7b20f6e2e59f3afd359c6ebeeb91bd0d18
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2025-02-19 16:36:55 +0100

    fix pnpm yaml

--- examples/client-sdk-with-vite/backend/src/mastra/index.ts
+++ examples/client-sdk-with-vite/backend/src/mastra/index.ts
@@ -1,10 +1,8 @@
 import { createLogger } from '@mastra/core/logger';
 import { Mastra } from '@mastra/core/mastra';
-import { DefaultStorage } from '@mastra/core/storage';
 import { Memory } from '@mastra/memory';
 
 import { weatherAgent } from './agents';
-import workflow from './workflow';
 import { weatherWorkflow } from './workflows';
 
 const memory = new Memory();
