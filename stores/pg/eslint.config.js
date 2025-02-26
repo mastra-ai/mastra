28b0b2fa47afe83471f4b361851ecb8e79158e6d
Your Name <you@example.com>
2025-02-26 13:27:45 -0800

    ignore vitest config in eslint

--- stores/pg/eslint.config.js
+++ stores/pg/eslint.config.js
@@ -3,4 +3,4 @@ import { createConfig } from '@internal/lint/eslint';
 const config = await createConfig();
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [...config];
+export default [...config.map(conf => ({ ...conf, ignores: [...(conf.ignores || []), '**/vitest.perf.config.ts'] }))];
