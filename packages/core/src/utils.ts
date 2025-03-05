9a6428d439680c2afc27687f568eb4cd7740db84
NikAiyer <nick.aiyer@gmail.com>
2025-03-05 16:40:45 -0600

    added tests

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -267,14 +267,14 @@ export async function* maskStreamTags(
  * Resolve serialized zod output - This function takes the string output ot the `jsonSchemaToZod` function
  * and instantiates the zod object correctly.
  *
- * @param obj - serialized zod object
+ * @param schema - serialized zod object
  * @returns resolved zod object
  */
-export function resolveSerializedZodOutput(obj: any) {
+export function resolveSerializedZodOutput(schema: string): z.ZodType {
   // Creates and immediately executes a new function that takes 'z' as a parameter
   // The function body is a string that returns the serialized zod schema
   // When executed with the 'z' parameter, it reconstructs the zod schema in the current context
-  return Function('z', `"use strict";return (${obj});`)(z);
+  return Function('z', `"use strict";return (${schema});`)(z);
 }
 
 /**
