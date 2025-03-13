70ad9e106fdbfe38be36cb421bb752f6cbd4ebb5
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2025-03-13 10:16:53 +0100

    add divider position

--- packages/playground-ui/src/domains/resizable-panel.tsx
+++ packages/playground-ui/src/domains/resizable-panel.tsx
@@ -10,6 +10,7 @@ export const MastraResizablePanel = ({
   className,
   disabled = false,
   setCurrentWidth,
+  dividerPosition = 'left',
 }: {
   children: ReactNode;
   defaultWidth: number;
@@ -18,6 +19,7 @@ export const MastraResizablePanel = ({
   className?: string;
   disabled?: boolean;
   setCurrentWidth?: (width: number) => void;
+  dividerPosition?: 'left' | 'right';
 }) => {
   const { sidebarWidth, isDragging, handleMouseDown, containerRef } = useResizeColumn({
     defaultWidth: disabled ? 100 : defaultWidth,
@@ -27,14 +29,21 @@ export const MastraResizablePanel = ({
   });
   return (
     <div className={cn('w-full h-full relative', className)} ref={containerRef} style={{ width: `${sidebarWidth}%` }}>
-      {disabled ? null : (
+      {!disabled && dividerPosition === 'left' ? (
         <div
           className={`w-1 bg-mastra-bg-1 bg-[#121212] h-full cursor-col-resize hover:w-1.5 hover:bg-mastra-border-2 hover:bg-[#424242] active:bg-mastra-border-3 active:bg-[#3e3e3e] transition-colors absolute inset-y-0 -left-1 -right-1 z-10
           ${isDragging ? 'bg-mastra-border-2 bg-[#424242] w-1.5 cursor- col-resize' : ''}`}
           onMouseDown={handleMouseDown}
         />
-      )}
+      ) : null}
       {children}
+      {!disabled && dividerPosition === 'right' ? (
+        <div
+          className={`w-1 bg-mastra-bg-1 bg-[#121212] h-full cursor-col-resize hover:w-1.5 hover:bg-mastra-border-2 hover:bg-[#424242] active:bg-mastra-border-3 active:bg-[#3e3e3e] transition-colors absolute inset-y-0 -left-1 -right-1 z-10
+          ${isDragging ? 'bg-mastra-border-2 bg-[#424242] w-1.5 cursor- col-resize' : ''}`}
+          onMouseDown={handleMouseDown}
+        />
+      ) : null}
     </div>
   );
 };
