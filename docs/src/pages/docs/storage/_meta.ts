0aa4095b954bdc99df2784c0aacc53ac1b718829
NikAiyer <nick.aiyer@gmail.com>
2025-03-17 16:18:15 -0500

    update docs

--- docs/src/pages/docs/storage/_meta.ts
+++ docs/src/pages/docs/storage/_meta.ts
@@ -1,8 +1,5 @@
 const meta = {
   overview: "Overview",
-  "memory-storage": "Storage as Memory",
-  "vector-storage": "Vector Storage",
-  "storage-telemetry": "Using Storage with Telemetry",
 };
 
 export default meta;
