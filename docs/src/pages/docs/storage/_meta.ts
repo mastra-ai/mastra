03d2dca306b5d2ef4fa35733254ad8c80195bcf6
NikAiyer <nick.aiyer@gmail.com>
2025-03-17 13:20:07 -0500

    initial docs commit

---
+++ docs/src/pages/docs/storage/_meta.ts
@@ -0,0 +1,8 @@
+const meta = {
+  overview: "Overview",
+  "memory-storage": "Storage as Memory",
+  "vector-storage": "Vector Storage",
+  "storage-telemetry": "Using Storage with Telemetry",
+};
+
+export default meta;
