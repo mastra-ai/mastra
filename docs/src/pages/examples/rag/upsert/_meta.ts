1ae8478cdc04d0e3b7e3b74aacb204083e67efeb
NikAiyer <nick.aiyer@gmail.com>
2025-03-18 11:50:04 -0500

    combined upsert docs

--- docs/src/pages/examples/rag/upsert/_meta.ts
+++ docs/src/pages/examples/rag/upsert/_meta.ts
@@ -1,12 +1,5 @@
 const meta = {
-  "insert-embedding-in-pinecone": "Insert Embedding in Pinecone",
-  "insert-embedding-in-pgvector": "Insert Embedding in PgVector",
-  "insert-embedding-in-upstash": "Insert Embedding in Upstash",
-  "insert-embedding-in-vectorize": "Insert Embedding in Vectorize",
-  "insert-embedding-in-libsql": "Insert Embedding in LibSQL",
-  "insert-embedding-in-chroma": "Insert Embedding in Chroma",
-  "insert-embedding-in-qdrant": "Insert Embedding in Qdrant",
-  "insert-embedding-in-astra": "Insert Embedding in Astra",
+  "upsert-embeddings": "Upsert Embeddings",
 };
 
 export default meta;
