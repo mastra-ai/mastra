9e2dca382319b427dcc1d6b96cc01144643675d7
Kenny <adelekekehinde06@gmail.com>
2025-03-18 12:29:33 +0100

    fix: build error

--- docs/src/components/footer.tsx
+++ docs/src/components/footer.tsx
@@ -1,5 +1,4 @@
 import { Check, SendHorizontal } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { SubscribeForm } from "./subscribe-form";
