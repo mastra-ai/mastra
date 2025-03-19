9c7c0f91181f2cc92bf39e589ee94d61c43e22c1
NikAiyer <nick.aiyer@gmail.com>
2025-03-18 19:15:21 -0500

    moved agent rag examples to usage folder

--- docs/src/pages/examples/rag/_meta.ts
+++ docs/src/pages/examples/rag/_meta.ts
@@ -14,12 +14,9 @@ const meta = {
   rerank: {
     title: "Rerank",
   },
-  "basic-rag": "Basic RAG",
-  "cleanup-rag": "Optimizing Information Density",
-  "filter-rag": "Metadata Filtering",
-  "cot-rag": "Chain of Thought Prompting",
-  "cot-workflow-rag": "Chain of Thought Prompting w/Workflow",
-  "graph-rag": "Graph RAG",
+  usage: {
+    title: "Usage",
+  },
 };
 
 export default meta;
---
+++ docs/src/pages/examples/rag/usage/_meta.ts
@@ -0,0 +1,10 @@
+const meta = {
+  "basic-rag": "Basic RAG",
+  "cleanup-rag": "Optimizing Information Density",
+  "filter-rag": "Metadata Filtering",
+  "cot-rag": "Chain of Thought Prompting",
+  "cot-workflow-rag": "Chain of Thought Prompting w/Workflow",
+  "graph-rag": "Graph RAG",
+};
+
+export default meta;
