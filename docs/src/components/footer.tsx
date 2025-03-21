de1f922aa243e965ee2d4413fcc2188bc27915ff
mfrachet <marvin@mastra.ai>
2025-03-21 17:04:01 +0100

    wip

--- docs/src/components/footer.tsx
+++ docs/src/components/footer.tsx
@@ -56,7 +56,7 @@ export const Footer = () => {
   return (
     <footer
       data-state={!showFooter}
-      className="flex z-30 max-w-[1184px] mx-auto px-1 bg-[#050505] border-t-[#222222] relative w-full border-t-[0.5px] flex-col items-center pt-8 lg:pt-[5rem] pb-24 md:pb-32 footer data-[state=false]:mt-8 "
+      className="flex z-30 max-w-[1184px] mx-auto px-1 bg-[#fafafa] dark:bg-[#050505]  border-t-[#dcdcdc] dark:border-t-[#222222] relative w-full border-t-[0.5px] flex-col items-center pt-8 lg:pt-[5rem] pb-24 md:pb-32 footer data-[state=false]:mt-8 "
     >
       <div className="flex flex-col lg:flex-row gap-16 lg:gap-0 w-full justify-between px-6 md:px-0 md:pl-3 md:pr-4">
         <div className="flex gap-2 dark:text-white">{logo}</div>
