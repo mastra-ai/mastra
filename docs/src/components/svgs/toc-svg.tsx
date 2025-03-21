f17f50d078444caf61deefa6f9e6d2903a6c136a
mfrachet <marvin@mastra.ai>
2025-03-21 09:43:07 +0100

    fix(docs): align theme with the marketing page

---
+++ docs/src/components/svgs/toc-svg.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from "react";
+
+export const TocSvg = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M3.00781 3.59326H12.9865M3.00781 8.05158H9.98202M3.00781 12.4062H6.66131"
+      strokeWidth="1.7"
+      strokeLinecap="round"
+    />
+  </svg>
+);
