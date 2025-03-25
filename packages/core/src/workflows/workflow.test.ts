cb90780b87e0c8406c3f1de5fb71ec832f732f57
dayo <ifedayoadeniyi@gmail.com>
2025-03-25 17:03:59 +0100

    cleanup

--- packages/core/src/workflows/workflow.test.ts
+++ packages/core/src/workflows/workflow.test.ts
@@ -10,7 +10,8 @@ import { Telemetry } from '../telemetry';
 import { createTool } from '../tools';
 
 import { Step } from './step';
-import { WhenConditionReturnValue, type WorkflowContext, type WorkflowResumeResult } from './types';
+import { WhenConditionReturnValue } from './types';
+import type { WorkflowContext, WorkflowResumeResult } from './types';
 import { Workflow } from './workflow';
 
 const storage = new DefaultStorage({
