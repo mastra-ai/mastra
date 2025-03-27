320f5e511a56bed6328679b5a51d3dcbb380fc40
Kenny <adelekekehinde06@gmail.com>
2025-03-27 17:10:58 +0100

    fix: banner conditional rendering

--- docs/src/components/cookie-banner.tsx
+++ docs/src/components/cookie-banner.tsx
@@ -9,43 +9,38 @@ export function CookieBanner({
   onConsentChange: (consent: boolean) => void;
 }) {
   const [showBanner, setShowBanner] = useState(false);
-  const flag = useFeatureFlagEnabled("cookie-banner");
+  const isEU = useFeatureFlagEnabled("cookie-banner");
 
   useEffect(() => {
-    if (!flag) {
-      setShowBanner(false);
-      window.gtag?.("consent", "update", {
-        analytics_storage: "granted",
-        ad_storage: "granted",
-        ad_user_data: "granted",
-        ad_personalization: "granted",
-      });
-      localStorage.setItem("cookie-consent", "true");
-      onConsentChange(true);
-      return;
-    }
-    const storedConsent = localStorage.getItem("cookie-consent");
-    if (!storedConsent) {
-      setShowBanner(true);
-      window.gtag?.("consent", "default", {
-        ad_storage: "denied",
-        ad_user_data: "denied",
-        ad_personalization: "denied",
-        analytics_storage: "denied",
-      });
-    } else {
-      const consent = storedConsent === "true";
-      onConsentChange(consent);
-      if (consent) {
+    if (isEU === undefined) return;
+
+    if (isEU === false) {
+      const storedConsent = localStorage.getItem("cookie-consent");
+      if (!storedConsent) {
         window.gtag?.("consent", "update", {
           analytics_storage: "granted",
           ad_storage: "granted",
           ad_user_data: "granted",
           ad_personalization: "granted",
         });
+        localStorage.setItem("cookie-consent", "true");
+        onConsentChange(true);
       }
+      return;
     }
-  }, [flag]);
+
+    const storedConsent = localStorage.getItem("cookie-consent");
+    if (!storedConsent) {
+      setShowBanner(true);
+      window.gtag?.("consent", "update", {
+        analytics_storage: "denied",
+        ad_storage: "denied",
+        ad_user_data: "denied",
+        ad_personalization: "denied",
+      });
+    }
+    onConsentChange(storedConsent === "true");
+  }, [isEU]);
 
   const handleAccept = () => {
     localStorage.setItem("cookie-consent", "true");
@@ -74,9 +69,9 @@ export function CookieBanner({
   if (!showBanner) return null;
 
   return (
-    <div className="border-border-1 fixed bottom-8 right-5 z-50 flex w-[322px] items-center justify-center rounded-xl border bg-black p-4">
+    <div className="fixed bottom-8 right-5 z-50 flex w-[322px] items-center justify-center rounded-xl dark:border-neutral-700 border bg-white dark:bg-black p-4">
       <div>
-        <p className="mb-4 font-sans text-sm">
+        <p className="mb-4 font-sans dark:text-white text-sm">
           We use tracking cookies to understand how you use the product and help
           us improve it. Please accept cookies to help us improve.
         </p>
@@ -86,6 +81,7 @@ export function CookieBanner({
         <span> </span>
         <Button
           variant={"secondary"}
+          className="dark:text-white"
           size={"slim"}
           type="button"
           onClick={handleReject}
