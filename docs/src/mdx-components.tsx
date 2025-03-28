f33c6a7d796c1507e0348d1772feb809e148a460
Kenny <adelekekehinde06@gmail.com>
2025-03-28 14:25:15 +0100

    feat: add custom components

--- docs/src/mdx-components.tsx
+++ docs/src/mdx-components.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useMDXComponents as getThemeComponents } from "nextra-theme-docs"; // nextra-theme-blog or your custom theme
+import { OperatorsTable } from "./components/operators-table";
+import { PropertiesTable } from "./components/properties-table";
 
 // Get the default MDX components
 const themeComponents = getThemeComponents();
@@ -8,5 +11,7 @@ export function useMDXComponents(components?: any) {
   return {
     ...themeComponents,
     ...components,
+    OperatorsTable,
+    PropertiesTable,
   };
 }
