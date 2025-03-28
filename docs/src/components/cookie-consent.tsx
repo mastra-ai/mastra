98af19570dd9ef8064291e3cd8d8140c5e352784
Kenny <adelekekehinde06@gmail.com>
2025-03-28 15:13:54 +0100

    feat: add hubspot tracker and modify form display

--- docs/src/components/cookie-consent.tsx
+++ docs/src/components/cookie-consent.tsx
@@ -4,6 +4,7 @@
 import Script from "next/script";
 import { useState } from "react";
 import { CookieBanner } from "./cookie-banner";
+import HubspotTracker from "./hubspot-tracker";
 
 declare global {
   interface Window {
@@ -58,6 +59,8 @@ export const CookieConsent = () => {
           </Script>
         </>
       )}
+
+      <HubspotTracker cookieConsent={cookieConsent ?? false} />
     </>
   );
 };
