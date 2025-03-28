96d034055fa948cb2e7d9d369b1eb6446c944081
Kenny <adelekekehinde06@gmail.com>
2025-03-28 13:23:41 +0100

    feat: remove unused mdx options

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,39 +1,28 @@
 /** @type {import('next').NextConfig} */
-import nextra from 'nextra';
+import nextra from "nextra";
 
 const withNextra = nextra({
   search: {
-    codeblocks: true
-  },
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-  mdxOptions: {
-    rehypePrettyCodeOptions: {
-      theme: {
-        dark: 'github-dark',
-        light: 'github-light',
-      },
-    },
+    codeblocks: true,
   },
 });
 
 export default withNextra({
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/docs' : '',
-
+  assetPrefix: process.env.NODE_ENV === "production" ? "/docs" : "",
   async rewrites() {
     return {
       beforeFiles: [
         {
-          source: '/docs/_next/:path+',
-          destination: '/_next/:path+',
+          source: "/docs/_next/:path+",
+          destination: "/_next/:path+",
         },
       ],
     };
   },
   redirects: () => [
     {
-      source: '/docs/08-running-evals',
-      destination: '/docs/evals/00-overview',
+      source: "/docs/08-running-evals",
+      destination: "/docs/evals/00-overview",
       permanent: true,
     },
   ],
