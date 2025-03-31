116271d2484e99dd0bc107c7c3fa8efbad4284f7
NikAiyer <nick.aiyer@gmail.com>
2025-03-31 10:02:25 -0500

    added specific redirects rather than pattern matchinfg

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -37,13 +37,74 @@ export default withNextra({
       permanent: true,
     },
     {
-      source: "/docs/guides/04-recruiter",
+      source: "/docs/agents/00-overview",
+      destination: "/docs/agents/overview",
+      permanent: true,
+    },
+    {
+      source: "/docs/agents/01-agent-memory",
+      destination: "/docs/agents/agent-memory",
+      permanent: true,
+    },
+    {
+      source: "/docs/agents/02-adding-tools",
+      destination: "/docs/agents/adding-tools",
+      permanent: true,
+    },
+    {
+      source: "/docs/agents/02a-mcp-guide",
+      destination: "/docs/agents/mcp-guide",
+      permanent: true,
+    },
+    {
+      source: "/docs/agents/03-adding-voice",
+      destination: "/docs/agents/adding-voice",
+      permanent: true,
+    },
+    {
+      source: "/docs/evals/00-overview",
+      destination: "/docs/evals/overview",
+      permanent: true,
+    },
+    {
+      source: "/docs/evals/01-textual-evals",
+      destination: "/docs/evals/textual-evals",
+      permanent: true,
+    },
+    {
+      source: "/docs/evals/02-custom-eval",
+      destination: "/docs/evals/custom-eval",
+      permanent: true,
+    },
+    {
+      source: "/docs/evals/03-running-in-ci",
+      destination: "/docs/evals/running-in-ci",
+      permanent: true,
+    },
+
+    {
+      source: "/docs/guides/01-chef-michel",
+      destination: "/docs/guides/chef-michel",
+      permanent: true,
+    },
+    {
+      source: "/docs/guides/02-stock-agent",
+      destination: "/docs/guides/stock-agent",
+      permanent: true,
+    },
+    {
+      source: "/docs/guides/03-recruiter",
       destination: "/docs/guides/ai-recruiter",
       permanent: true,
     },
     {
-      source: "/docs/:path*/:prefix(\\d{2}[a-z]?)-:slug",
-      destination: "/docs/:path*/:slug",
+      source: "/docs/guides/04-research-assistant",
+      destination: "/docs/guides/research-assistant",
+      permanent: true,
+    },
+    {
+      source: "/docs/workflows/00-overview",
+      destination: "/docs/workflows/overview",
       permanent: true,
     },
   ],
