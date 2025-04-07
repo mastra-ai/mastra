a0eed74105fd503e4256436a03507b2d12f69800
Kenny <adelekekehinde06@gmail.com>
2025-04-07 12:43:13 +0100

    feat: use logo with suffix in navbar

--- docs/src/components/navbar.tsx
+++ docs/src/components/navbar.tsx
@@ -1,11 +1,11 @@
 import { Navbar } from "nextra-theme-docs";
-import { logo } from "@/components/logo";
+import { LogoWithSuffix } from "@/components/logo";
 import { GithubStarCount } from "@/components/github-star-count";
 
 export const Nav = () => {
   return (
     <Navbar
-      logo={logo}
+      logo={<LogoWithSuffix />}
       logoLink={process.env.NEXT_PUBLIC_APP_URL}
       projectIcon={<GithubStarCount />}
       projectLink="https://github.com/mastra-ai/mastra"
