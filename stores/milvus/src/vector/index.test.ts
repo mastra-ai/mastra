c4cb72e5f4f9ec4843dd744761e3f05cdaf73dd7
vishesh <visheshbaghel99@gmail.com>
2025-04-14 00:52:05 +0530

    added basic create and drop collection apis

--- stores/milvus/src/vector/index.test.ts
+++ stores/milvus/src/vector/index.test.ts
@@ -1,8 +1,55 @@
-import { describe, it } from 'vitest';
-import { connect } from './index';
+import { DataType } from '@zilliz/milvus2-sdk-node';
+import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import { MilvusVectorStore } from './index';
 
 describe('Milvus Vector tests', () => {
-  it('should connect to local milvus db instance', async () => {
-    await connect();
+  let milvusClient: MilvusVectorStore;
+
+  beforeAll(async () => {
+    // for running the tests, you need to have a local milvus instance running
+    // start the milvus db server by running `docker-compose up -d` in the root directory
+    milvusClient = new MilvusVectorStore({
+      address: '127.0.0.1:19530',
+      username: 'milvus-username',
+      password: 'milvus-password',
+      ssl: false,
+    });
+
+    expect(milvusClient).toBeDefined();
+  });
+
+  describe('Schema operations', () => {
+    const collection_name = `book`;
+    afterAll(async () => {
+      await milvusClient.dropCollection(collection_name);
+    });
+
+    it('should create collection', async () => {
+      // define schema
+      const dim = 128;
+      const schema = [
+        {
+          name: `book_id`,
+          description: `customized primary id`,
+          data_type: DataType.Int64,
+          is_primary_key: true,
+          autoID: false,
+        },
+        {
+          name: `word_count`,
+          description: `word count`,
+          data_type: DataType.Int64,
+        },
+        {
+          name: `book_intro`,
+          description: `word count`,
+          data_type: DataType.FloatVector,
+          dim: dim,
+        },
+      ];
+
+      const results = await milvusClient.createCollection(collection_name, schema);
+      expect(results).toBeDefined();
+    });
   });
 });
--- stores/milvus/src/vector/index.ts
+++ stores/milvus/src/vector/index.ts
@@ -1,14 +1,79 @@
+import type {
+  CreateIndexArgs,
+  CreateIndexParams,
+  IndexStats,
+  ParamsToArgs,
+  QueryResult,
+  QueryVectorArgs,
+  QueryVectorParams,
+  UpsertVectorArgs,
+  UpsertVectorParams,
+} from '@mastra/core';
+import { MastraVector } from '@mastra/core/vector';
 import { MilvusClient } from '@zilliz/milvus2-sdk-node';
+import type { CheckHealthResponse, GetVersionResponse, ResStatus } from '@zilliz/milvus2-sdk-node';
 
-async function connect() {
-  const address = '127.0.0.1:19530';
-  const username = 'your-milvus-username'; // optional username
-  const password = 'your-milvus-password'; // optional password
-  const ssl = false; // secure or not
+export class MilvusVectorStore extends MastraVector {
+  private client: MilvusClient;
+  constructor({
+    address,
+    username,
+    password,
+    ssl,
+  }: {
+    address: string;
+    username?: string;
+    password?: string;
+    ssl?: boolean;
+  }) {
+    super();
+    this.client = new MilvusClient({ address, ssl, username, password });
+  }
 
-  // connect to milvus
-  const client = new MilvusClient({ address, ssl, username, password });
-  console.log(await client.getVersion());
+  async checkHealth(): Promise<CheckHealthResponse> {
+    return this.client.checkHealth();
+  }
+
+  async checkVersion(): Promise<GetVersionResponse> {
+    return this.client.getVersion();
+  }
+
+  async createCollection(collectionName: string, schema: any): Promise<ResStatus> {
+    return this.client.createCollection({
+      collection_name: collectionName,
+      description: `my first collection`,
+      fields: schema,
+    });
+  }
+
+  async dropCollection(collectionName: string): Promise<ResStatus> {
+    return this.client.dropCollection({
+      collection_name: collectionName,
+    });
+  }
+
+  query<E extends QueryVectorArgs = QueryVectorArgs>(
+    ...args: ParamsToArgs<QueryVectorParams> | E
+  ): Promise<QueryResult[]> {
+    throw new Error('Method not implemented.' + args);
+  }
+  upsert<E extends UpsertVectorArgs = UpsertVectorArgs>(
+    ...args: ParamsToArgs<UpsertVectorParams> | E
+  ): Promise<string[]> {
+    throw new Error('Method not implemented.' + args);
+  }
+  createIndex<E extends CreateIndexArgs = CreateIndexArgs>(
+    ...args: ParamsToArgs<CreateIndexParams> | E
+  ): Promise<void> {
+    throw new Error('Method not implemented.' + args);
+  }
+  listIndexes(): Promise<string[]> {
+    throw new Error('Method not implemented.');
+  }
+  describeIndex(indexName: string): Promise<IndexStats> {
+    throw new Error('Method not implemented.' + indexName);
+  }
+  deleteIndex(indexName: string): Promise<void> {
+    throw new Error('Method not implemented.' + indexName);
+  }
 }
-
-export { connect };
