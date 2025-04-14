3032754e98e330de5c1e0861e4171c34b3e63e18
vishesh <visheshbaghel99@gmail.com>
2025-04-14 23:34:00 +0530

    added support for creating and describing collections with custom schemas

--- stores/milvus/src/vector/index.test.ts
+++ stores/milvus/src/vector/index.test.ts
@@ -1,3 +1,4 @@
+import type { FieldType } from '@zilliz/milvus2-sdk-node';
 import { DataType } from '@zilliz/milvus2-sdk-node';
 import { describe, it, expect, beforeAll, afterAll } from 'vitest';
 import { MilvusVectorStore } from './index';
@@ -25,9 +26,8 @@ describe('Milvus Vector tests', () => {
     });
 
     it('should create collection', async () => {
-      // define schema
       const dim = 128;
-      const schema = [
+      const schema: FieldType[] = [
         {
           name: `book_id`,
           description: `customized primary id`,
@@ -50,6 +50,113 @@ describe('Milvus Vector tests', () => {
 
       const results = await milvusClient.createCollection(collection_name, schema);
       expect(results).toBeDefined();
+      expect(results.error_code).toBe('Success');
+    });
+
+    it('should throw error when creating collection with dynamic field', async () => {
+      const dim = 128;
+      const schema: FieldType[] = [
+        {
+          name: `book_id`,
+          description: `customized primary id`,
+          data_type: DataType.Int64,
+          is_primary_key: true,
+          autoID: false,
+        },
+        {
+          name: `word_count`,
+          description: `word count`,
+          data_type: DataType.Int64,
+        },
+        {
+          name: `book_intro`,
+          description: `word count`,
+          data_type: DataType.FloatVector,
+          dim: dim,
+        },
+      ];
+
+      await expect(
+        milvusClient.createCollection(collection_name, schema, { enable_dynamic_field: true }),
+      ).rejects.toThrowError();
+    });
+
+    it('should describe collection', async () => {
+      const results = await milvusClient.describeCollection(collection_name);
+      expect(results).toBeDefined();
+      expect(results.status.error_code).toBe('Success');
+      expect(results.schema.name).toBe(collection_name);
+      expect(results.schema.fields.length).toBe(3);
+      expect(results.schema.fields[0].name).toBe('book_id');
+      expect(results.schema.fields[1].name).toBe('word_count');
+      expect(results.schema.fields[2].name).toBe('book_intro');
+      expect(results.schema.enable_dynamic_field).toBe(false);
+      expect(results.schema.autoID).toBe(false);
+      expect(results.shards_num).toBe(2);
+      expect(results.num_partitions).toBe('0');
+    });
+
+    it('should drop collection', async () => {
+      const results = await milvusClient.dropCollection(collection_name);
+      expect(results).toBeDefined();
+      expect(results.error_code).toBe('Success');
+    });
+
+    it('should return error response when dropping non-existent collection', async () => {
+      const response = await milvusClient.dropCollection(collection_name);
+      expect(response).toBeDefined();
+      expect(response.error_code).toBe('UnexpectedError');
+      expect(response.reason).toBe("DescribeCollection failed: can't find collection: book");
+    });
+
+    it('should create collection with all options', async () => {
+      const dim = 128;
+      const schema: FieldType[] = [
+        {
+          name: `book_id`,
+          description: `customized primary id`,
+          data_type: DataType.Int64,
+          is_primary_key: true,
+          autoID: false,
+        },
+        {
+          name: `word_count`,
+          description: `word count`,
+          data_type: DataType.Int64,
+        },
+        {
+          name: `book_intro`,
+          description: `word count`,
+          data_type: DataType.FloatVector,
+          dim: dim,
+        },
+      ];
+
+      const results = await milvusClient.createCollection(collection_name, schema, {
+        enable_dynamic_field: false,
+        num_partitions: 2,
+        consistency_level: 'Eventually',
+        description: 'test collection',
+        timeout: 10000,
+      });
+
+      expect(results).toBeDefined();
+      expect(results.error_code).toBe('Success');
+
+      const describeResults = await milvusClient.describeCollection(collection_name);
+      expect(describeResults).toBeDefined();
+      expect(describeResults.status.error_code).toBe('Success');
+      expect(describeResults.schema.name).toBe(collection_name);
+      expect(describeResults.schema.fields.length).toBe(3);
+      expect(describeResults.schema.fields[0].name).toBe('book_id');
+      expect(describeResults.schema.fields[1].name).toBe('word_count');
+      expect(describeResults.schema.fields[2].name).toBe('book_intro');
+      expect(describeResults.schema.enable_dynamic_field).toBe(false);
+      expect(describeResults.schema.autoID).toBe(false);
+      expect(describeResults.schema.description).toBe('test collection');
+      expect(describeResults.shards_num).toBe(2);
+      expect(describeResults.num_partitions).toBe('0');
+      expect(describeResults.consistency_level).toBe('Eventually');
     });
   });
 });
--- stores/milvus/src/vector/index.ts
+++ stores/milvus/src/vector/index.ts
@@ -11,7 +11,21 @@ import type {
 } from '@mastra/core';
 import { MastraVector } from '@mastra/core/vector';
 import { MilvusClient } from '@zilliz/milvus2-sdk-node';
-import type { CheckHealthResponse, GetVersionResponse, ResStatus } from '@zilliz/milvus2-sdk-node';
+import type {
+  CheckHealthResponse,
+  DescribeCollectionResponse,
+  FieldType,
+  GetVersionResponse,
+  ResStatus,
+} from '@zilliz/milvus2-sdk-node';
+
+export type CollectionOptions = {
+  description?: string;
+  timeout?: number;
+  consistency_level?: 'Strong' | 'Session' | 'Bounded' | 'Eventually' | 'Customized';
+  num_partitions?: number;
+  enable_dynamic_field?: boolean;
+};
 
 export class MilvusVectorStore extends MastraVector {
   private client: MilvusClient;
@@ -38,18 +52,40 @@ export class MilvusVectorStore extends MastraVector {
     return this.client.getVersion();
   }
 
-  async createCollection(collectionName: string, schema: any): Promise<ResStatus> {
-    return this.client.createCollection({
-      collection_name: collectionName,
-      description: `my first collection`,
-      fields: schema,
-    });
+  async createCollection(name: string, schema: FieldType[], options?: CollectionOptions): Promise<ResStatus> {
+    try {
+      return this.client.createCollection({
+        collection_name: name,
+        description: options?.description ?? '',
+        fields: schema,
+        timeout: options?.timeout,
+        consistency_level: options?.consistency_level,
+        num_partitions: options?.num_partitions,
+        enable_dynamic_field: options?.enable_dynamic_field,
+      });
+    } catch (error) {
+      throw new Error('Failed to create collection: ' + error);
+    }
   }
 
   async dropCollection(collectionName: string): Promise<ResStatus> {
-    return this.client.dropCollection({
-      collection_name: collectionName,
-    });
+    try {
+      return this.client.dropCollection({
+        collection_name: collectionName,
+      });
+    } catch (error) {
+      throw new Error('Failed to drop collection: ' + error);
+    }
+  }
+
+  async describeCollection(collectionName: string): Promise<DescribeCollectionResponse> {
+    try {
+      return this.client.describeCollection({
+        collection_name: collectionName,
+      });
+    } catch (error) {
+      throw new Error('Failed to describe collection: ' + error);
+    }
   }
 
   query<E extends QueryVectorArgs = QueryVectorArgs>(
