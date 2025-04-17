5a0ab1214f28c9fbcfb35c9126d49c2ad7ee67f0
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2025-04-17 14:54:26 +0100

    hardcode buildbundler entry

--- packages/cli/src/commands/build/BuildBundler.ts
+++ packages/cli/src/commands/build/BuildBundler.ts
@@ -49,14 +49,65 @@ export class BuildBundler extends Bundler {
   }
 
   protected getEntry({ swaggerUI, openapi, apiReqLogs }: ServerBundleOptions): string {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
-    const template = readFileSync(join(__dirname, 'templates', 'build.entry.js'), 'utf8');
     const options = JSON.stringify({
       swaggerUI: swaggerUI ?? false,
       openapi: openapi ?? false,
       apiReqLogs: apiReqLogs ?? false,
     });
-    return template.replace('__SERVER_OPTIONS__', options);
+    return `
+    // @ts-ignore
+    import { evaluate } from '@mastra/core/eval';
+    import { AvailableHooks, registerHook } from '@mastra/core/hooks';
+    import { TABLE_EVALS } from '@mastra/core/storage';
+    import { checkEvalStorageFields } from '@mastra/core/utils';
+    import { mastra } from '#mastra';
+    import { createNodeServer } from '#server';
+
+    // @ts-ignore
+    await createNodeServer(mastra, ${options});
+
+    registerHook(AvailableHooks.ON_GENERATION, ({ input, output, metric, runId, agentName, instructions }) => {
+    evaluate({
+      agentName,
+      input,
+      metric,
+      output,
+        runId,
+        globalRunId: runId,
+        instructions,
+      });
+    });
+
+    if (mastra.getStorage()) {
+      // start storage init in the background
+      mastra.getStorage().init();
+    }
+
+    registerHook(AvailableHooks.ON_EVALUATION, async traceObject => {
+      const storage = mastra.getStorage();
+      if (storage) {
+        // Check for required fields
+        const logger = mastra?.getLogger();
+        const areFieldsValid = checkEvalStorageFields(traceObject, logger);
+        if (!areFieldsValid) return;
+
+        await storage.insert({
+          tableName: TABLE_EVALS,
+          record: {
+            input: traceObject.input,
+            output: traceObject.output,
+            result: JSON.stringify(traceObject.result || {}),
+            agent_name: traceObject.agentName,
+            metric_name: traceObject.metricName,
+            instructions: traceObject.instructions,
+            test_info: null,
+            global_run_id: traceObject.globalRunId,
+            run_id: traceObject.runId,
+            created_at: new Date().toISOString(),
+          },
+          });
+        }
+      });
+`;
   }
 }
