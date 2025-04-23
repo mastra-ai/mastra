19a6bf501715b3ecc4cb70f666da775743cd98be
Ryan Hansen <rphansen91@gmail.com>
2025-04-23 14:06:00 -0700

    speech to speech example

--- docs/src/content/en/examples/voice/_meta.ts
+++ docs/src/content/en/examples/voice/_meta.ts
@@ -2,6 +2,7 @@ const meta = {
   "text-to-speech": "Text to Speech",
   "speech-to-text": "Speech to Text",
   "turn-taking": "Turn Taking",
+  "speech-to-speech": "Speech to Speech",
 };
 
 export default meta;
