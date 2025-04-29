ce3ea7143d88abf9e9af11d90a36c479bb4654d0
Viraj Agarwal <virajagarwal15@gmail.com>
2025-04-29 11:58:15 +0530

    moved start and stop dockers scripts to pre and post test script

--- stores/couchbase/scripts/start-docker.js
+++ stores/couchbase/scripts/start-docker.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 import { execSync } from 'child_process';
+try {
+  execSync('docker compose -f "./docker-compose.yaml" ps --quiet');
+  console.log('Container already running, bringing it down first...');
+  execSync('docker compose -f "./docker-compose.yaml" down --volumes', { stdio: 'inherit' });
+} catch (error) {
+  // pass
+}
 execSync('docker compose -f "./docker-compose.yaml" up --wait', { stdio: 'inherit' });
