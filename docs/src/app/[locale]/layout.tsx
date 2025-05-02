937f937f063ac6eff96d1f765555b54ec325c1f6
dayo <ifedayoadeniyi@gmail.com>
2025-05-02 09:56:13 +0100

    make reo script ID const

--- docs/src/app/[locale]/layout.tsx
+++ docs/src/app/[locale]/layout.tsx
@@ -13,6 +13,9 @@ import { CookieConsent } from "@/components/cookie-consent";
 import { NextraLayout } from "@/components/nextra-layout";
 import { GTProvider } from "gt-next";
 import Script from "next/script";
+
+const REO_SCRIPT_ID = "reo-script";
+
 const fetchStars = async () => {
   try {
     const res = await fetch("https://api.github.com/repos/mastra-ai/mastra", {
@@ -76,7 +79,7 @@ export default async function RootLayout({
           <Toaster />
           <CookieConsent />
         </GTProvider>
-        <Script id="reo-script" strategy="afterInteractive">
+        <Script id={REO_SCRIPT_ID} strategy="afterInteractive">
           {`!function(){var e,t,n;e="fdd9258c52d6769",t=function(){Reo.init({clientID:"fdd9258c52d6769"})},
           (n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.defer=!0,
           n.onload=t,document.head.appendChild(n)}();`}
