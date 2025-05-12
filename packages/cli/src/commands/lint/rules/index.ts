8694f8827d96314906c164738899c5570edcc295
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2025-05-12 15:50:38 +0100

    mastra lint for node version

--- packages/cli/src/commands/lint/rules/index.ts
+++ packages/cli/src/commands/lint/rules/index.ts
@@ -1,7 +1,14 @@
 import { mastraCoreRule } from './mastraCoreRule.js';
 import { mastraDepsCompatibilityRule } from './mastraDepsCompatibilityRule.js';
 import { nextConfigRule } from './nextConfigRule.js';
+import { nodeVersionRule } from './nodeVersionRule.js';
 import { tsConfigRule } from './tsConfigRule.js';
 import type { LintRule } from './types.js';
 
-export const rules: LintRule[] = [nextConfigRule, tsConfigRule, mastraCoreRule, mastraDepsCompatibilityRule];
+export const rules: LintRule[] = [
+  nextConfigRule,
+  tsConfigRule,
+  mastraCoreRule,
+  mastraDepsCompatibilityRule,
+  nodeVersionRule,
+];
