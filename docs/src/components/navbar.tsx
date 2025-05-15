c1e636090bc2d3d4ebbfba48c8821a199a992dbe
dayo <ifedayoadeniyi@gmail.com>
2025-05-15 12:51:12 +0100

    show downsized search on mobile

--- docs/src/components/navbar.tsx
+++ docs/src/components/navbar.tsx
@@ -2,6 +2,18 @@ import { GithubStarCount } from "@/components/github-star-count";
 
 import Link from "next/link";
 import { Navbar } from "nextra-theme-docs";
+import DocsChat from "@/chatbot/components/chat-widget";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import { Search } from "lucide-react";
+import { useEffect, useState } from "react";
+import { CustomSearch } from "./custom-search";
+import { getSearchPlaceholder } from "./search-placeholder";
+import { Button } from "./ui/button";
 
 export const Logo = () => {
   return (
@@ -53,7 +65,7 @@ export const Logo = () => {
     </svg>
   );
 };
-export const Nav = ({ stars }: { stars: number }) => {
+export const Nav = ({ stars, locale }: { stars: number; locale: string }) => {
   return (
     <Navbar
       logo={<Logo />}
@@ -70,6 +82,109 @@ export const Nav = ({ stars }: { stars: number }) => {
       >
         Docs
       </Link>
+      <SearchWrapperMobile locale={locale} />
     </Navbar>
+  );
+};
+
+const INPUTS = new Set(["INPUT", "SELECT", "BUTTON", "TEXTAREA"]);
+
+export const SearchWrapperMobile = ({ locale }: { locale: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isAgentMode, setIsAgentMode] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      const el = document.activeElement;
+      if (
+        !el ||
+        INPUTS.has(el.tagName) ||
+        (el as HTMLElement).isContentEditable
+      ) {
+        return;
+      }
+      if (
+        event.key === "/" ||
+        (event.key === "k" &&
+          !event.shiftKey &&
+          (navigator.userAgent.includes("Mac") ? event.metaKey : event.ctrlKey))
+      ) {
+        event.preventDefault();
+        // prevent to scroll to top
+        setIsOpen(true);
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  function open() {
+    setIsOpen(true);
+  }
+
+  function close() {
+    setIsOpen(false);
+    setIsAgentMode(false);
+  }
+
+  function handleUseAgent({ searchQuery }: { searchQuery: string }) {
+    setIsAgentMode(true);
+    setSearchQuery(searchQuery);
+  }
+
+  return (
+    <>
+      <Button
+        onClick={open}
+        size="sm"
+        variant="ghost"
+        className="block cursor-pointer md:hidden w-fit text-icons-3"
+      >
+        <Search className="w-4 h-4" />
+      </Button>
+      <Dialog
+        open={isOpen}
+        as="div"
+        className="relative md:hidden z-1000 focus:outline-none"
+        onClose={close}
+        unmount={true}
+      >
+        <DialogBackdrop className="fixed inset-0 delay-[0ms] duration-300 ease-out bg-black/50 backdrop-blur-md" />
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex items-start pt-[200px] justify-center min-h-full p-4">
+            <DialogPanel
+              transition
+              className="w-full border-[0.5px] border-borders-2 h-fit max-w-[660px] mx-auto rounded-xl bg-surface-4 duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+            >
+              <DialogTitle as="h3" className="sr-only">
+                Search
+              </DialogTitle>
+              <div className="w-full">
+                {isAgentMode ? (
+                  <DocsChat
+                    setIsAgentMode={setIsAgentMode}
+                    searchQuery={searchQuery}
+                  />
+                ) : (
+                  <div className="p-[10px]">
+                    <CustomSearch
+                      placeholder={getSearchPlaceholder(locale)}
+                      isAgentMode={isAgentMode}
+                      setIsAgentMode={setIsAgentMode}
+                      onUseAgent={handleUseAgent}
+                      closeModal={close}
+                    />
+                  </div>
+                )}
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
+    </>
   );
 };
