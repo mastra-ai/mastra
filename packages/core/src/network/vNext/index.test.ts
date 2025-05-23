27dfef67e5c033cea12c11282fcd4b7598cbcc97
Tony Kovanen <tony.kovanen@gmail.com>
2025-05-23 15:26:21 +0300

    individual use case

--- packages/core/src/network/vNext/index.test.ts
+++ packages/core/src/network/vNext/index.test.ts
@@ -134,7 +134,7 @@ class MockMemory extends MastraMemory {
 }
 
 describe('NewAgentNetwork', () => {
-  it('should create a new agent network', async () => {
+  it.skip('should create a new agent network', async () => {
     const memory = new MockMemory({
       name: 'test-memory',
     });
@@ -209,5 +209,105 @@ describe('NewAgentNetwork', () => {
     console.log(
       await network.generate('What are the biggest cities in France? How are they like?', { runtimeContext }),
     );
+  });
+
+  it.only('should create a new agent network single call', async () => {
+    const memory = new MockMemory({
+      name: 'test-memory',
+    });
+
+    const agent1 = new Agent({
+      name: 'agent1',
+      instructions:
+        'This agent is used to do research, but not create full responses. Answer in bullet points only and be concise.',
+      description:
+        'This agent is used to do research, but not create full responses. Answer in bullet points only and be concise.',
+      model: openai('gpt-4o'),
+    });
+
+    const agent2 = new Agent({
+      name: 'agent2',
+      description:
+        'This agent is used to do text synthesis on researched material. Write a full report based on the researched material. Do not use bullet points. Write full paragraphs. There should not be a single bullet point in the final report. You write articles.',
+      instructions:
+        'This agent is used to do text synthesis on researched material. Write a full report based on the researched material. Do not use bullet points. Write full paragraphs. There should not be a single bullet point in the final report. You write articles. [IMPORTANT] Make sure to mention information that has been highlighted as relevant in message history.',
+      model: openai('gpt-4o'),
+    });
+
+    const agentStep1 = createStep({
+      id: 'agent-step',
+      description: 'This step is used to do research and text synthesis.',
+      inputSchema: z.object({
+        city: z.string().describe('The city to research'),
+      }),
+      outputSchema: z.object({
+        text: z.string(),
+      }),
+      execute: async ({ inputData }) => {
+        const resp = await agent1.generate(inputData.city, {
+          output: z.object({
+            text: z.string(),
+          }),
+        });
+
+        return { text: resp.object.text };
+      },
+    });
+
+    const agentStep2 = createStep({
+      id: 'agent-step',
+      description: 'This step is used to do research and text synthesis.',
+      inputSchema: z.object({
+        text: z.string().describe('The city to research'),
+      }),
+      outputSchema: z.object({
+        text: z.string(),
+      }),
+      execute: async ({ inputData }) => {
+        const resp = await agent2.generate(inputData.text, {
+          output: z.object({
+            text: z.string(),
+          }),
+        });
+
+        return { text: resp.object.text };
+      },
+    });
+
+    const workflow1 = createWorkflow({
+      id: 'workflow1',
+      description: 'This workflow is perfect for researching a specific city.',
+      steps: [],
+      inputSchema: z.object({
+        city: z.string(),
+      }),
+      outputSchema: z.object({
+        text: z.string(),
+      }),
+    })
+      .then(agentStep1)
+      .then(agentStep2)
+      .commit();
+
+    const network = new NewAgentNetwork({
+      id: 'test-network',
+      name: 'Test Network',
+      instructions:
+        'You can research cities. You can also synthesize research material. You can also write a full report based on the researched material.',
+      model: openai('gpt-4o'),
+      agents: {
+        agent1,
+        agent2,
+      },
+      workflows: {
+        workflow1,
+      },
+      memory: memory,
+    });
+
+    const runtimeContext = new RuntimeContext();
+
+    console.log(await network.execute('What are the biggest cities in France? How are they like?', { runtimeContext }));
+    console.log(await network.execute('Tell me more about Paris', { runtimeContext }));
   });
 }, 120e3);
