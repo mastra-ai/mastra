8454a5bf1c2422696fa77e0ac89db6a46da30e05
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-05-30 12:22:30 +0200

    Fix example

--- examples/agent/src/mastra/index.ts
+++ examples/agent/src/mastra/index.ts
@@ -1,19 +1,19 @@
 import { Mastra } from '@mastra/core';
 import { PinoLogger } from '@mastra/loggers';
-// import { LibSQLStore } from '@mastra/libsql';
+import { LibSQLStore } from '@mastra/libsql';
 
 import { chefAgent, chefAgentResponses, dynamicAgent } from './agents/index';
 import { myMcpServer, myMcpServerTwo } from './mcp/server';
 import { myWorkflow } from './workflows';
 
-// const storage = new LibSQLStore({
-//   url: 'file:./mastra.db',
-// });
+const storage = new LibSQLStore({
+  url: 'file:./mastra.db',
+});
 
 export const mastra = new Mastra({
   agents: { chefAgent, chefAgentResponses, dynamicAgent },
   logger: new PinoLogger({ name: 'Chef', level: 'debug' }),
-  // storage,
+  storage,
   mcpServers: {
     myMcpServer,
     myMcpServerTwo,
