66aa2a891b0a8a0cbec3cc5149e4b301d0dbf00b
mfrachet <marvin@mastra.ai>
2025-06-06 10:35:01 +0200

    wip

--- packages/cli/src/playground/src/pages/workflows/layouts/workflow-graph-layout.tsx
+++ packages/cli/src/playground/src/pages/workflows/layouts/workflow-graph-layout.tsx
@@ -1,31 +1,40 @@
 import { WorkflowInformation } from '@/domains/workflows/workflow-information';
-import { MastraResizablePanel } from '@mastra/playground-ui';
+
 import { useParams } from 'react-router';
-import { WorkflowLogsWrapper } from '../workflow/components/workflow-logs-wrapper';
+
 import { useState } from 'react';
+import { WorkflowLogs } from '@/domains/workflows/workflow-logs';
+import { Header } from '@mastra/playground-ui';
+import { useSidebar } from '@/components/ui/sidebar';
+import clsx from 'clsx';
 
 export interface WorkflowGraphLayoutProps {
   children: React.ReactNode;
 }
 
 export const WorkflowGraphLayout = ({ children }: WorkflowGraphLayoutProps) => {
+  const { open } = useSidebar();
   const { workflowId } = useParams();
   const [runId, setRunId] = useState<string | undefined>(undefined);
 
   return (
     <main className="flex-1 relative divide-x flex w-full h-full overflow-y-auto">
-      <div className="min-w-[325px] grow relative">
-        {children}
-        {runId && <WorkflowLogsWrapper runId={runId} />}
-      </div>
-      <MastraResizablePanel
-        defaultWidth={20}
-        minimumWidth={20}
-        maximumWidth={60}
-        className="flex flex-col min-w-[325px] right-0 top-0 h-full z-20 bg-surface3 [&>div:first-child]:-left-[1px] [&>div:first-child]:-right-[1px] [&>div:first-child]:w-[1px] [&>div:first-child]:bg-[#424242] [&>div:first-child]:hover:w-[2px] [&>div:first-child]:active:w-[2px]"
-      >
+      <div className="min-w-[325px] grow relative">{children}</div>
+
+      {runId && (
+        <div
+          className={clsx(
+            'fixed bottom-3 rounded-t-lg right-[342px] overflow-y-auto h-1/4 bg-surface3 border-t-sm border-r-sm border-l-sm border-border1',
+            open ? 'left-44' : 'left-16',
+          )}
+        >
+          <Header>Logs</Header>
+          <WorkflowLogs runId={runId} />
+        </div>
+      )}
+      <div className="flex flex-col min-w-[325px] right-0 top-0 h-full z-20 bg-surface3">
         <WorkflowInformation workflowId={workflowId!} onTrigger={setRunId} />
-      </MastraResizablePanel>
+      </div>
     </main>
   );
 };
