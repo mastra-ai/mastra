b174950075dfee80d7d55ba89fc47c045d8d63b9
Marvin Frachet <marvin@mastra.ai>
2025-07-01 15:21:47 +0200

    Update packages/cli/src/playground/src/hooks/use-workflows.ts
    Co-authored-by: Tony Kovanen <rase-@users.noreply.github.com>

--- packages/cli/src/playground/src/hooks/use-workflows.ts
+++ packages/cli/src/playground/src/hooks/use-workflows.ts
@@ -271,7 +271,6 @@ export const useStreamWorkflow = () => {
         let status = '' as WorkflowRunStatus;
         while (true) {
           const { done, value } = await reader.read();
-          console.log('value', value);
           if (done) break;
           if (value.type === 'start') {
             setStreamResult((prev: WorkflowWatchResult) => ({
