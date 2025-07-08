125b705456e2564c74173fba70e85ae7cb0aaaec
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2025-07-08 13:02:51 +0100

    implement workflow retry and delay

--- examples/agent-network/src/mastra/workflows/step3.ts
+++ examples/agent-network/src/mastra/workflows/step3.ts
@@ -134,12 +134,18 @@ const planIndoorActivities = createStep({
   id: 'plan-indoor-activities',
   description: 'Suggests indoor activities based on weather conditions',
   inputSchema: forecastSchema,
+  resumeSchema: forecastSchema,
   outputSchema: z.object({
     activities: z.string(),
   }),
-  execute: async ({ inputData, mastra }) => {
-    console.log('planIndoorActivities', inputData);
-    const forecast = inputData;
+  execute: async ({ inputData, mastra, resumeData, suspend }) => {
+    if (!resumeData) {
+      suspend({ inputData });
+
+      return { activities: '' };
+    }
+    console.log('planIndoorActivities', resumeData);
+    const forecast = resumeData;
 
     if (!forecast) {
       throw new Error('Forecast data not found');
@@ -252,6 +258,17 @@ const planBothWorkflow = createWorkflow({
       }),
       steps: [planIndoorActivities],
     })
+      .then(
+        createStep({
+          id: 'plan-indoor-activities-first-step',
+          description: 'Plan indoor activities first step',
+          inputSchema: forecastSchema,
+          outputSchema: forecastSchema,
+          execute: async ({ inputData }) => {
+            return inputData;
+          },
+        }),
+      )
       .then(planIndoorActivities)
       .commit(),
   ])
