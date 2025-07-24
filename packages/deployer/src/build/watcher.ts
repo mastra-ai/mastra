cb3af9fbfcbca4aefadc7c04c740742c77cca22f
wardpeet <ward@coding-tech.com>
2025-07-24 13:42:57 +0200

    update watch cli to support transpile packages

--- packages/deployer/src/build/watcher.ts
+++ packages/deployer/src/build/watcher.ts
@@ -4,17 +4,42 @@ import { getInputOptions as getBundlerInputOptions } from './bundler';
 import { aliasHono } from './plugins/hono-alias';
 import { nodeModulesExtensionResolver } from './plugins/node-modules-extension-resolver';
 import { tsConfigPaths } from './plugins/tsconfig-paths';
+import { bundleExternals } from './analyze';
+import { noopLogger } from '@mastra/core/logger';
 
 export async function getInputOptions(
   entryFile: string,
   platform: 'node' | 'browser',
   env?: Record<string, string>,
-  { sourcemap = false }: { sourcemap?: boolean } = {},
+  { sourcemap = false, transpilePackages = [] }: { sourcemap?: boolean; transpilePackages?: string[] } = {},
 ) {
+  const dependencies = new Map<string, string>();
+
+  if (transpilePackages.length) {
+    const { output, reverseVirtualReferenceMap } = await bundleExternals(
+      new Map(transpilePackages.map(pkg => [pkg, ['*']])),
+      '.mastra/.build',
+      noopLogger,
+      {
+        transpilePackages,
+      },
+    );
+
+    for (const file of output) {
+      if (file.type === 'asset') {
+        continue;
+      }
+
+      if (file.isEntry && reverseVirtualReferenceMap.has(file.name)) {
+        dependencies.set(reverseVirtualReferenceMap.get(file.name)!, file.fileName);
+      }
+    }
+  }
+
   const inputOptions = await getBundlerInputOptions(
     entryFile,
     {
-      dependencies: new Map(),
+      dependencies,
       externalDependencies: new Set(),
       invalidChunks: new Set(),
     },
