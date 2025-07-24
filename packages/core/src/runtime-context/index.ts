074a879c34d456dfc8ed86b811cc313376dc487e
Gautam Panakkal <gautampanakkal@gmail.com>
2025-07-24 13:17:49 -0700

    `RuntimeContext.entries()`: improve type narrowing on return value
    Co-authored-by: Morgan Davis <morgansumdavis@gmail.com>

--- packages/core/src/runtime-context/index.ts
+++ packages/core/src/runtime-context/index.ts
@@ -2,24 +2,19 @@ type RecordToTuple<T> = {
   [K in keyof T]: [K, T[K]];
 }[keyof T][];
 
-export class RuntimeContext<Values extends Record<string, any> | unknown = unknown> {
-  private registry = new Map<string, unknown>();
+export interface RuntimeContextInterface extends Record<string, any> {}
 
-  constructor(
-    iterable?: Values extends Record<string, any>
-      ? RecordToTuple<Partial<Values>>
-      : Iterable<readonly [string, unknown]>,
-  ) {
+export class RuntimeContext<Values extends RuntimeContextInterface = RuntimeContextInterface> {
+  private registry: Map<string, any>;
+
+  constructor(iterable?: Iterable<{ [K in keyof Values]: K extends string ? [K, Values[K]] : never }[keyof Values]>) {
     this.registry = new Map(iterable);
   }
 
   /**
    * set a value with strict typing if `Values` is a Record and the key exists in it.
    */
-  public set<K extends Values extends Record<string, any> ? keyof Values : string>(
-    key: K,
-    value: Values extends Record<string, any> ? (K extends keyof Values ? Values[K] : never) : unknown,
-  ): void {
+  public set<K extends keyof Values>(key: K, value: Values[K]): void {
     // The type assertion `key as string` is safe because K always extends string ultimately.
     this.registry.set(key as string, value);
   }
@@ -27,24 +22,21 @@ export class RuntimeContext<Values extends Record<string, any> | unknown = unkno
   /**
    * Get a value with its type
    */
-  public get<
-    K extends Values extends Record<string, any> ? keyof Values : string,
-    R = Values extends Record<string, any> ? (K extends keyof Values ? Values[K] : never) : unknown,
-  >(key: K): R {
-    return this.registry.get(key as string) as R;
+  public get<K extends keyof Values>(key: K): Values[K] {
+    return this.registry.get(key as string) as Values[K];
   }
 
   /**
    * Check if a key exists in the container
    */
-  public has<K extends Values extends Record<string, any> ? keyof Values : string>(key: K): boolean {
+  public has(key: string): boolean {
     return this.registry.has(key);
   }
 
   /**
    * Delete a value by key
    */
-  public delete<K extends Values extends Record<string, any> ? keyof Values : string>(key: K): boolean {
+  public delete(key: string): boolean {
     return this.registry.delete(key);
   }
 
@@ -58,24 +50,22 @@ export class RuntimeContext<Values extends Record<string, any> | unknown = unkno
   /**
    * Get all keys in the container
    */
-  public keys<R = Values extends Record<string, any> ? keyof Values : string>(): IterableIterator<R> {
-    return this.registry.keys() as IterableIterator<R>;
+  public keys(): IterableIterator<keyof Values> {
+    return this.registry.keys() as IterableIterator<keyof Values>;
   }
 
   /**
    * Get all values in the container
    */
-  public values<R = Values extends Record<string, any> ? Values[keyof Values] : unknown>(): IterableIterator<R> {
-    return this.registry.values() as IterableIterator<R>;
+  public values(): IterableIterator<Values[keyof Values]> {
+    return this.registry.values() as IterableIterator<Values[keyof Values]>;
   }
 
   /**
    * Get all entries in the container
    */
-  public entries<R = Values extends Record<string, any> ? Values[keyof Values] : unknown>(): IterableIterator<
-    [string, R]
-  > {
-    return this.registry.entries() as IterableIterator<[string, R]>;
+  public entries(): IterableIterator<{ [K in keyof Values]: [K, Values[K]] }[keyof Values]> {
+    return this.registry.entries() as IterableIterator<{ [K in keyof Values]: [K, Values[K]] }[keyof Values]>;
   }
 
   /**
@@ -88,7 +78,7 @@ export class RuntimeContext<Values extends Record<string, any> | unknown = unkno
   /**
    * Execute a function for each entry in the container
    */
-  public forEach<T = any>(callbackfn: (value: T, key: string, map: Map<string, any>) => void): void {
+  public forEach<K extends keyof Values>(callbackfn: (value: Values[K], key: K, map: Map<string, any>) => void): void {
     this.registry.forEach(callbackfn as any);
   }
 }
