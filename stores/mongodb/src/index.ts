432bd46d1d9f012e127a1cae07628357ad2b9925
raulanatol <raul@natol.es>
2025-07-25 08:43:30 +0100

    Cleanup duplicate ConnectorHandler

--- stores/mongodb/src/index.ts
+++ stores/mongodb/src/index.ts
@@ -1,4 +1,4 @@
 export * from './vector';
 export * from './storage';
-export * from './storage/ConnectorHandler';
+export * from './storage/connectors/base';
 export { MONGODB_PROMPT } from './vector/prompt';
