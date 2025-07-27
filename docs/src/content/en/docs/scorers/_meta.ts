f0f8e8ade6bfb33de95309e41ff40f16ef68d824
Yujohn Nattrass <yujohn.natt@gmail.com>
2025-07-27 14:36:01 -0700

    init docs

---
+++ docs/src/content/en/docs/scorers/_meta.ts
@@ -0,0 +1,8 @@
+const meta = {
+  overview: "Overview",
+  "off-the-shelf-scorers": "Off-the-Shelf Scorers",
+  "custom-scorers": "Custom Scorers",
+  "running-in-ci": "Running in CI",
+};
+
+export default meta;
