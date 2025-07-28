94de71b8b773b756d2bb4dda5a14385a6327d326
Yujohn Nattrass <yujohn.natt@gmail.com>
2025-07-28 08:22:23 -0700

    continue to improve

--- docs/src/content/en/docs/scorers/_meta.ts
+++ docs/src/content/en/docs/scorers/_meta.ts
@@ -2,7 +2,6 @@ const meta = {
   overview: "Overview",
   "off-the-shelf-scorers": "Off the Shelf Scorers",
   "custom-scorers": "Custom Scorers",
-  "running-in-ci": "Running in CI",
 };
 
 export default meta;
