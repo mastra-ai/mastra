3b53872ea80ccc73527708219fd9b7f45f491f65
Akuya Ekorot <akuyaekorot@gmail.com>
2025-07-28 09:51:13 +0300

    docs(fix): add templates documentation

---
+++ docs/src/content/en/docs/templates/_meta.ts
@@ -0,0 +1,19 @@
+import { Meta } from "nextra";
+
+const meta: Meta = {
+  overview: {
+    title: "Overview",
+  },
+  "using-templates": {
+    title: "Using Templates",
+  },
+  "template-rules": {
+    title: "Template Rules",
+  },
+  contributing: {
+    title: "Contributing",
+  },
+};
+
+export default meta;
+
