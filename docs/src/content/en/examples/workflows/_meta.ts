ce2eb6faa0539988005b272c5df172b3864bbdf0
Paul Scanlon <pauliescanlon@gmail.com>
2025-07-28 10:36:05 -0500

    dos: update control flow and conditional branching

--- docs/src/content/en/examples/workflows/_meta.ts
+++ docs/src/content/en/examples/workflows/_meta.ts
@@ -1,11 +1,11 @@
 const meta = {
+  "control-flow": "Control Flow",
   "conditional-branching": "Conditional Branching",
   "parallel-steps": "Parallel Steps",
+  "array-as-input": "Array as Input",
   "calling-agent": "Calling an Agent",
   "agent-and-tool-interop": "Tool & Agent as a Step",
   "human-in-the-loop": "Human in the Loop",
-  "control-flow": "Control Flow",
-  "array-as-input": "Array as Input",
   "inngest-workflow": "Inngest Workflow",
   // "input-data-mapping": "Input Data Mapping",
 };
