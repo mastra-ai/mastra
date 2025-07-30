b89172a410a3d7dea8f73d1522ece228ec7255e0
Tyler Barnes <tylerdbarnes@gmail.com>
2025-07-30 11:46:31 -0700

    prettier

--- packages/evals/tsup.config.ts
+++ packages/evals/tsup.config.ts
@@ -5,7 +5,14 @@ import { defineConfig } from 'tsup';
 const exec = promisify(spawn);
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/metrics/judge/index.ts', 'src/metrics/llm/index.ts', 'src/metrics/nlp/index.ts', 'src/scorers/llm/index.ts', 'src/scorers/code/index.ts'],
+  entry: [
+    'src/index.ts',
+    'src/metrics/judge/index.ts',
+    'src/metrics/llm/index.ts',
+    'src/metrics/nlp/index.ts',
+    'src/scorers/llm/index.ts',
+    'src/scorers/code/index.ts',
+  ],
   format: ['esm', 'cjs'],
   clean: true,
   dts: false,
