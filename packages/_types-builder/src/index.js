c420b671ca08c715122c1f746216407dcd920654
wardpeet <ward@coding-tech.com>
2025-08-05 14:10:36 +0200

    fix @mastra/client types

---
+++ packages/_types-builder/src/index.js
@@ -0,0 +1,47 @@
+import { exec as originalExec } from 'child_process';
+import { promisify } from 'util';
+import { globby } from 'globby';
+import fs from 'fs/promises';
+import path from 'path';
+
+const exec = promisify(originalExec);
+const rgxFrom = /(?<=from )['|"](.*)['|"]/gm;
+
+// @see https://blog.devgenius.io/compiling-from-typescript-with-js-extension-e2b6de3e6baf
+export async function generateTypes(rootDir) {
+  try {
+    await exec('pnpm tsc -p tsconfig.build.json', {
+      stdio: 'inherit',
+      cwd: rootDir,
+    });
+
+    const dtsFiles = await globby('dist/**/*.d.ts', {
+      cwd: rootDir,
+      onlyFiles: true,
+    });
+    for (const dtsFile of dtsFiles) {
+      const fullPath = path.join(rootDir, dtsFile);
+      let modified = false;
+      let code = (await fs.readFile(fullPath)).toString();
+
+      code = code.replace(rgxFrom, (_, p) => {
+        if (!(p.startsWith('./') || p.startsWith('../')) || p.endsWith('.js')) {
+          return `'${p}'`;
+        }
+
+        modified = true;
+        return `'${p}.js'`;
+      });
+
+      if (!modified) {
+        continue;
+      }
+
+      await fs.writeFile(fullPath, code);
+    }
+  } catch (err) {
+    console.log(err);
+    // debug here?
+    throw err;
+  }
+}
