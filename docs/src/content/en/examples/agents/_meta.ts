12cb7361e0b1ee46b859bf90fe9a587d1c869540
Paul Scanlon <pauliescanlon@gmail.com>
2025-08-19 19:37:17 +0100

    docs: add using a workflow agent example

--- docs/src/content/en/examples/agents/_meta.ts
+++ docs/src/content/en/examples/agents/_meta.ts
@@ -1,11 +1,11 @@
 const meta = {
   "calling-agents": "Calling Agents",
   "system-prompt": "Changing the System Prompt",
-  "using-a-tool": "Adding a tool",
+  "using-a-tool": "Adding a Tool",
+  "using-a-workflow": "Adding a Workflow",
   "supervisor-agent": "Supervisor Agent",
   "image-analysis": "Image Analysis",
   "adding-voice-capabilities": "Using Voice",
-  "workflow-as-tool": "Workflow as Tool",
   "dynamic-agents": "Dynamic Context",
   "deploying-mcp-server": "Deploying an MCPServer",
   "ai-sdk-v5-integration": "AI SDK v5 Integration",
