37d961d915e0d321e356467a87ecd19b0ab0d43d
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-08-20 18:20:43 -0700

    Remove log

--- packages/core/src/stream/base/output.ts
+++ packages/core/src/stream/base/output.ts
@@ -182,7 +182,6 @@ export class MastraModelOutput extends MastraBase {
               };
               break;
             case 'reasoning-delta': {
-              console.log(chunk.payload.text, 'REASONING DELTA #####');
               self.#bufferedReasoning.push(chunk.payload.text);
               self.#bufferedByStep.reasoning += chunk.payload.text;
 
