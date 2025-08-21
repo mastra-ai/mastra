08273ffaaf1bc5a934f394c05bf6b82b34f45078
Yujohn Nattrass <yujohn.natt@gmail.com>
2025-08-20 21:06:47 -0700

    fix agent scorers display

--- packages/server/src/server/handlers/scores.ts
+++ packages/server/src/server/handlers/scores.ts
@@ -24,12 +24,12 @@ async function getScorersFromSystem({
     if (Object.keys(scorers).length > 0) {
       for (const [scorerId, scorer] of Object.entries(scorers)) {
         if (scorersMap.has(scorerId)) {
-          scorersMap.get(scorerId)?.agentIds.push(agentId);
+          scorersMap.get(scorerId)?.agentIds.push(agent.name);
         } else {
           scorersMap.set(scorerId, {
             workflowIds: [],
             ...scorer,
-            agentIds: [agentId],
+            agentIds: [agent.name],
           });
         }
       }
