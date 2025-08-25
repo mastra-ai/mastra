4286297a7d12c885a12aa63ebd4f6ee2be8cc72b
Kenny <adelekekehinde06@gmail.com>
2025-08-25 13:49:26 +0100

    fix: bugs with focus in copy dropdown

--- docs/src/mdx-components.tsx
+++ docs/src/mdx-components.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* jeslint-disable @typescript-eslint/no-explicit-any */
 import { useMDXComponents as getThemeComponents } from "nextra-theme-docs"; // nextra-theme-blog or your custom theme
 import { OperatorsTable } from "./components/operators-table";
 import { PropertiesTable } from "./components/properties-table";
