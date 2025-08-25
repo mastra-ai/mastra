c13b111059e206dbf0eb0e868c219828e6ad41ae
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2025-08-25 15:31:30 +0100

    support z.date in custom zodToJson

--- packages/schema-compat/src/zod-to-json.ts
+++ packages/schema-compat/src/zod-to-json.ts
@@ -8,7 +8,16 @@ import zodToJsonSchemaOriginal from 'zod-to-json-schema';
 export function zodToJsonSchema(zodSchema: ZodSchemaV3 | ZodSchemaV4, target: Targets = 'jsonSchema7') {
   if ('toJSONSchema' in z) {
     // @ts-expect-error - type not present main zod v3
-    return z.toJSONSchema(zodSchema) as JSONSchema7;
+    return z.toJSONSchema(zodSchema, {
+      unrepresentable: 'any',
+      override: (ctx: any) => {
+        const def = ctx.zodSchema._zod.def;
+        if (def.type === 'date') {
+          ctx.jsonSchema.type = 'string';
+          ctx.jsonSchema.format = 'date-time';
+        }
+      },
+    }) as JSONSchema7;
   } else {
     return zodToJsonSchemaOriginal(zodSchema as ZodSchemaV3, {
       $refStrategy: 'none',
