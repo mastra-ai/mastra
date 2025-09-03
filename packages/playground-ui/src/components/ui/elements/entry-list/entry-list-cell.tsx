437f30e241bb874cee00d7eafa50f03bb3aab6e4
Greg Lobinski <greglobinski@gmail.com>
2025-09-03 12:17:03 +0200

    add useMemo

--- packages/playground-ui/src/components/ui/elements/entry-list/entry-list-cell.tsx
+++ packages/playground-ui/src/components/ui/elements/entry-list/entry-list-cell.tsx
@@ -1,18 +1,19 @@
 import { cn } from '@/lib/utils';
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
+import { useMemo } from 'react';
 
 export function EntryListTextCell({ children, isLoading }: { children: React.ReactNode; isLoading?: boolean }) {
-  // Generate random width from 50% to 90%
-  const getRandomWidth = () => {
+  // Generate random width once per component render
+  const randomWidth = useMemo(() => {
     return Math.floor(Math.random() * (90 - 50 + 1)) + 50;
-  };
+  }, []);
 
   return (
     <div className="text-icon4 text-[0.875rem] truncate ">
       {isLoading ? (
         <div
           className=" bg-surface4 rounded-md animate-pulse text-transparent h-[1rem] select-none"
-          style={{ width: `${getRandomWidth()}%` }}
+          style={{ width: `${randomWidth}%` }}
         ></div>
       ) : (
         children
