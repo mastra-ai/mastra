d706fad6e6e4b72357b18d229ba38e6c913c0e70
Marvin Frachet <marvin@mastra.ai>
2025-09-15 15:48:20 +0200

    fix: make sure to fetch the runs list when clicking on the tab (#7852)
    ## Description
    
    The runs list was fetched when mounting the graph component meaning that
    subsequent triggers will never show up.
    
    This PR moves the fetch to the runs tab directly so that it remounts
    every time we click on it (and thus refetch the runs, and thus is up to
    date 🙃 )
    
    ## Related Issue(s)
    
    <!-- Link to the issue(s) this PR addresses, using hashtag notation:
    #123 -->
    
    ## Type of Change
    
    - [x] Bug fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Documentation update
    - [ ] Code refactoring
    - [ ] Performance improvement
    - [ ] Test update
    
    ## Checklist
    
    - [ ] I have made corresponding changes to the documentation (if
    applicable)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works

--- packages/playground/src/domains/workflows/workflow-layout.tsx
+++ packages/playground/src/domains/workflows/workflow-layout.tsx
@@ -8,6 +8,7 @@ import {
   MastraResizablePanel,
   MainContentContent,
   useWorkflow,
+  useWorkflowRuns,
 } from '@mastra/playground-ui';
 
 import { Skeleton } from '@/components/ui/skeleton';
@@ -15,13 +16,12 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { cn } from '@/lib/utils';
 
 import { WorkflowHeader } from './workflow-header';
-import { useWorkflowRuns } from '@/pages/workflows/workflow/hooks/use-workflow-runs';
 import { WorkflowInformation } from './workflow-information';
 
 export const WorkflowLayout = ({ children }: { children: React.ReactNode }) => {
   const { workflowId, runId } = useParams();
   const { data: workflow, isLoading: isWorkflowLoading } = useWorkflow(workflowId!);
-  const { data: runs } = useWorkflowRuns({ workflowId: workflowId! });
+  const { data: runs } = useWorkflowRuns(workflowId!);
   const location = useLocation();
   const isGraphPage = location.pathname.includes('/graph');
 
