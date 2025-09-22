d3698f71b3ee42fbecd6e1c4acad235e9b7ea9b2
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-09-22 15:06:57 +0200

    AISDK convert

--- examples/agent/src/mastra/agents/external.ts
+++ examples/agent/src/mastra/agents/external.ts
@@ -1,23 +1,62 @@
-import { toMastraCompatible } from "@mastra/agent-compat";
-import { z } from "zod";
+import { toMastraCompatible, toMastraCompatibleAISDK } from '@mastra/agent-compat';
+import { Experimental_Agent as Agent, stepCountIs, tool as aisdkTool } from 'ai-v5';
+import { z } from 'zod';
+
+import { openai } from '@ai-sdk/openai-v5';
 // npm install @langchain/anthropic to call the model
-import { createAgent, tool } from "langchain";
+import { createAgent, tool } from 'langchain';
 
 const getWeather = tool(({ city }) => `It's always sunny in ${city}!`, {
-    name: "get_weather",
-    description: "Get the weather for a given city",
-    schema: z.object({
-        city: z.string(),
-    }),
+  name: 'get_weather',
+  description: 'Get the weather for a given city',
+  schema: z.object({
+    city: z.string(),
+  }),
 });
 
 const agent = createAgent({
-    name: "langchain-compatible",
-    prompt: "Hi",
-    model: "openai:gpt-4o-mini",
-    tools: [getWeather],
+  name: 'langchain-compatible',
+  prompt: 'Hi',
+  model: 'openai:gpt-4o-mini',
+  tools: [getWeather],
 });
 
-export const mastraCompatible = toMastraCompatible({
-    agent: agent as any,
+export const weatherAgentLangChain = toMastraCompatible({
+  agent: agent as any,
+});
+
+const aisdkAgent = new Agent({
+  model: openai('gpt-4o-mini'),
+  tools: {
+    weatherTool: aisdkTool({
+      description: 'Get the Weather',
+      inputSchema: z.object({
+        location: z.string(),
+      }),
+      execute: async ({ location }) => {
+        return { output: `The weather in ${location} is sunny` };
+      },
+    }),
+  },
+  stopWhen: [
+    stepCountIs(10), // Maximum 10 steps
+  ],
+  system: `
+        You are a helpful weather assistant that provides accurate weather information and can help planning activities based on the weather.
+
+        Your primary function is to help users get weather details for specific locations. When responding:
+        - Always ask for a location if none is provided
+        - If the location name isn't in English, please translate it
+        - If giving a location with multiple parts (e.g. "New York, NY"), use the most relevant part (e.g. "New York")
+        - Include relevant details like humidity, wind conditions, and precipitation
+        - Keep responses concise but informative
+        - If the user asks for activities and provides the weather forecast, suggest activities based on the weather forecast.
+        - If the user asks for activities, respond in the format they request.
+
+        Use the weatherTool to fetch current weather data.
+    `,
+});
+
+export const weatherAgentAISDK = toMastraCompatibleAISDK({
+  agent: aisdkAgent as any,
 });
--- examples/agent/src/mastra/index.ts
+++ examples/agent/src/mastra/index.ts
@@ -8,7 +8,7 @@ import { myWorkflow } from './workflows';
 import { chefModelV2Agent, networkAgent } from './agents/model-v2-agent';
 import { createScorer } from '@mastra/core/scores';
 import { myWorkflowX } from './workflows/other';
-import { mastraCompatible } from './agents/external';
+import { weatherAgentAISDK, weatherAgentLangChain } from './agents/external';
 
 const storage = new LibSQLStore({
   url: 'file:./mastra.db',
@@ -30,7 +30,8 @@ export const mastra = new Mastra({
     evalAgent,
     chefModelV2Agent,
     networkAgent,
-    mastraCompatible: mastraCompatible as any,
+    weatherAgentLangChain: weatherAgentLangChain,
+    weatherAgentAISDK: weatherAgentAISDK,
   },
   logger: new PinoLogger({ name: 'Chef', level: 'debug' }),
   storage,
