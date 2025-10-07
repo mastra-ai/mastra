6ba2893e4ed5076870023695fde23c20105fe14a
dayo <ifedayoadeniyi@gmail.com>
2025-10-07 13:54:31 +0100

    update flag setup

--- docs/src/app/.well-known/vercel/flags/route.ts
+++ docs/src/app/.well-known/vercel/flags/route.ts
@@ -1,16 +1,4 @@
-import { verifyAccess, type ApiData } from "flags";
-import { getProviderData } from "flags/next";
-
-import { NextResponse, type NextRequest } from "next/server";
-
+import { getProviderData, createFlagsDiscoveryEndpoint } from "flags/next";
 import * as flags from "@/lib/flags";
 
-export const runtime = "edge";
-export const dynamic = "force-dynamic";
-
-export async function GET(request: NextRequest) {
-  const access = await verifyAccess(request.headers.get("Authorization"));
-  if (!access) return NextResponse.json(null, { status: 401 });
-
-  return NextResponse.json<ApiData>(getProviderData(flags));
-}
+export const GET = createFlagsDiscoveryEndpoint(() => getProviderData(flags));
