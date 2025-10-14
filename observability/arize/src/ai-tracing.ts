0553e11504bcd7fd8d6a41e61748a3480f20bd61
Tony Powell <apowell@arize.com>
2025-10-14 10:36:25 -0400

    Correctly pass through configuration object to OTELExporter

--- observability/arize/src/ai-tracing.ts
+++ observability/arize/src/ai-tracing.ts
@@ -1,21 +1,55 @@
-import type { TracingConfig } from '@mastra/core/ai-tracing';
-import { OtelExporter } from '@mastra/otel-exporter';
+import { OtelExporter, type OtelExporterConfig } from '@mastra/otel-exporter';
 import { ArizeSpanConverter } from './span-converter';
+import { SEMRESATTRS_PROJECT_NAME } from '@arizeai/openinference-semantic-conventions';
 
-export type ArizeExporterConfig = TracingConfig & {
+export type ArizeExporterConfig = Omit<OtelExporterConfig, 'provider'> & {
+  /**
+   * Required if sending traces to Arize AX
+   */
   spaceId?: string;
-  spaceKey?: string;
+  /**
+   * Required if sending traces to Arize AX, or to any other collector that
+   * requires an Authorization header
+   */
   apiKey?: string;
+  /**
+   * Required collector endpoint destination for trace exports
+   */
   endpoint: string;
+  /**
+   * Optional project name to be added as a resource attribute using
+   * OpenInference Semantic Conventions
+   */
+  projectName?: string;
 };
 
 export class ArizeExporter extends OtelExporter {
   name = 'arize';
 
   constructor(config: ArizeExporterConfig) {
+    const headers: Record<string, string> = {};
+    if (config.spaceId) {
+      // arize ax header configuration
+      headers['space_id'] = config.spaceId;
+      headers['api_key'] = config.apiKey ?? '';
+    } else if (config.apiKey) {
+      // standard otel header configuration
+      headers['Authorization'] = `Bearer ${config.apiKey}`;
+    }
     super({
-      ...config,
       spanConverters: [new ArizeSpanConverter()],
-    });
+      ...config,
+      resourceAttributes: {
+        [SEMRESATTRS_PROJECT_NAME]: config.projectName,
+        ...config.resourceAttributes,
+      },
+      provider: {
+        custom: {
+          endpoint: config.endpoint,
+          headers,
+          protocol: 'http/protobuf',
+        },
+      } satisfies OtelExporterConfig['provider'],
+    } satisfies OtelExporterConfig);
   }
 }
