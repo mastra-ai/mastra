70f7c8c5f78530837ef56443aeb6dd60c75a1a0e
Tony Powell <apowell@arize.com>
2025-10-14 16:05:36 -0400

    Bump @arizeai/openinference-genai

--- observability/arize/src/span-converter.ts
+++ observability/arize/src/span-converter.ts
@@ -14,6 +14,13 @@ export class ArizeSpanConverter extends ReadableSpanConverterInterface {
   }
 
   convertSpan(span: ReadableSpan): ReadableSpan {
+    // slight hacks until otel-exporter adheres to modern genai spec
+    if (span.attributes['input']) {
+      span.attributes['gen_ai.input.messages'] = span.attributes['input'];
+    }
+    if (span.attributes['agent.id']) {
+      span.attributes['gen_ai.agent.id'] = span.attributes['agent.id'];
+    }
     const attributes = convertGenAISpanAttributesToOpenInferenceSpanAttributes(span.attributes);
     return {
       ...span,
