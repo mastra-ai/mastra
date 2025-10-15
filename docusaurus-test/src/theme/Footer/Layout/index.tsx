8f9a66bfb35ac4817fdc5df42f5e6a1cbb064440
Kenny <adelekekehinde06@gmail.com>
2025-10-15 15:20:31 +0100

    feat: update footer layout

--- docusaurus-test/src/theme/Footer/Layout/index.tsx
+++ docusaurus-test/src/theme/Footer/Layout/index.tsx
@@ -5,13 +5,13 @@ import { Logo } from '../../Navbar';
 
 export default function FooterLayout({ style, links, logo, copyright }: Props): ReactNode {
   return (
-    <footer className="border-t-[0.5px] border-(--border) pt-8  pb-24 md:pb-20">
+    <footer className="border-t-[0.5px] border-(--border) pt-8  pb-24 md:pb-6">
       <div className="max-w-(--ifm-container-width) mx-auto w-full">
-        <div className="flex justify-between gap-[400px]">
+        <div className="flex justify-between">
           <Logo />
-          <div className="container container-fluid">{links}</div>
+          <div className="container-fluid">{links}</div>
         </div>
-        {copyright && <div className="mt-20 text-left">{copyright}</div>}
+        {copyright && <div className="text-left">{copyright}</div>}
       </div>
     </footer>
   );
