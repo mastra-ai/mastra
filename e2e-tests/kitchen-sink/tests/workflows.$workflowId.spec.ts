87e75a851f2ed085d2b8edd8b7b3b44e3e13aeb1
mfrachet <marvin@mastra.ai>
2025-10-15 13:43:02 +0200

    initial workflow state

--- e2e-tests/kitchen-sink/tests/workflows.$workflowId.spec.ts
+++ e2e-tests/kitchen-sink/tests/workflows.$workflowId.spec.ts
@@ -36,33 +36,56 @@ test('overall layout information', async ({ page }) => {
   await expect(codeEditor).toHaveAttribute('data-language', 'json');
 });
 
-test('running the workflow (form)', async ({ page }) => {
-  await page.goto('http://localhost:4111/workflows/complexWorkflow/graph');
-  await checkInitialState(page);
+test.describe('workflow run', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:4111/workflows/complexWorkflow/graph');
+  });
+
+  test('initial workflow run state', async ({ page }) => {
+    const nodes = await page.locator('[data-workflow-node]');
+    await expect(nodes).toHaveCount(14);
+
+    // Check node ordering
+    await expect(nodes.nth(0)).toContainText('add-letter');
+    await expect(nodes.nth(1)).toContainText('add-letter-b');
+    await expect(nodes.nth(2)).toContainText('add-letter-c');
+    await expect(nodes.nth(3)).toContainText('mapping');
+    await expect(nodes.nth(4)).toContainText('WHEN');
+    await expect(nodes.nth(5)).toContainText('short-text');
+    await expect(nodes.nth(6)).toContainText('WHEN');
+    await expect(nodes.nth(7)).toContainText('long-text');
+    await expect(nodes.nth(8)).toContainText('mapping');
+    await expect(nodes.nth(9)).toContainText('nested-text-processor');
+    await expect(nodes.nth(10)).toContainText('add-letter-with-count');
+    await expect(nodes.nth(11)).toContainText('DOUNTIL');
+    await expect(nodes.nth(12)).toContainText('suspend-resume');
+    await expect(nodes.nth(13)).toContainText('final-step');
+  });
+
+  test('running the workflow (form) - short condition', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'Text' }).fill('A');
+    await page.getByRole('button', { name: 'Run' }).click();
+
+    await runWorkflow(page);
+    const nodes = await page.locator('[data-workflow-node]');
+    const shortNode = nodes.nth(5);
+    const longNode = nodes.nth(7);
+    await expect(shortNode.locator('[aria-label="Success"]')).toBeVisible();
+    await expect(longNode.locator('[aria-label="Idle"]')).toBeVisible();
+  });
+
+  test('running the workflow (form) - long condition', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'Text' }).fill('SuperLongTextToStartWith');
+    await page.getByRole('button', { name: 'Run' }).click();
+
+    await runWorkflow(page);
+    const nodes = await page.locator('[data-workflow-node]');
+    const shortNode = nodes.nth(5);
+    const longNode = nodes.nth(7);
+
+    await expect(shortNode.locator('[aria-label="Idle"]')).toBeVisible();
+    await expect(longNode.locator('[aria-label="Success"]')).toBeVisible();
+  });
 });
 
-async function checkInitialState(page: Page) {
-  const nodes = await page.locator('[data-workflow-node]');
-  await expect(nodes).toHaveCount(14);
-
-  // Check node ordering
-  await expect(nodes.nth(0)).toContainText('add-letter');
-  await expect(nodes.nth(1)).toContainText('add-letter-b');
-  await expect(nodes.nth(2)).toContainText('add-letter-c');
-  await expect(nodes.nth(3)).toContainText('mapping');
-  await expect(nodes.nth(4)).toContainText('WHEN');
-  await expect(nodes.nth(5)).toContainText('short-text');
-  await expect(nodes.nth(6)).toContainText('WHEN');
-  await expect(nodes.nth(7)).toContainText('long-text');
-  await expect(nodes.nth(8)).toContainText('mapping');
-  await expect(nodes.nth(9)).toContainText('nested-text-processor');
-  await expect(nodes.nth(10)).toContainText('add-letter-with-count');
-  await expect(nodes.nth(11)).toContainText('DOUNTIL');
-  await expect(nodes.nth(12)).toContainText('suspend-resume');
-  await expect(nodes.nth(13)).toContainText('final-step');
-}
-
-async function runWorkflow(page: Page) {
-  await page.getByRole('textbox', { name: 'Text' }).fill('Hello World');
-  await page.getByRole('button', { name: 'Run' }).click();
-}
+async function runWorkflow(page: Page) {}
