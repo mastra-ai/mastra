a7e978b20a6a41baac33a17deeff34cb6723c4da
LekoArts <lekoarts@gmail.com>
2025-10-16 11:37:23 +0200

    update test

--- e2e-tests/monorepo/template/packages/hello-world/src/tool.ts
+++ e2e-tests/monorepo/template/packages/hello-world/src/tool.ts
@@ -1,8 +1,8 @@
 import { createTool } from '@mastra/core/tools';
-import { HELLO_WORLD } from './constants';
+import { HELLO_WORLD, TEST_PATH } from './constants';
 
 export const helloWorldTool = createTool({
   id: 'hello-world',
   description: 'A tool that returns hello world',
-  execute: async () => HELLO_WORLD,
+  execute: async () => HELLO_WORLD + ' from ' + TEST_PATH,
 });
