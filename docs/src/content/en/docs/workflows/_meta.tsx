3689314a9ba85ff2e3357dadb97aa35501bbdb8a
Paul Scanlon <pauliescanlon@gmail.com>
2025-10-20 09:28:25 +0100

    docs: wip

--- docs/src/content/en/docs/workflows/_meta.tsx
+++ docs/src/content/en/docs/workflows/_meta.tsx
@@ -3,8 +3,8 @@ import { Tag } from "@/components/tag";
 const meta = {
   overview: <Tag text="new">Overview</Tag>,
   "control-flow": "Control Flow",
-  "suspend-and-resume": "Suspend & Resume",
   "using-with-agents-and-tools": "Agents and Tools",
+  "suspend-and-resume": "Suspend & Resume",
   "pausing-execution": "Sleep & Events",
   "error-handling": "Error Handling",
   "input-data-mapping": "Input Data Mapping",
