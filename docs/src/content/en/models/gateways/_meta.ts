1b26fb4be154f00470c3ce3f194daf78c24c12dd
Tyler Barnes <tylerdbarnes@gmail.com>
2025-10-22 15:14:29 -0700

    prettier

--- docs/src/content/en/models/gateways/_meta.ts
+++ docs/src/content/en/models/gateways/_meta.ts
@@ -1,5 +1,8 @@
 const meta = {
-  index: "Overview",  netlify: "Netlify",  openrouter: "OpenRouter",  vercel: "Vercel",
+  index: "Overview",
+  netlify: "Netlify",
+  openrouter: "OpenRouter",
+  vercel: "Vercel",
 };
 
 export default meta;
