4b4eeb6793d69ce2334bab6e025690fa184b3c8c
Kenny <adelekekehinde06@gmail.com>
2025-10-22 12:01:59 +0100

    feat: add custom components

--- docs-new/src/theme/MDXComponents.tsx
+++ docs-new/src/theme/MDXComponents.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import GithubLink from '@site/src/components/GithubLink';
-import MastraCloudCallout from '@site/src/components/MastraCloudCallout';
 import NetlifyLogo from '@site/src/components/NetlifyLogo';
+import OperatorsTable from '@site/src/components/OperatorsTable';
 import ProviderModelsTable from '@site/src/components/ProviderModelsTable';
 import PropertiesTable from '@site/src/components/PropertiesTable';
 import { CardGrid, CardGridItem } from '@site/src/components/CardGrid';
@@ -10,8 +10,8 @@ import { CardGrid, CardGridItem } from '@site/src/components/CardGrid';
 export default {
   ...MDXComponents,
   GithubLink,
-  MastraCloudCallout,
   NetlifyLogo,
+  OperatorsTable,
   ProviderModelsTable,
   PropertiesTable,
   CardGrid,
