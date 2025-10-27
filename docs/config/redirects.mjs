2f31348da8b52df71989643b25f11e28013c108e
Akuya Ekorot <akuyaekorot@gmail.com>
2025-10-27 10:23:55 +0300

    docs(fix): move mongodb example from /examples to /docs

--- docs/config/redirects.mjs
+++ docs/config/redirects.mjs
@@ -267,6 +267,11 @@ export const redirectList = [
     permanent: true,
   },
   {
+    source: "/:locale/examples/memory/memory-with-mongodb",
+    destination: "/:locale/docs/memory/storage/memory-with-mongodb",
+    permanent: true,
+  },
+  {
     source: "/:locale/examples/memory/short-term-working-memory",
     destination: "/:locale/docs/memory/storage/memory-with-libsql",
     permanent: true,
