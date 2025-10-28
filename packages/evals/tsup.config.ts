205f8cff98cc38d466ff22b49a644c69a1459ccb
Yujohn Nattrass <yujohn.natt@gmail.com>
2025-10-28 07:43:53 -0700

    update tsup config for evals package

--- packages/evals/tsup.config.ts
+++ packages/evals/tsup.config.ts
@@ -2,7 +2,11 @@ import { generateTypes } from '@internal/types-builder';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/scorers/llm/index.ts', 'src/scorers/code/index.ts', 'src/scorers/utils.ts'],
+  entry: {
+    'scorers/llm/index': 'src/scorers/llm/index.ts',
+    'scorers/code/index': 'src/scorers/code/index.ts',
+    'scorers/utils': 'src/scorers/utils.ts',
+  },
   format: ['esm', 'cjs'],
   clean: true,
   dts: false,
