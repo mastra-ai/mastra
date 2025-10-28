ced06093cfc0c2324cb588dd5abac54c4a171fc1
Kenny <adelekekehinde06@gmail.com>
2025-10-28 18:48:54 +0100

    chore: remove docs-new

--- docs-new/src/components/AudioPlayback.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface AudioPlaybackProps {
-  audio: string;
-}
-
-export const AudioPlayback = ({ audio }: AudioPlaybackProps) => {
-  return <audio src={audio} controls />;
-};
