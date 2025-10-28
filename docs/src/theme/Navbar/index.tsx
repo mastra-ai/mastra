6315602906f4f14b819d699892f5aa9f3810793c
Kenny <adelekekehinde06@gmail.com>
2025-10-28 15:26:02 +0100

    fix: navbar

--- docs/src/theme/Navbar/index.tsx
+++ docs/src/theme/Navbar/index.tsx
@@ -7,22 +7,26 @@ import { type ReactNode } from "react";
 import SearchContainer from "./Search";
 import { Logo } from "./logo";
 import { TabSwitcher } from "./tab-switcher";
+import { MobileDocsDropdown } from "@site/src/components/mobile-docs-dropdown";
 
 function NavbarContentDesktop() {
   return (
     <>
-      <div className="flex px-4 border-b-[0.5px] h-[47px] border-(--border-subtle) mx-auto w-full items-center justify-between">
+      <div className="flex px-4 border-b-[0.5px] h-[47px] border-(--border-subtle) mx-auto w-full items-center justify-between @container">
         <div className="flex gap-2 items-center">
           <Link href="/docs">
             <Logo />
           </Link>
-          <div className="hidden lg:block">
+          <div className="hidden @[1262px]:block">
             <TabSwitcher />
+          </div>
+          <div className="w-[200px] hidden @[1023px]:block @[1262px]:hidden">
+            <MobileDocsDropdown />
           </div>
         </div>
 
         <div className="flex gap-2 items-center">
-          <div className="hidden lg:block">
+          <div className="hidden @[798px]:block">
             <SearchContainer locale="en" />
           </div>
           <div className="flex gap-4 items-center">
