0133cd05ec05b53b033d8dd10ca675378578b935
Kenny <adelekekehinde06@gmail.com>
2025-10-29 10:26:09 +0100

    fix: build error

--- docs/src/theme/MDXComponents.tsx
+++ docs/src/theme/MDXComponents.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import MDXComponents from "@theme-original/MDXComponents";
 import GithubLink from "@site/src/components/GithubLink";
 import NetlifyLogo from "@site/src/components/NetlifyLogo";
@@ -6,6 +5,7 @@ import OperatorsTable from "@site/src/components/OperatorsTable";
 import ProviderModelsTable from "@site/src/components/ProviderModelsTable";
 import PropertiesTable from "@site/src/components/PropertiesTable";
 import { CardGrid, CardGridItem } from "@site/src/components/CardGrid";
+import YouTube from "@site/src/components/YouTube-player";
 
 export default {
   ...MDXComponents,
@@ -16,4 +16,5 @@ export default {
   PropertiesTable,
   CardGrid,
   CardGridItem,
+  YouTube,
 };
