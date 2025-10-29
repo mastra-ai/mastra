089df724effcfec618a51bc3aea6f1552a8728a7
Kenny <adelekekehinde06@gmail.com>
2025-10-29 09:36:50 +0100

    fix: path and svg properties fix

--- docs/src/components/search-wrapper-mobile.tsx
+++ docs/src/components/search-wrapper-mobile.tsx
@@ -26,9 +26,9 @@ function SearchIcon() {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="size-4"
     >
       <path d="m21 21-4.34-4.34" />
