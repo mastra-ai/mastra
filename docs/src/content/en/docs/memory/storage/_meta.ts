6fd0dd6511af4298c7b5a959d7a227a8999361f5
wardpeet <ward@coding-tech.com>
2025-10-29 15:48:59 +0100

    move main to 0.x

--- docs/src/content/en/docs/memory/storage/_meta.ts
+++
@@ -1,8 +0,0 @@
-const meta = {
-  "memory-with-libsql": "Memory with LibSQL",
-  "memory-with-mongodb": "Memory with MongoDB",
-  "memory-with-pg": "Memory with PostgreSQL",
-  "memory-with-upstash": "Memory with Upstash",
-};
-
-export default meta;
