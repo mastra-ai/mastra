71f9338575d512ddd4e8db26aa30434c8e111895
Kenny <adelekekehinde06@gmail.com>
2025-10-30 15:00:18 +0100

    fix: hover card

--- docs/src/components/cards/card-grid.tsx
+++ docs/src/components/cards/card-grid.tsx
@@ -39,9 +39,9 @@ export const CardGridItem = ({
   return (
     <Link
       to={href}
-      className="block no-underline! dark:text-white! h-full w-full"
+      className="block no-underline! text-black! dark:text-white! h-full w-full"
     >
-      <Card className="h-full w-full shadow-none bg-(--mastra-surface-1)/20 dark:border-(--border) border-(--border) hover:bg-gray-50 dark:hover:bg-gray-900 transition-colors cursor-pointer">
+      <Card className="h-full w-full shadow-none hover:bg-(--mastra-surface-1)/70 bg-(--mastra-surface-1)/20 dark:border-(--border) border-(--border) dark:hover:bg-(--mastra-surface-2) transition-colors cursor-pointer">
         <CardHeader>
           <div className="flex items-center gap-3">
             {logo &&
