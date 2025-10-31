a5148657f8092f515c1567f9508b6e91157f950d
LekoArts <lekoarts@gmail.com>
2025-10-31 11:46:52 +0100

    allow mastra class export from top-level

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,1 @@
-throw new Error(`No exports are available from the top-level "@mastra/core" package.
-
-Use specific subpath imports instead, e.g. "@mastra/core/agent" or "@mastra/core/memory".`);
+export { Mastra, type Config } from './mastra';
