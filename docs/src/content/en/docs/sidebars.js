9e8bae5297f67bd6d73a7dc2cc35ca28c2383d5c
Alex Booker <booker@booker.codes>
2025-10-31 10:48:41 +0000

    rename playground to studio

--- docs/src/content/en/docs/sidebars.js
+++ docs/src/content/en/docs/sidebars.js
@@ -29,7 +29,7 @@ const sidebars = {
         {
           type: "doc",
           id: "getting-started/studio",
-          label: "Playground",
+          label: "Studio",
         },
         {
           type: "doc",
