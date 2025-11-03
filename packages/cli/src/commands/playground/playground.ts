1490ffecbbaa755f5f46c1373b2c48f57922b490
mfrachet <marvin@mastra.ai>
2025-11-03 13:46:19 -0800

    better error

--- packages/cli/src/commands/playground/playground.ts
+++ packages/cli/src/commands/playground/playground.ts
@@ -30,32 +30,8 @@ export async function playground(options: PlaygroundOptions = {}) {
 
     server.on('exit', code => {
       if (code !== 0 && stderrBuffer) {
-        if (stderrBuffer.includes('ERR_MODULE_NOT_FOUND')) {
-          const packageNameMatch = stderrBuffer.match(/Cannot find package '([^']+)'/);
-          const packageName = packageNameMatch ? packageNameMatch[1] : null;
+        logger.error(stderrBuffer.trim());
 
-          if (!packageName) {
-            logger.error(stderrBuffer.trim());
-          } else {
-            logger.error(`Module \`${packageName}\` not found while starting the Mastra server.
-This usually indicates that a transitive dependency could not be bundled correctly during the build process.
-Try adding \`${packageName}\` to your externals:
-
-export const mastra = new Mastra({
-  bundler: {
-    externals: ["${packageName}"],
-  }
-})
-
-If this doesn't resolve the issue, investigate the dependencies you added to your package.json as one of them might use \`${packageName}\` internally. Add that particular dependency to the externals instead. Also consider opening an issue.
-
-Original error:
-
-${stderrBuffer.trim()}`);
-          }
-        } else {
-          logger.error(stderrBuffer.trim());
-        }
         process.exit(code);
       }
     });
