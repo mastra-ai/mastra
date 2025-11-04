bf9c6113e74df54296393210e929a0d6c63a7285
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2025-11-04 15:19:44 +0000

    remove default value

--- packages/playground-ui/src/components/ui/autoform/components/SelectField.tsx
+++ packages/playground-ui/src/components/ui/autoform/components/SelectField.tsx
@@ -18,7 +18,6 @@ export const SelectField: React.FC<AutoFormFieldProps> = ({ field, inputProps, e
         } as React.ChangeEvent<HTMLInputElement>;
         props.onChange(syntheticEvent);
       }}
-      defaultValue={field.default}
     >
       <SelectTrigger id={id} className={error ? 'border-destructive' : ''}>
         <SelectValue placeholder="Select an option" />
