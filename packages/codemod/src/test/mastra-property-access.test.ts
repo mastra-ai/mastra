9935456cd8d7f83942e7370c975adefd87bf3267
LekoArts <lekoarts@gmail.com>
2025-11-05 09:32:20 -0800

    rename

--- packages/codemod/src/test/mastra-property-access.test.ts
+++ packages/codemod/src/test/mastra-property-access.test.ts
@@ -1,9 +1,9 @@
 import { describe, it } from 'vitest';
-import transformer from '../codemods/v1/property-access';
+import transformer from '../codemods/v1/mastra-property-access';
 import { testTransform } from './test-utils';
 
 describe('property-access', () => {
   it('transforms correctly', () => {
-    testTransform(transformer, 'property-access');
+    testTransform(transformer, 'mastra-property-access');
   });
 });
