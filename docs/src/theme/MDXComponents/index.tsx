7aacaa542fe465883a0377f3483114c9bc704367
Paul Scanlon <pauliescanlon@gmail.com>
2025-11-10 11:26:44 +0000

    test: add import back to MDXComponents

--- docs/src/theme/MDXComponents/index.tsx
+++ docs/src/theme/MDXComponents/index.tsx
@@ -17,7 +17,7 @@ import BrowserOnly from "@docusaurus/BrowserOnly";
 import GithubLink from "@site/src/components/GithubLink";
 import NetlifyLogo from "@site/src/components/NetlifyLogo";
 import OperatorsTable from "@site/src/components/OperatorsTable";
-
+import ProviderModelsTable from "@site/src/components/ProviderModelsTable";
 import PropertiesTable from "@site/src/components/PropertiesTable";
 import { CardGrid, CardGridItem } from "@site/src/components/CardGrid";
 import YouTube from "@site/src/components/YouTube-player";
