724a58cf3bc473f8d9791264859e7e0ec22e7d49
dayo <ifedayoadeniyi@gmail.com>
2025-11-12 17:06:59 +0100

    fix

--- docs/src/components/ui/conversation.tsx
+++ docs/src/components/ui/conversation.tsx
@@ -79,10 +79,10 @@ export const ConversationScrollButton = ({
   }, [scrollToBottom]);
 
   return (
-    // !isAtBottom && (
       <Button
         className={cn(
           "absolute bottom-4 left-[50%] translate-x-[-50%] rounded-full",
+          isAtBottom && "hidden",
           className,
         )}
         onClick={handleScrollToBottom}
@@ -93,6 +93,5 @@ export const ConversationScrollButton = ({
       >
         <ArrowDownIcon className="size-4" />
       </Button>
-    // )
   );
 };
