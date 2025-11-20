7066e92e159e4102aa4e7e42bdec89e34d30b97c
Tyler Barnes <tylerdbarnes@gmail.com>
2025-11-20 11:36:41 -0800

    update snapshots

--- packages/core/src/loop/test-utils/resultObject.ts
+++ packages/core/src/loop/test-utils/resultObject.ts
@@ -305,8 +305,7 @@ export function resultObjectTests({ loopFn, runId }: { loopFn: typeof loop; runI
             {
               "id": "msg-0",
               "metadata": {
-                "__originalContent": "Hello",
-                "createdAt": 2024-01-01T00:00:00.001Z,
+                "createdAt": 2024-01-01T00:00:00.000Z,
               },
               "parts": [
                 {
@@ -742,8 +741,7 @@ export function resultObjectTests({ loopFn, runId }: { loopFn: typeof loop; runI
                 {
                   "id": "msg-0",
                   "metadata": {
-                    "__originalContent": "Hello!",
-                    "createdAt": 2024-01-01T00:00:00.003Z,
+                    "createdAt": 2024-01-01T00:00:00.002Z,
                   },
                   "parts": [
                     {
