1963e6603addc4cb9daf508e76fa95f8c41a2036
wardpeet <ward@coding-tech.com>
2025-11-21 15:50:00 +0100

    Improve module validation when bundling

--- packages/deployer/src/validator/validate.ts
+++ packages/deployer/src/validator/validate.ts
@@ -3,6 +3,25 @@ import type { SpawnOptions } from 'node:child_process';
 import { dirname } from 'node:path';
 import { slash } from '../build/utils';
 
+type ValidationArgs = {
+  message: string;
+  type: string;
+  stack: string;
+  info: Record<string, unknown>;
+};
+
+export class ValidationError extends Error {
+  public readonly type: string;
+  public readonly stack: string;
+  public readonly info: Record<string, unknown>;
+  constructor(args: ValidationArgs) {
+    super(args.message);
+    this.type = args.type;
+    this.stack = args.stack;
+    this.info = args.info;
+  }
+}
+
 /**
  * Promisified version of Node.js spawn function
  *
@@ -13,6 +32,7 @@ import { slash } from '../build/utils';
  */
 function spawn(command: string, args: string[] = [], options: SpawnOptions = {}): Promise<void> {
   return new Promise((resolve, reject) => {
+    let validationError: ValidationArgs | null = null;
     const childProcess = nodeSpawn(command, args, {
       // stdio: 'inherit',
       ...options,
@@ -24,20 +44,65 @@ function spawn(command: string, args: string[] = [], options: SpawnOptions = {})
 
     let stderr = '';
     childProcess.stderr?.on('data', message => {
-      stderr += message;
+      try {
+        validationError = JSON.parse(message.toString());
+      } catch {
+        stderr += message;
+      }
     });
 
     childProcess.on('close', code => {
       if (code === 0) {
         resolve();
       } else {
-        reject(new Error(stderr));
+        if (validationError) {
+          reject(new ValidationError(validationError));
+        } else {
+          reject(new Error(stderr));
+        }
       }
     });
   });
 }
 
-export function validate(file: string) {
+export function validate(file: string, injectESMShim = false) {
+  let prefixCode = '';
+  if (injectESMShim) {
+    prefixCode = `import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+globalThis.__filename = fileURLToPath(import.meta.url);
+globalThis.__dirname = dirname(__filename);
+    `;
+  }
+
+  // Used to log a proper error we can parse instead of trying to do some fancy string grepping
+  function errorHandler(err: Error, file: string) {
+    if (err.stack?.includes?.('[ERR_MODULE_NOT_FOUND]')) {
+      const moduleName = err.message.match(/Cannot find package '([^']+)'/)?.[1];
+      console.error(
+        JSON.stringify({
+          type: 'ModuleNotFoundError',
+          message: err.message,
+          info: {
+            moduleName,
+            sourceFile: file,
+          },
+          stack: err.stack,
+        }),
+      );
+    } else {
+      console.error(
+        JSON.stringify({
+          type: err.name,
+          message: err.message,
+          stack: err.stack,
+        }),
+      );
+    }
+    process.exit(1);
+  }
+
   return spawn(
     'node',
     [
@@ -45,7 +110,10 @@ export function validate(file: string) {
       import.meta.resolve('@mastra/deployer/loader'),
       '--input-type=module',
       '-e',
-      `import('file://${slash(file)}')`,
+      `${prefixCode};import('file://${slash(file)}').catch(err => {
+        ${errorHandler.toString()}
+        errorHandler(err, "${file}");
+      })`.replaceAll(/\n/g, ''),
     ],
     {
       cwd: dirname(file),
