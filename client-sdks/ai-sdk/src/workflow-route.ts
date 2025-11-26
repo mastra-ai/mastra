DevZonayed/nexalance-skill-mastra-ai-mastra

Fix: Forward request context set in middleware to workflows in workflowRoute (#10427)
Co-authored-by: Abhi Aiyer <abhiaiyer91@gmail.com>
Co-authored-by: Ward Peeters <ward@coding-tech.com>

client-sdks/ai-sdk/src/workflow-route.ts
@@ -74,6 +74,7 @@ export function workflowRoute({
     handler: async c => {
       const { runId, resourceId, inputData, resumeData, ...rest } = (await c.req.json()) as WorkflowRouteBody;
       const mastra = c.get('mastra');
+      const requestContext = (c as any).get('requestContext') as RequestContext | undefined;
 
       let workflowToUse: string | undefined = workflow;
       if (!workflow) {
@@ -97,9 +98,19 @@ export function workflowRoute({
         throw new Error(`Workflow ${workflowToUse} not found`);
       }
 
+      if (requestContext && rest.requestContext) {
+        mastra
+          .getLogger()
+          ?.warn(
+            `"requestContext" from the request body will be ignored because "requestContext" is already set in the route options.`,
+          );
+      }
+
       const run = await workflowObj.createRun({ runId, resourceId, ...rest });
 
-      const stream = resumeData ? run.resumeStream({ resumeData, ...rest }) : run.stream({ inputData, ...rest });
+      const stream = resumeData
+        ? run.resumeStream({ resumeData, ...rest, requestContext: requestContext || rest.requestContext })
+        : run.stream({ inputData, ...rest, requestContext: requestContext || rest.requestContext });
 
       const uiMessageStream = createUIMessageStream({
         execute: async ({ writer }) => {

mastra-ai/mastra

(cherry picked from commit ee6c628fabfb18323bee319268d122f3835e393f)

client-sdks/ai-sdk/src/workflow-route.ts
@@ -74,6 +74,7 @@ export function workflowRoute({
     handler: async c => {
       const { runId, resourceId, inputData, resumeData, ...rest } = (await c.req.json()) as WorkflowRouteBody;
       const mastra = c.get('mastra');
+      const runtimeContext = (c as any).get('runtimeContext') as RuntimeContext | undefined;
 
       let workflowToUse: string | undefined = workflow;
       if (!workflow) {
@@ -97,9 +98,19 @@ export function workflowRoute({
         throw new Error(`Workflow ${workflowToUse} not found`);
       }
 
+      if (runtimeContext && rest.runtimeContext) {
+        mastra
+          .getLogger()
+          ?.warn(
+            `"runtimeContext" from the request body will be ignored because "runtimeContext" is already set in the route options.`,
+          );
+      }
+
       const run = await workflowObj.createRunAsync({ runId, resourceId, ...rest });
 
-      const stream = resumeData ? run.resumeStream({ resumeData, ...rest }) : run.stream({ inputData, ...rest });
+      const stream = resumeData
+        ? run.resumeStream({ resumeData, ...rest, runtimeContext: runtimeContext || rest.runtimeContext })
+        : run.stream({ inputData, ...rest, runtimeContext: runtimeContext || rest.runtimeContext });
 
       const uiMessageStream = createUIMessageStream({
         execute: async ({ writer }) => {