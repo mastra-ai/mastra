DevZonayed/nexalance-skill-mastra-ai-mastra

fix(agent): persist messages before tool suspension (#9745, #9906) (#10369)
## Description

<!-- Provide a brief description of the changes in this PR -->
**Issue #9745:** When a tool has `requireApproval: true`, the thread and
messages are not saved until after approval/decline. If a user refreshes
before approving, the conversation disappears.

**Issue #9906:** When a tool calls `suspend()` during execution,
messages are not persisted even with `savePerStep: true`, causing loss
of audit trail for backend actions.

This PR fixes these issues by persisting messages before suspension and
enabling tool approval state to survive page refreshes.

**Backend changes (@mastra/core):**
- Add assistant messages to messageList immediately after LLM execution
- Flush messages synchronously before suspension to persist state
- Create thread if it doesn't exist before flushing
- Add metadata helpers to persist and remove tool approval state
- Pass saveQueueManager and memory context through workflow for
immediate persistence

**Frontend changes (@mastra/react):**
- Extract runId from pending approvals to enable resumption after
refresh
- Convert `pendingToolApprovals` (DB format) to
`requireApprovalMetadata` (runtime format)
- Handle both `dynamic-tool` and `tool-{NAME}` part types for approval
state
- Change runId from hardcoded `agentId` to unique `uuid()`

**UI changes (@mastra/playground-ui):**
- Handle tool calls awaiting approval in message initialization
- Convert approval metadata format when loading initial messages

## Related Issue(s)

<!-- Link to the issue(s) this PR addresses, using hashtag notation:
#123 -->

Fixes #9745
Fixes #9906

## Type of Change

- [x] Bug fix (non-breaking change that fixes an issue)
- [ ] New feature (non-breaking change that adds functionality)
- [ ] Breaking change (fix or feature that would cause existing
functionality to change)
- [ ] Documentation update
- [ ] Code refactoring
- [ ] Performance improvement
- [ ] Test update

## Checkl

packages/core/src/loop/workflows/agentic-execution/tool-call-step.ts
@@ -1,4 +1,5 @@
 import type { ToolSet } from 'ai-v5';
+import type { MastraDBMessage } from '../../../memory';
 import type { OutputSchema } from '../../../stream/base/schema';
 import { ChunkFrom } from '../../../stream/types';
 import type { MastraToolInvocationOptions } from '../../../tools/types';
@@ -18,12 +19,118 @@ export function createToolCallStep<
   runId,
   streamState,
   modelSpanTracker,
+  _internal,
 }: OuterLLMRun<Tools, OUTPUT>) {
   return createStep({
     id: 'toolCallStep',
     inputSchema: toolCallInputSchema,
     outputSchema: toolCallOutputSchema,
     execute: async ({ inputData, suspend, resumeData, requestContext }) => {
+      // Helper function to add tool approval metadata to the assistant message
+      const addToolApprovalMetadata = (toolCallId: string, toolName: string, args: unknown) => {
+        // Find the last assistant message in the response (which should contain this tool call)
+        const responseMessages = messageList.get.response.db();
+        const lastAssistantMessage = [...responseMessages].reverse().find(msg => msg.role === 'assistant');
+
+        if (lastAssistantMessage) {
+          const content = lastAssistantMessage.content;
+          if (!content) return;
+          // Add metadata to indicate this tool call is pending approval
+          const metadata =
+            typeof lastAssistantMessage.content.metadata === 'object' && lastAssistantMessage.content.metadata !== null
+              ? (lastAssistantMessage.content.metadata as Record<string, any>)
+              : {};
+          metadata.pendingToolApprovals = metadata.pendingToolApprovals || {};
+          metadata.pendingToolApprovals[toolCallId] = {
+            toolName,
+            args,
+            type: 'approval',
+            runId, // Store the runId so we can resume after page refresh
+          };
+          lastAssistantMessage.content.metadata = metadata;
+        }
+      };
+
+      // Helper function to remove tool approval metadata after approval/decline
+      const removeToolApprovalMetadata = async (toolCallId: string) => {
+        const { saveQueueManager, memoryConfig, threadId } = _internal || {};
+
+        if (!saveQueueManager || !threadId) {
+          return;
+        }
+
+        const getMetadata = (message: MastraDBMessage) => {
+          const content = message.content;
+          if (!content) return undefined;
+          const metadata =
+            typeof content.metadata === 'object' && content.metadata !== null
+              ? (content.metadata as Record<string, any>)
+              : undefined;
+          return metadata;
+        };
+
+        // Find and update the assistant message to remove approval metadata
+        // At this point, messages have been persisted, so we look in all messages
+        const allMessages = messageList.get.all.db();
+        const lastAssistantMessage = [...allMessages].reverse().find(msg => {
+          const metadata = getMetadata(msg);
+          const pendingToolApprovals = metadata?.pendingToolApprovals as Record<string, any> | undefined;
+          return !!pendingToolApprovals?.[toolCallId];
+        });
+
+        if (lastAssistantMessage) {
+          const metadata = getMetadata(lastAssistantMessage);
+          const pendingToolApprovals = metadata?.pendingToolApprovals as Record<string, any> | undefined;
+
+          if (pendingToolApprovals && typeof pendingToolApprovals === 'object') {
+            delete pendingToolApprovals[toolCallId];
+
+            // If no more pending suspensions, remove the whole object
+            if (metadata && Object.keys(pendingToolApprovals).length === 0) {
+              delete metadata.pendingToolApprovals;
+            }
+
+            // Flush to persist the metadata removal
+            try {
+              await saveQueueManager.flushMessages(messageList, threadId, memoryConfig);
+            } catch (error) {
+              console.error('Error removing tool approval metadata:', error);
+            }
+          }
+        }
+      };
+
+      // Helper function to flush messages before suspension
+      const flushMessagesBeforeSuspension = async () => {
+        const { saveQueueManager, memoryConfig, threadId, resourceId, memory } = _internal || {};
+
+        if (!saveQueueManager || !threadId) {
+          console.warn('flushMessagesBeforeSuspension: saveQueueManager or threadId is missing');
+          return;
+        }
+
+        try {
+          // Ensure thread exists before flushing messages
+          if (memory && !_internal.threadExists && resourceId) {
+            const thread = await memory.getThreadById?.({ threadId });
+            if (!thread) {
+              // Thread doesn't exist yet, create it now
+              await memory.createThread?.({
+                threadId,
+                resourceId,
+                memoryConfig,
+              });
+            }
+            _internal.threadExists = true;
+          }
+
+          // Flush all pending messages immediately
+          await saveQueueManager.flushMessages(messageList, threadId, memoryConfig);
+        } catch (error) {
+          console.error('Error flushing messages before suspension:', error);
+        }
+      };
+
       // If the tool was already executed by the provider, skip execution
       if (inputData.providerExecuted) {
         return {
@@ -71,6 +178,13 @@ export function createToolCallStep<
                 args: inputData.args,
               },
             });
+
+            // Add approval metadata to message before persisting
+            addToolApprovalMetadata(inputData.toolCallId, inputData.toolName, inputData.args);
+
+            // Flush messages before suspension to ensure they are persisted
+            await flushMessagesBeforeSuspension();
+
             return suspend(
               {
                 requireToolApproval: {
@@ -85,6 +199,9 @@ export function createToolCallStep<
               },
             );
           } else {
+            // Remove approval metadata since we're resuming (either approved or declined)
+            await removeToolApprovalMetadata(inputData.toolCallId);
+
             if (!resumeData.approved) {
               return {
                 result: 'Tool call was not approved by the user',
@@ -108,6 +225,9 @@ export function createToolCallStep<
               from: ChunkFrom.AGENT,
               payload: { toolCallId: inputData.toolCallId, toolName: inputData.toolName, suspendPayload },
             });
+
+            // Flush messages before suspension to ensure they are persisted
+            await flushMessagesBeforeSuspension();
 
             return await suspend(
               {

mastra-ai/mastra

packages/core/src/loop/workflows/agentic-execution/tool-call-step.ts
@@ -1,4 +1,5 @@
 import type { ToolSet } from 'ai-v5';
+import type { MastraMessageV2 } from '../../../memory';
 import type { OutputSchema } from '../../../stream/base/schema';
 import { ChunkFrom } from '../../../stream/types';
 import type { MastraToolInvocationOptions } from '../../../tools/types';
@@ -20,12 +21,118 @@ export function createToolCallStep<
   runId,
   streamState,
   modelSpanTracker,
+  _internal,
 }: OuterLLMRun<Tools, OUTPUT>) {
   return createStep({
     id: 'toolCallStep',
     inputSchema: toolCallInputSchema,
     outputSchema: toolCallOutputSchema,
     execute: async ({ inputData, suspend, resumeData, runtimeContext }) => {
+      // Helper function to add tool approval metadata to the assistant message
+      const addToolApprovalMetadata = (toolCallId: string, toolName: string, args: unknown) => {
+        // Find the last assistant message in the response (which should contain this tool call)
+        const responseMessages = messageList.get.response.v2();
+        const lastAssistantMessage = [...responseMessages].reverse().find(msg => msg.role === 'assistant');
+
+        if (lastAssistantMessage) {
+          const content = lastAssistantMessage.content;
+          if (!content) return;
+          // Add metadata to indicate this tool call is pending approval
+          const metadata =
+            typeof lastAssistantMessage.content.metadata === 'object' && lastAssistantMessage.content.metadata !== null
+              ? (lastAssistantMessage.content.metadata as Record<string, any>)
+              : {};
+          metadata.pendingToolApprovals = metadata.pendingToolApprovals || {};
+          metadata.pendingToolApprovals[toolCallId] = {
+            toolName,
+            args,
+            type: 'approval',
+            runId, // Store the runId so we can resume after page refresh
+          };
+          lastAssistantMessage.content.metadata = metadata;
+        }
+      };
+
+      // Helper function to remove tool approval metadata after approval/decline
+      const removeToolApprovalMetadata = async (toolCallId: string) => {
+        const { saveQueueManager, memoryConfig, threadId } = _internal || {};
+
+        if (!saveQueueManager || !threadId) {
+          return;
+        }
+
+        const getMetadata = (message: MastraMessageV2) => {
+          const content = message.content;
+          if (!content) return undefined;
+          const metadata =
+            typeof content.metadata === 'object' && content.metadata !== null
+              ? (content.metadata as Record<string, any>)
+              : undefined;
+          return metadata;
+        };
+
+        // Find and update the assistant message to remove approval metadata
+        // At this point, messages have been persisted, so we look in all messages
+        const allMessages = messageList.get.all.v2();
+        const lastAssistantMessage = [...allMessages].reverse().find(msg => {
+          const metadata = getMetadata(msg);
+          const pendingToolApprovals = metadata?.pendingToolApprovals as Record<string, any> | undefined;
+          return !!pendingToolApprovals?.[toolCallId];
+        });
+
+        if (lastAssistantMessage) {
+          const metadata = getMetadata(lastAssistantMessage);
+          const pendingToolApprovals = metadata?.pendingToolApprovals as Record<string, any> | undefined;
+
+          if (pendingToolApprovals && typeof pendingToolApprovals === 'object') {
+            delete pendingToolApprovals[toolCallId];
+
+            // If no more pending suspensions, remove the whole object
+            if (metadata && Object.keys(pendingToolApprovals).length === 0) {
+              delete metadata.pendingToolApprovals;
+            }
+
+            // Flush to persist the metadata removal
+            try {
+              await saveQueueManager.flushMessages(messageList, threadId, memoryConfig);
+            } catch (error) {
+              console.error('Error removing tool approval metadata:', error);
+            }
+          }
+        }
+      };
+
+      // Helper function to flush messages before suspension
+      const flushMessagesBeforeSuspension = async () => {
+        const { saveQueueManager, memoryConfig, threadId, resourceId, memory } = _internal || {};
+
+        if (!saveQueueManager || !threadId) {
+          console.warn('flushMessagesBeforeSuspension: saveQueueManager or threadId is missing');
+          return;
+        }
+
+        try {
+          // Ensure thread exists before flushing messages
+          if (memory && !_internal.threadExists && resourceId) {
+            const thread = await memory.getThreadById?.({ threadId });
+            if (!thread) {
+              // Thread doesn't exist yet, create it now
+              await memory.createThread?.({
+                threadId,
+                resourceId,
+                memoryConfig,
+              });
+            }
+            _internal.threadExists = true;
+          }
+
+          // Flush all pending messages immediately
+          await saveQueueManager.flushMessages(messageList, threadId, memoryConfig);
+        } catch (error) {
+          console.error('Error flushing messages before suspension:', error);
+        }
+      };
+
       // If the tool was already executed by the provider, skip execution
       if (inputData.providerExecuted) {
         // Still emit telemetry for provider-executed tools
@@ -114,6 +221,13 @@ export function createToolCallStep<
                 args: inputData.args,
               },
             });
+
+            // Add approval metadata to message before persisting
+            addToolApprovalMetadata(inputData.toolCallId, inputData.toolName, inputData.args);
+
+            // Flush messages before suspension to ensure they are persisted
+            await flushMessagesBeforeSuspension();
+
             return suspend(
               {
                 requireToolApproval: {
@@ -128,6 +242,9 @@ export function createToolCallStep<
               },
             );
           } else {
+            // Remove approval metadata since we're resuming (either approved or declined)
+            await removeToolApprovalMetadata(inputData.toolCallId);
+
             if (!resumeData.approved) {
               span.end();
               span.setAttributes({
@@ -155,6 +272,9 @@ export function createToolCallStep<
               from: ChunkFrom.AGENT,
               payload: { toolCallId: inputData.toolCallId, toolName: inputData.toolName, suspendPayload },
             });
+
+            // Flush messages before suspension to ensure they are persisted
+            await flushMessagesBeforeSuspension();
 
             return await suspend(
               {