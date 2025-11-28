016142d363e7fb0fed69fe28a778a2bb0176a55e
Kenny <adelekekehinde06@gmail.com>
2025-11-29 00:00:49 +0100

    feat: add tags and sidebar related components

---
+++ docs/src/components/SidebarBadge.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './SidebarBadge.module.css';
+
+type BadgeType = 'new' | 'advanced' | 'experimental';
+
+interface SidebarBadgeProps {
+  type: BadgeType;
+}
+
+export function SidebarBadge({ type }: SidebarBadgeProps) {
+  const getLabel = (type: BadgeType) => {
+    switch (type) {
+      case 'new':
+        return 'NEW';
+      case 'advanced':
+        return 'ADVANCED';
+      case 'experimental':
+        return 'EXPERIMENTAL';
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <span className={`${styles.badge} ${styles[`badge--${type}`]}`}>
+      {getLabel(type)}
+    </span>
+  );
+}
+
+export default SidebarBadge;
