DevZonayed/nexalance-skill-mastra-ai-mastra

timeTravel workflows (#9994)
## Description

<!-- Provide a brief description of the changes in this PR -->

Add timeTravel to workflows. This makes it possible to start a workflow
run from a particular step in the workflow

Example code:
```ts
const result = await run.timeTravel({
  step: "step2",
  inputData: {
    value: "input"
  }
})
```

Also makes it possible to resume a suspended workflow from a previous
step

Example code:
```ts
const result = await run.timeTravel({
  step: "stepBeforeSuspendedStep",
  resumeData: {
    resumeInfo: "resume"
  }
})
```


## Related Issue(s)

<!-- Link to the issue(s) this PR addresses, using hashtag notation:
#123 -->

## Type of Change

- [ ] Bug fix (non-breaking change that fixes an issue)
- [x] New feature (non-breaking change that adds functionality)
- [ ] Breaking change (fix or feature that would cause existing
functionality to change)
- [ ] Documentation update
- [ ] Code refactoring
- [ ] Performance improvement
- [x] Test update

## Checklist

- [ ] I have made corresponding changes to the documentation (if
applicable)
- [x] I have added tests that prove my fix is effective or that my
feature works


<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

* **New Features**
* Time travel: run or stream a workflow from a specific step with
input/resume data, nested context support, state restoration, and
tracing/requestContext propagation; new public run APIs and types; new
workflow management helpers to list and restart active runs; optional
input validation flag for runs.

* **Tests**
* Extensive time-travel test suites covering nested, parallel, loops,
suspend/resume, agents, and streaming scenarios.

* **Chores**
  * Added a utility to remove undefined values from objects.
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

packages/core/src/workflows/evented/execution-engine.ts
@@ -3,8 +3,13 @@ import type { Event } from '../../events/types';
 import type { Mastra } from '../../mastra';
 import { ExecutionEngine } from '../../workflows/execution-engine';
 import type { ExecutionEngineOptions, ExecutionGraph } from '../../workflows/execution-engine';
-import type { RestartExecutionParams } from '../default';
-import type { Emitter, SerializedStepFlowEntry, StepResult } from '../types';
+import type {
+  Emitter,
+  SerializedStepFlowEntry,
+  StepResult,
+  RestartExecutionParams,
+  TimeTravelExecutionParams,
+} from '../types';
 import type { WorkflowEventProcessor } from './workflow-event-processor';
 import { getStep } from './workflow-event-processor/utils';
 
@@ -42,6 +47,7 @@ export class EventedExecutionEngine extends ExecutionEngine {
     serializedStepGraph: SerializedStepFlowEntry[];
     input?: TInput;
     restart?: RestartExecutionParams;
+    timeTravel?: TimeTravelExecutionParams;
     resume?: {
       steps: string[];
       stepResults: Record<string, StepResult<any, any, any, any>>;
@@ -77,6 +83,23 @@ export class EventedExecutionEngine extends ExecutionEngine {
           resumeSteps: params.resume.steps,
           prevResult: { status: 'success', output: prevResult?.payload },
           resumeData: params.resume.resumePayload,
+          requestContext: Object.fromEntries(params.requestContext.entries()),
+          format: params.format,
+        },
+      });
+    } else if (params.timeTravel) {
+      const prevStep = getStep(this.mastra!.getWorkflow(params.workflowId), params.timeTravel.executionPath);
+      const prevResult = params.timeTravel.stepResults[prevStep?.id ?? 'input'];
+      await pubsub.publish('workflows', {
+        type: 'workflow.start',
+        runId: params.runId,
+        data: {
+          workflowId: params.workflowId,
+          runId: params.runId,
+          executionPath: params.timeTravel.executionPath,
+          stepResults: params.timeTravel.stepResults,
+          timeTravel: params.timeTravel,
+          prevResult: { status: 'success', output: prevResult?.payload },
           requestContext: Object.fromEntries(params.requestContext.entries()),
           format: params.format,
         },

mastra-ai/mastra

packages/core/src/workflows/evented/execution-engine.ts
@@ -1,15 +1,15 @@
-import type {
-  Emitter,
-  ExecutionGraph,
-  SerializedStepFlowEntry,
-  StepResult,
-  Mastra,
-  ExecutionEngineOptions,
-  RestartExecutionParams,
-} from '../..';
+import type { Mastra } from '../..';
 import type { RuntimeContext } from '../../di';
 import type { Event } from '../../events/types';
+import type { ExecutionEngineOptions, ExecutionGraph } from '../../workflows/execution-engine';
 import { ExecutionEngine } from '../../workflows/execution-engine';
+import type {
+  Emitter,
+  SerializedStepFlowEntry,
+  StepResult,
+  RestartExecutionParams,
+  TimeTravelExecutionParams,
+} from '../types';
 import type { WorkflowEventProcessor } from './workflow-event-processor';
 import { getStep } from './workflow-event-processor/utils';
 
@@ -47,6 +47,7 @@ export class EventedExecutionEngine extends ExecutionEngine {
     serializedStepGraph: SerializedStepFlowEntry[];
     input?: TInput;
     restart?: RestartExecutionParams;
+    timeTravel?: TimeTravelExecutionParams;
     resume?: {
       steps: string[];
       stepResults: Record<string, StepResult<any, any, any, any>>;
@@ -82,6 +83,23 @@ export class EventedExecutionEngine extends ExecutionEngine {
           resumeSteps: params.resume.steps,
           prevResult: { status: 'success', output: prevResult?.payload },
           resumeData: params.resume.resumePayload,
+          runtimeContext: Object.fromEntries(params.runtimeContext.entries()),
+          format: params.format,
+        },
+      });
+    } else if (params.timeTravel) {
+      const prevStep = getStep(this.mastra!.getWorkflow(params.workflowId), params.timeTravel.executionPath);
+      const prevResult = params.timeTravel.stepResults[prevStep?.id ?? 'input'];
+      await pubsub.publish('workflows', {
+        type: 'workflow.start',
+        runId: params.runId,
+        data: {
+          workflowId: params.workflowId,
+          runId: params.runId,
+          executionPath: params.timeTravel.executionPath,
+          stepResults: params.timeTravel.stepResults,
+          timeTravel: params.timeTravel,
+          prevResult: { status: 'success', output: prevResult?.payload },
           runtimeContext: Object.fromEntries(params.runtimeContext.entries()),
           format: params.format,
         },