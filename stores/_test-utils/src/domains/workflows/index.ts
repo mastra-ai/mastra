DevZonayed/nexalance-skill-mastra-ai-mastra

Restart interrupted workflows (#9750)
## Description

<!-- Provide a brief description of the changes in this PR -->

- Add restart method to workflow run that allows restarting an active
workflow run
- Add status filter to `listWorkflowRuns`
- Add automatic restart to restart active workflow runs when server
starts

## Related Issue(s)

<!-- Link to the issue(s) this PR addresses, using hashtag notation:
#123 -->
#7464 

## Type of Change

- [ ] Bug fix (non-breaking change that fixes an issue)
- [x] New feature (non-breaking change that adds functionality)
- [ ] Breaking change (fix or feature that would cause existing
functionality to change)
- [ ] Documentation update
- [ ] Code refactoring
- [ ] Performance improvement
- [x] Test update

## Checklist

- [ ] I have made corresponding changes to the documentation (if
applicable)
- [x] I have added tests that prove my fix is effective or that my
feature works


<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

* **New Features**
  * Manual restart API for active workflow runs.
  * Status-based filtering when listing workflow runs.
  * Automatic restart of active workflow runs on server startup.

* **Tests**
* Added tests for restart behavior and engine-specific restart
constraints.

* **Chores**
  * Bumped multiple packages to patch versions.
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

stores/_test-utils/src/domains/workflows/index.ts
@@ -61,6 +61,25 @@ export function createWorkflowsTests({ storage }: { storage: MastraStorage }) {
       expect(snapshot.context?.[stepId1]?.status).toBe('completed');
     });
 
+    it('filters by status', async () => {
+      const workflowName1 = 'filter_test_1';
+      const workflowName2 = 'filter_test_2';
+
+      const { snapshot: workflow1, runId: runId1, stepId: stepId1 } = createSampleWorkflowSnapshot('success');
+      const { snapshot: workflow2, runId: runId2 } = createSampleWorkflowSnapshot('failed');
+
+      await storage.persistWorkflowSnapshot({ workflowName: workflowName1, runId: runId1, snapshot: workflow1 });
+      await new Promise(resolve => setTimeout(resolve, 10)); // Small delay to ensure different timestamps
+      await storage.persistWorkflowSnapshot({ workflowName: workflowName2, runId: runId2, snapshot: workflow2 });
+
+      const { runs, total } = await storage.listWorkflowRuns({ status: 'success' });
+      expect(runs).toHaveLength(1);
+      expect(total).toBe(1);
+      expect(runs[0]!.workflowName).toBe(workflowName1);
+      const snapshot = runs[0]!.snapshot as WorkflowRunState;
+      expect(snapshot.status).toBe('success');
+    });
+
     it('filters by date range', async () => {
       const now = new Date();
       const yesterday = new Date(now.getTime() - 24 * 60 * 60 * 1000);
@@ -251,15 +270,16 @@ export function createWorkflowsTests({ storage }: { storage: MastraStorage }) {
     const runId = 'test-run-id';
     const snapshot = {
       runId,
+      status: 'success' as WorkflowRunState['status'],
       value: {},
       context: {},
       activePaths: [],
+      activeStepsPath: {},
       suspendedPaths: {},
       resumeLabels: {},
       serializedStepGraph: [],
       waitingPaths: {},
       timestamp: Date.now(),
-      status: 'success' as WorkflowRunState['status'],
     };
     await storage.persistWorkflowSnapshot({
       workflowName,
@@ -282,15 +302,16 @@ export function createWorkflowsTests({ storage }: { storage: MastraStorage }) {
     const runId = 'test-run-id-2';
     const snapshot = {
       runId,
+      status: 'success' as WorkflowRunState['status'],
       value: {},
       context: {},
       activePaths: [],
+      activeStepsPath: {},
       suspendedPaths: {},
       resumeLabels: {},
       serializedStepGraph: [],
       waitingPaths: {},
       timestamp: Date.now(),
-      status: 'success' as WorkflowRunState['status'],
     };
     await storage.persistWorkflowSnapshot({
       workflowName,

stores/pg/src/storage/index.ts
@@ -343,15 +343,8 @@ export class PostgresStore extends MastraStorage {
     return this.stores.workflows.loadWorkflowSnapshot({ workflowName, runId });
   }
 
-  async listWorkflowRuns({
-    workflowName,
-    fromDate,
-    toDate,
-    perPage,
-    page,
-    resourceId,
-  }: StorageListWorkflowRunsInput = {}): Promise<WorkflowRuns> {
-    return this.stores.workflows.listWorkflowRuns({ workflowName, fromDate, toDate, perPage, page, resourceId });
+  async listWorkflowRuns(args: StorageListWorkflowRunsInput = {}): Promise<WorkflowRuns> {
+    return this.stores.workflows.listWorkflowRuns(args);
   }
 
   async getWorkflowRunById({

mastra-ai/mastra

stores/_test-utils/src/domains/workflows/index.ts
@@ -61,6 +61,25 @@ export function createWorkflowsTests({ storage }: { storage: MastraStorage }) {
       expect(snapshot.context?.[stepId1]?.status).toBe('completed');
     });
 
+    it('filters by status', async () => {
+      const workflowName1 = 'filter_test_1';
+      const workflowName2 = 'filter_test_2';
+
+      const { snapshot: workflow1, runId: runId1, stepId: stepId1 } = createSampleWorkflowSnapshot('success');
+      const { snapshot: workflow2, runId: runId2 } = createSampleWorkflowSnapshot('failed');
+
+      await storage.persistWorkflowSnapshot({ workflowName: workflowName1, runId: runId1, snapshot: workflow1 });
+      await new Promise(resolve => setTimeout(resolve, 10)); // Small delay to ensure different timestamps
+      await storage.persistWorkflowSnapshot({ workflowName: workflowName2, runId: runId2, snapshot: workflow2 });
+
+      const { runs, total } = await storage.getWorkflowRuns({ status: 'success' });
+      expect(runs).toHaveLength(1);
+      expect(total).toBe(1);
+      expect(runs[0]!.workflowName).toBe(workflowName1);
+      const snapshot = runs[0]!.snapshot as WorkflowRunState;
+      expect(snapshot.status).toBe('success');
+    });
+
     it('filters by date range', async () => {
       const now = new Date();
       const yesterday = new Date(now.getTime() - 24 * 60 * 60 * 1000);
@@ -251,15 +270,16 @@ export function createWorkflowsTests({ storage }: { storage: MastraStorage }) {
     const runId = 'test-run-id';
     const snapshot = {
       runId,
+      status: 'success' as WorkflowRunState['status'],
       value: {},
       context: {},
       activePaths: [],
+      activeStepsPath: {},
       suspendedPaths: {},
       resumeLabels: {},
       serializedStepGraph: [],
       waitingPaths: {},
       timestamp: Date.now(),
-      status: 'success' as WorkflowRunState['status'],
     };
     await storage.persistWorkflowSnapshot({
       workflowName,
@@ -282,15 +302,16 @@ export function createWorkflowsTests({ storage }: { storage: MastraStorage }) {
     const runId = 'test-run-id-2';
     const snapshot = {
       runId,
+      status: 'success' as WorkflowRunState['status'],
       value: {},
       context: {},
       activePaths: [],
+      activeStepsPath: {},
       suspendedPaths: {},
       resumeLabels: {},
       serializedStepGraph: [],
       waitingPaths: {},
       timestamp: Date.now(),
-      status: 'success' as WorkflowRunState['status'],
     };
     await storage.persistWorkflowSnapshot({
       workflowName,

stores/pg/src/storage/index.ts
@@ -21,6 +21,7 @@ import type {
   AISpanRecord,
   AITraceRecord,
   AITracesPaginatedArg,
+  StorageListWorkflowRunsInput,
 } from '@mastra/core/storage';
 import type { Trace } from '@mastra/core/telemetry';
 import type { StepResult, WorkflowRunState } from '@mastra/core/workflows';
@@ -431,22 +432,8 @@ export class PostgresStore extends MastraStorage {
     return this.stores.workflows.loadWorkflowSnapshot({ workflowName, runId });
   }
 
-  async getWorkflowRuns({
-    workflowName,
-    fromDate,
-    toDate,
-    limit,
-    offset,
-    resourceId,
-  }: {
-    workflowName?: string;
-    fromDate?: Date;
-    toDate?: Date;
-    limit?: number;
-    offset?: number;
-    resourceId?: string;
-  } = {}): Promise<WorkflowRuns> {
-    return this.stores.workflows.getWorkflowRuns({ workflowName, fromDate, toDate, limit, offset, resourceId });
+  async getWorkflowRuns(args: StorageListWorkflowRunsInput = {}): Promise<WorkflowRuns> {
+    return this.stores.workflows.getWorkflowRuns(args);
   }
 
   async getWorkflowRunById({