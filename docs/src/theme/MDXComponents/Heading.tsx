84de7372f1a2a116bc47d6db247e5b5c9dbfb06b
Benedikt Rötsch <benedikt@hashbite.net>
2025-12-02 12:19:27 +0100

    style: make prettier happy

--- docs/src/theme/MDXComponents/Heading.tsx
+++ docs/src/theme/MDXComponents/Heading.tsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import clsx from 'clsx';
-import {translate} from '@docusaurus/Translate';
-import {useAnchorTargetClassName} from '@docusaurus/theme-common';
-import Link from '@docusaurus/Link';
-import useBrokenLinks from '@docusaurus/useBrokenLinks';
+import React from "react";
+import clsx from "clsx";
+import { translate } from "@docusaurus/Translate";
+import { useAnchorTargetClassName } from "@docusaurus/theme-common";
+import Link from "@docusaurus/Link";
+import useBrokenLinks from "@docusaurus/useBrokenLinks";
 
-export default function Heading({as: As, id, ...props}) {
+export default function Heading({ as: As, id, ...props }) {
   const brokenLinks = useBrokenLinks();
   const anchorTargetClassName = useAnchorTargetClassName(id);
   // H1 headings do not need an id because they don't appear in the TOC.
-  if (As === 'h1' || !id) {
+  if (As === "h1" || !id) {
     return <As {...props} id={undefined} />;
   }
   brokenLinks.collectAnchor(id);
   const anchorTitle = translate(
     {
-      id: 'theme.common.headingLinkTitle',
-      message: 'Direct link to {heading}',
-      description: 'Title for link to heading',
+      id: "theme.common.headingLinkTitle",
+      message: "Direct link to {heading}",
+      description: "Title for link to heading",
     },
     {
-      heading: typeof props.children === 'string' ? props.children : id,
+      heading: typeof props.children === "string" ? props.children : id,
     },
   );
   return (
     <As
       {...props}
-      className={clsx('anchor', anchorTargetClassName, props.className)}
-      id={id}>
+      className={clsx("anchor", anchorTargetClassName, props.className)}
+      id={id}
+    >
       {props.children}
       <Link
         className="hash-link"
         to={`#${id}`}
         aria-label={anchorTitle}
         title={anchorTitle}
-        translate="no">
+        translate="no"
+      >
         <span className="sr-only">{anchorTitle}</span>
       </Link>
     </As>
