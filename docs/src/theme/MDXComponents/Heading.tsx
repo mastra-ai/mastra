c8658789bcaa1ae31b648bbc7ee7d91668d71c8b
Benedikt Rötsch <benedikt@hashbite.net>
2025-12-02 11:43:08 +0100

    fix: ensure anchor links have content to avoid SEO-Tools complaining about empty links

--- docs/src/theme/MDXComponents/Heading.tsx
+++ docs/src/theme/MDXComponents/Heading.tsx
@@ -1,7 +1,42 @@
-import React, { type ReactNode } from "react";
-import Heading from "@theme/Heading";
-import type { Props } from "@theme/MDXComponents/Heading";
+import React from 'react';
+import clsx from 'clsx';
+import {translate} from '@docusaurus/Translate';
+import {useAnchorTargetClassName} from '@docusaurus/theme-common';
+import Link from '@docusaurus/Link';
+import useBrokenLinks from '@docusaurus/useBrokenLinks';
 
-export default function MDXHeading(props: Props): ReactNode {
-  return <Heading {...props} />;
+export default function Heading({as: As, id, ...props}) {
+  const brokenLinks = useBrokenLinks();
+  const anchorTargetClassName = useAnchorTargetClassName(id);
+  // H1 headings do not need an id because they don't appear in the TOC.
+  if (As === 'h1' || !id) {
+    return <As {...props} id={undefined} />;
+  }
+  brokenLinks.collectAnchor(id);
+  const anchorTitle = translate(
+    {
+      id: 'theme.common.headingLinkTitle',
+      message: 'Direct link to {heading}',
+      description: 'Title for link to heading',
+    },
+    {
+      heading: typeof props.children === 'string' ? props.children : id,
+    },
+  );
+  return (
+    <As
+      {...props}
+      className={clsx('anchor', anchorTargetClassName, props.className)}
+      id={id}>
+      {props.children}
+      <Link
+        className="hash-link"
+        to={`#${id}`}
+        aria-label={anchorTitle}
+        title={anchorTitle}
+        translate="no">
+        <span className="sr-only">{anchorTitle}</span>
+      </Link>
+    </As>
+  );
 }
