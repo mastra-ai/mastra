DevZonayed/nexalance-skill-mastra-ai-mastra

time travel in studio (#10361)
## Description

<!-- Provide a brief description of the changes in this PR -->
- Add APIs for time travel and restart
- Implement timeTravel in studio



https://github.com/user-attachments/assets/274c1744-9529-4fde-8166-7672d21a29db



## Related Issue(s)

<!-- Link to the issue(s) this PR addresses, using hashtag notation:
#123 -->

## Type of Change

- [ ] Bug fix (non-breaking change that fixes an issue)
- [x] New feature (non-breaking change that adds functionality)
- [ ] Breaking change (fix or feature that would cause existing
functionality to change)
- [ ] Documentation update
- [ ] Code refactoring
- [ ] Performance improvement
- [ ] Test update

## Checklist

- [ ] I have made corresponding changes to the documentation (if
applicable)
- [ ] I have added tests that prove my fix is effective or that my
feature works


<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

## Release Notes

* **New Features**
* Time travel functionality now enables rewinding and replaying workflow
runs to specific steps for debugging and recovery.
* Restart capabilities added for individual workflow runs and all active
runs.
* Enhanced workflow visualization and control in the studio with
improved run management and time travel UI controls.

<sub>✏️ Tip: You can customize this high-level summary in your review
settings.</sub>
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

packages/playground-ui/src/domains/workflows/context/workflow-run-context.tsx
@@ -1,34 +1,107 @@
 import { WorkflowRunState, WorkflowStreamResult } from '@mastra/core/workflows';
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useMemo, useState, type Dispatch, type SetStateAction, type ReactNode } from 'react';
 import { convertWorkflowRunStateToStreamResult } from '../utils';
+import { useCancelWorkflowRun, useExecuteWorkflow, useStreamWorkflow } from '../hooks';
+import { WorkflowTriggerProps } from '../workflow/workflow-trigger';
+import { useWorkflow, useWorkflowRunExecutionResult } from '@/hooks';
+import { TimeTravelParams } from '@mastra/client-js';
 
 export type WorkflowRunStreamResult = WorkflowStreamResult<any, any, any, any>;
 
 type WorkflowRunContextType = {
   result: WorkflowRunStreamResult | null;
-  setResult: React.Dispatch<React.SetStateAction<WorkflowRunStreamResult | null>>;
+  setResult: Dispatch<SetStateAction<WorkflowRunStreamResult | null>>;
   payload: any;
-  setPayload: React.Dispatch<React.SetStateAction<any>>;
+  setPayload: Dispatch<SetStateAction<any>>;
   clearData: () => void;
   snapshot?: WorkflowRunState;
   runId?: string;
-  setRunId: React.Dispatch<React.SetStateAction<string>>;
-};
+  setRunId: Dispatch<SetStateAction<string>>;
+  workflowError: Error | null;
+  observeWorkflowStream?: ({
+    workflowId,
+    runId,
+    storeRunResult,
+  }: {
+    workflowId: string;
+    runId: string;
+    storeRunResult: WorkflowRunStreamResult | null;
+  }) => void;
+  closeStreamsAndReset: () => void;
+  timeTravelWorkflowStream: (
+    params: {
+      workflowId: string;
+      requestContext: Record<string, unknown>;
+    } & Omit<TimeTravelParams, 'requestContext'>,
+  ) => Promise<void>;
+  runSnapshot?: WorkflowRunState;
+  isLoadingRunExecutionResult?: boolean;
+  withoutTimeTravel?: boolean;
+} & Omit<WorkflowTriggerProps, 'paramsRunId' | 'setRunId' | 'observeWorkflowStream'>;
 
 export const WorkflowRunContext = createContext<WorkflowRunContextType>({} as WorkflowRunContextType);
 
 export function WorkflowRunProvider({
   children,
   snapshot,
+  workflowId,
+  initialRunId,
+  withoutTimeTravel = false,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
   snapshot?: WorkflowRunState;
+  workflowId: string;
+  initialRunId?: string;
+  withoutTimeTravel?: boolean;
 }) {
   const [result, setResult] = useState<WorkflowRunStreamResult | null>(() =>
     snapshot ? convertWorkflowRunStateToStreamResult(snapshot) : null,
   );
   const [payload, setPayload] = useState<any>(() => snapshot?.context?.input ?? null);
-  const [runId, setRunId] = useState<string>(() => snapshot?.runId ?? '');
+  const [runId, setRunId] = useState<string>(() => initialRunId ?? '');
+  const [isRunning, setIsRunning] = useState(false);
+
+  const refetchExecResultInterval = isRunning
+    ? undefined
+    : ['success', 'failed', 'canceled', 'bailed'].includes(result?.status ?? '')
+      ? undefined
+      : 5000;
+
+  const { isLoading: isLoadingRunExecutionResult, data: runExecutionResult } = useWorkflowRunExecutionResult(
+    workflowId,
+    initialRunId ?? '',
+    refetchExecResultInterval,
+  );
+
+  const runSnapshot = useMemo(() => {
+    return runExecutionResult && initialRunId
+      ? ({
+          context: {
+            input: runExecutionResult?.payload,
+            ...runExecutionResult?.steps,
+          } as any,
+          status: runExecutionResult?.status,
+          result: runExecutionResult?.result,
+          error: runExecutionResult?.error,
+          runId: initialRunId,
+          serializedStepGraph: runExecutionResult?.serializedStepGraph,
+        } as WorkflowRunState)
+      : undefined;
+  }, [runExecutionResult, initialRunId]);
+
+  const { data: workflow, isLoading, error } = useWorkflow(workflowId);
+
+  const { createWorkflowRun } = useExecuteWorkflow();
+  const {
+    streamWorkflow,
+    streamResult,
+    isStreaming,
+    observeWorkflowStream,
+    closeStreamsAndReset,
+    resumeWorkflowStream,
+    timeTravelWorkflowStream,
+  } = useStreamWorkflow();
+  const { mutateAsync: cancelWorkflowRun, isPending: isCancellingWorkflowRun } = useCancelWorkflowRun();
 
   const clearData = () => {
     setResult(null);
@@ -36,16 +109,21 @@ export function WorkflowRunProvider({
   };
 
   useEffect(() => {
-    if (snapshot?.runId) {
-      setResult(convertWorkflowRunStateToStreamResult(snapshot));
-      setPayload(snapshot.context?.input);
-      setRunId(snapshot.runId);
+    setIsRunning(false);
+  }, [initialRunId]);
+
+  useEffect(() => {
+    if (runSnapshot?.runId) {
+      setResult(convertWorkflowRunStateToStreamResult(runSnapshot));
+      setPayload(runSnapshot.context?.input);
+      setRunId(runSnapshot.runId);
     }
-  }, [snapshot]);
+  }, [runSnapshot]);
 
   return (
     <WorkflowRunContext.Provider
       value={{
+        workflowId,
         result,
         setResult,
         payload,
@@ -54,6 +132,34 @@ export function WorkflowRunProvider({
         snapshot,
         runId,
         setRunId,
+        workflowError: error ?? null,
+        workflow: workflow ?? undefined,
+        isLoading,
+        createWorkflowRun: createWorkflowRun.mutateAsync,
+        streamWorkflow: props => {
+          setIsRunning(true);
+          return streamWorkflow.mutateAsync(props);
+        },
+        resumeWorkflow: props => {
+          setIsRunning(true);
+          return resumeWorkflowStream.mutateAsync(props);
+        },
+        streamResult,
+        isStreamingWorkflow: isStreaming,
+        isCancellingWorkflowRun,
+        cancelWorkflowRun,
+        observeWorkflowStream: props => {
+          setIsRunning(true);
+          return observeWorkflowStream.mutate(props);
+        },
+        closeStreamsAndReset,
+        timeTravelWorkflowStream: props => {
+          setIsRunning(true);
+          return timeTravelWorkflowStream.mutateAsync(props);
+        },
+        runSnapshot,
+        isLoadingRunExecutionResult,
+        withoutTimeTravel,
       }}
     >
       {children}

packages/playground-ui/src/domains/workflows/hooks/use-workflows-actions.ts
@@ -1,4 +1,4 @@
-import { StreamVNextChunkType } from '@mastra/client-js';
+import { StreamVNextChunkType, TimeTravelParams } from '@mastra/client-js';
 import { RequestContext } from '@mastra/core/request-context';
 import { WorkflowStreamResult as CoreWorkflowStreamResult } from '@mastra/core/workflows';
 import { useMutation } from '@tanstack/react-query';
@@ -93,6 +93,7 @@ export const useStreamWorkflow = () => {
   const readerRef = useRef<ReadableStreamDefaultReader<StreamVNextChunkType> | null>(null);
   const observerRef = useRef<ReadableStreamDefaultReader<StreamVNextChunkType> | null>(null);
   const resumeStreamRef = useRef<ReadableStreamDefaultReader<StreamVNextChunkType> | null>(null);
+  const timeTravelStreamRef = useRef<ReadableStreamDefaultReader<StreamVNextChunkType> | null>(null);
   const isMountedRef = useRef(true);
 
   // Cleanup on unmount
@@ -123,6 +124,14 @@ export const useStreamWorkflow = () => {
           // Reader might already be released, ignore the error
         }
         resumeStreamRef.current = null;
+      }
+      if (timeTravelStreamRef.current) {
+        try {
+          timeTravelStreamRef.current.releaseLock();
+        } catch (error) {
+          // Reader might already be released, ignore the error
+        }
+        timeTravelStreamRef.current = null;
       }
     };
   }, []);
@@ -378,6 +387,79 @@ export const useStreamWorkflow = () => {
     },
   });
 
+  const timeTravelWorkflowStream = useMutation({
+    mutationFn: async ({
+      workflowId,
+      requestContext: playgroundRequestContext,
+      ...params
+    }: {
+      workflowId: string;
+      requestContext: Record<string, unknown>;
+    } & Omit<TimeTravelParams, 'requestContext'>) => {
+      // Clean up any existing reader before starting new stream
+      if (timeTravelStreamRef.current) {
+        timeTravelStreamRef.current.releaseLock();
+      }
+
+      if (!isMountedRef.current) return;
+
+      setIsStreaming(true);
+      const workflow = client.getWorkflow(workflowId);
+      const requestContext = new RequestContext();
+      Object.entries(playgroundRequestContext).forEach(([key, value]) => {
+        requestContext.set(key as keyof RequestContext, value);
+      });
+      const stream = await workflow.timeTravelStream({ ...params, requestContext });
+
+      if (!stream) {
+        return handleStreamError(new Error('No stream returned'), 'No stream returned', setIsStreaming);
+      }
+
+      // Get a reader from the ReadableStream and store it in ref
+      const reader = stream.getReader();
+      timeTravelStreamRef.current = reader;
+
+      try {
+        while (true) {
+          if (!isMountedRef.current) break;
+
+          const { done, value } = await reader.read();
+          if (done) break;
+
+          // Only update state if component is still mounted
+          if (isMountedRef.current) {
+            setStreamResult(prev => {
+              const newResult = mapWorkflowStreamChunkToWatchResult(prev, value);
+              return newResult;
+            });
+
+            if (value.type === 'workflow-step-start') {
+              setIsStreaming(true);
+            }
+
+            if (value.type === 'workflow-step-suspended') {
+              setIsStreaming(false);
+            }
+
+            if (value.type === 'workflow-finish') {
+              handleWorkflowFinish(value);
+            }
+          }
+        }
+      } catch (err) {
+        handleStreamError(err, 'Error time traveling workflow stream');
+      } finally {
+        if (isMountedRef.current) {
+          setIsStreaming(false);
+        }
+        if (timeTravelStreamRef.current) {
+          timeTravelStreamRef.current.releaseLock();
+          timeTravelStreamRef.current = null;
+        }
+      }
+    },
+  });
+
   const closeStreamsAndReset = () => {
     setIsStreaming(false);
     setStreamResult({} as WorkflowStreamResult);
@@ -405,6 +487,14 @@ export const useStreamWorkflow = () => {
       }
       resumeStreamRef.current = null;
     }
+    if (timeTravelStreamRef.current) {
+      try {
+        timeTravelStreamRef.current.releaseLock();
+      } catch (error) {
+        // Reader might already be released, ignore the error
+      }
+      timeTravelStreamRef.current = null;
+    }
   };
 
   return {
@@ -414,6 +504,7 @@ export const useStreamWorkflow = () => {
     observeWorkflowStream,
     closeStreamsAndReset,
     resumeWorkflowStream,
+    timeTravelWorkflowStream,
   };
 };
 

mastra-ai/mastra

packages/playground-ui/src/domains/workflows/context/workflow-run-context.tsx
@@ -1,34 +1,107 @@
 import { WorkflowRunState, WorkflowStreamResult } from '@mastra/core/workflows';
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useMemo, useState, type Dispatch, type SetStateAction, type ReactNode } from 'react';
 import { convertWorkflowRunStateToStreamResult } from '../utils';
+import { useCancelWorkflowRun, useExecuteWorkflow, useStreamWorkflow } from '../hooks';
+import { WorkflowTriggerProps } from '../workflow/workflow-trigger';
+import { useWorkflow, useWorkflowRunExecutionResult } from '@/hooks';
+import { TimeTravelParams } from '@mastra/client-js';
 
 export type WorkflowRunStreamResult = WorkflowStreamResult<any, any, any, any>;
 
 type WorkflowRunContextType = {
   result: WorkflowRunStreamResult | null;
-  setResult: React.Dispatch<React.SetStateAction<WorkflowRunStreamResult | null>>;
+  setResult: Dispatch<SetStateAction<WorkflowRunStreamResult | null>>;
   payload: any;
-  setPayload: React.Dispatch<React.SetStateAction<any>>;
+  setPayload: Dispatch<SetStateAction<any>>;
   clearData: () => void;
   snapshot?: WorkflowRunState;
   runId?: string;
-  setRunId: React.Dispatch<React.SetStateAction<string>>;
-};
+  setRunId: Dispatch<SetStateAction<string>>;
+  workflowError: Error | null;
+  observeWorkflowStream?: ({
+    workflowId,
+    runId,
+    storeRunResult,
+  }: {
+    workflowId: string;
+    runId: string;
+    storeRunResult: WorkflowRunStreamResult | null;
+  }) => void;
+  closeStreamsAndReset: () => void;
+  timeTravelWorkflowStream: (
+    params: {
+      workflowId: string;
+      runtimeContext: Record<string, unknown>;
+    } & Omit<TimeTravelParams, 'runtimeContext'>,
+  ) => Promise<void>;
+  runSnapshot?: WorkflowRunState;
+  isLoadingRunExecutionResult?: boolean;
+  withoutTimeTravel?: boolean;
+} & Omit<WorkflowTriggerProps, 'paramsRunId' | 'setRunId' | 'observeWorkflowStream'>;
 
 export const WorkflowRunContext = createContext<WorkflowRunContextType>({} as WorkflowRunContextType);
 
 export function WorkflowRunProvider({
   children,
   snapshot,
+  workflowId,
+  initialRunId,
+  withoutTimeTravel = false,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
   snapshot?: WorkflowRunState;
+  workflowId: string;
+  initialRunId?: string;
+  withoutTimeTravel?: boolean;
 }) {
   const [result, setResult] = useState<WorkflowRunStreamResult | null>(() =>
     snapshot ? convertWorkflowRunStateToStreamResult(snapshot) : null,
   );
   const [payload, setPayload] = useState<any>(() => snapshot?.context?.input ?? null);
-  const [runId, setRunId] = useState<string>(() => snapshot?.runId ?? '');
+  const [runId, setRunId] = useState<string>(() => initialRunId ?? '');
+  const [isRunning, setIsRunning] = useState(false);
+
+  const refetchExecResultInterval = isRunning
+    ? undefined
+    : ['success', 'failed', 'canceled', 'bailed'].includes(result?.status ?? '')
+      ? undefined
+      : 5000;
+
+  const { isLoading: isLoadingRunExecutionResult, data: runExecutionResult } = useWorkflowRunExecutionResult(
+    workflowId,
+    initialRunId ?? '',
+    refetchExecResultInterval,
+  );
+
+  const runSnapshot = useMemo(() => {
+    return runExecutionResult && initialRunId
+      ? ({
+          context: {
+            input: runExecutionResult?.payload,
+            ...runExecutionResult?.steps,
+          } as any,
+          status: runExecutionResult?.status,
+          result: runExecutionResult?.result,
+          error: runExecutionResult?.error,
+          runId: initialRunId,
+          serializedStepGraph: runExecutionResult?.serializedStepGraph,
+        } as WorkflowRunState)
+      : undefined;
+  }, [runExecutionResult, initialRunId]);
+
+  const { data: workflow, isLoading, error } = useWorkflow(workflowId);
+
+  const { createWorkflowRun } = useExecuteWorkflow();
+  const {
+    streamWorkflow,
+    streamResult,
+    isStreaming,
+    observeWorkflowStream,
+    closeStreamsAndReset,
+    resumeWorkflowStream,
+    timeTravelWorkflowStream,
+  } = useStreamWorkflow();
+  const { mutateAsync: cancelWorkflowRun, isPending: isCancellingWorkflowRun } = useCancelWorkflowRun();
 
   const clearData = () => {
     setResult(null);
@@ -36,16 +109,21 @@ export function WorkflowRunProvider({
   };
 
   useEffect(() => {
-    if (snapshot?.runId) {
-      setResult(convertWorkflowRunStateToStreamResult(snapshot));
-      setPayload(snapshot.context?.input);
-      setRunId(snapshot.runId);
+    setIsRunning(false);
+  }, [initialRunId]);
+
+  useEffect(() => {
+    if (runSnapshot?.runId) {
+      setResult(convertWorkflowRunStateToStreamResult(runSnapshot));
+      setPayload(runSnapshot.context?.input);
+      setRunId(runSnapshot.runId);
     }
-  }, [snapshot]);
+  }, [runSnapshot]);
 
   return (
     <WorkflowRunContext.Provider
       value={{
+        workflowId,
         result,
         setResult,
         payload,
@@ -54,6 +132,34 @@ export function WorkflowRunProvider({
         snapshot,
         runId,
         setRunId,
+        workflowError: error ?? null,
+        workflow: workflow ?? undefined,
+        isLoading,
+        createWorkflowRun: createWorkflowRun.mutateAsync,
+        streamWorkflow: props => {
+          setIsRunning(true);
+          return streamWorkflow.mutateAsync(props);
+        },
+        resumeWorkflow: props => {
+          setIsRunning(true);
+          return resumeWorkflowStream.mutateAsync(props);
+        },
+        streamResult,
+        isStreamingWorkflow: isStreaming,
+        isCancellingWorkflowRun,
+        cancelWorkflowRun,
+        observeWorkflowStream: props => {
+          setIsRunning(true);
+          return observeWorkflowStream.mutate(props);
+        },
+        closeStreamsAndReset,
+        timeTravelWorkflowStream: props => {
+          setIsRunning(true);
+          return timeTravelWorkflowStream.mutateAsync(props);
+        },
+        runSnapshot,
+        isLoadingRunExecutionResult,
+        withoutTimeTravel,
       }}
     >
       {children}

packages/playground-ui/src/domains/workflows/hooks/use-workflows-actions.ts
@@ -1,4 +1,4 @@
-import { StreamVNextChunkType, WorkflowWatchResult } from '@mastra/client-js';
+import { StreamVNextChunkType, TimeTravelParams, WorkflowWatchResult } from '@mastra/client-js';
 import { RuntimeContext } from '@mastra/core/runtime-context';
 import { WorkflowStreamResult as CoreWorkflowStreamResult } from '@mastra/core/workflows';
 import { useMutation } from '@tanstack/react-query';
@@ -141,6 +141,7 @@ export const useStreamWorkflow = () => {
   const readerRef = useRef<ReadableStreamDefaultReader<StreamVNextChunkType> | null>(null);
   const observerRef = useRef<ReadableStreamDefaultReader<StreamVNextChunkType> | null>(null);
   const resumeStreamRef = useRef<ReadableStreamDefaultReader<StreamVNextChunkType> | null>(null);
+  const timeTravelStreamRef = useRef<ReadableStreamDefaultReader<StreamVNextChunkType> | null>(null);
   const isMountedRef = useRef(true);
 
   // Cleanup on unmount
@@ -172,6 +173,14 @@ export const useStreamWorkflow = () => {
         }
         resumeStreamRef.current = null;
       }
+      if (timeTravelStreamRef.current) {
+        try {
+          timeTravelStreamRef.current.releaseLock();
+        } catch (error) {
+          // Reader might already be released, ignore the error
+        }
+        timeTravelStreamRef.current = null;
+      }
     };
   }, []);
 
@@ -180,6 +189,7 @@ export const useStreamWorkflow = () => {
     if (err instanceof TypeError) {
       return;
     }
+    console.log('err in handleStreamError===', err);
     const errorMessage = err instanceof Error ? err.message : defaultMessage;
     toast.error(errorMessage);
     setIsStreaming?.(false);
@@ -426,6 +436,79 @@ export const useStreamWorkflow = () => {
     },
   });
 
+  const timeTravelWorkflowStream = useMutation({
+    mutationFn: async ({
+      workflowId,
+      runtimeContext: playgroundRuntimeContext,
+      ...params
+    }: {
+      workflowId: string;
+      runtimeContext: Record<string, unknown>;
+    } & Omit<TimeTravelParams, 'runtimeContext'>) => {
+      // Clean up any existing reader before starting new stream
+      if (timeTravelStreamRef.current) {
+        timeTravelStreamRef.current.releaseLock();
+      }
+
+      if (!isMountedRef.current) return;
+
+      setIsStreaming(true);
+      const workflow = client.getWorkflow(workflowId);
+      const runtimeContext = new RuntimeContext();
+      Object.entries(playgroundRuntimeContext).forEach(([key, value]) => {
+        runtimeContext.set(key as keyof RuntimeContext, value);
+      });
+      const stream = await workflow.timeTravelStream({ ...params, runtimeContext });
+
+      if (!stream) {
+        return handleStreamError(new Error('No stream returned'), 'No stream returned', setIsStreaming);
+      }
+
+      // Get a reader from the ReadableStream and store it in ref
+      const reader = stream.getReader();
+      timeTravelStreamRef.current = reader;
+
+      try {
+        while (true) {
+          if (!isMountedRef.current) break;
+
+          const { done, value } = await reader.read();
+          if (done) break;
+
+          // Only update state if component is still mounted
+          if (isMountedRef.current) {
+            setStreamResult(prev => {
+              const newResult = mapWorkflowStreamChunkToWatchResult(prev, value);
+              return newResult;
+            });
+
+            if (value.type === 'workflow-step-start') {
+              setIsStreaming(true);
+            }
+
+            if (value.type === 'workflow-step-suspended') {
+              setIsStreaming(false);
+            }
+
+            if (value.type === 'workflow-finish') {
+              handleWorkflowFinish(value);
+            }
+          }
+        }
+      } catch (err) {
+        handleStreamError(err, 'Error time traveling workflow stream');
+      } finally {
+        if (isMountedRef.current) {
+          setIsStreaming(false);
+        }
+        if (timeTravelStreamRef.current) {
+          timeTravelStreamRef.current.releaseLock();
+          timeTravelStreamRef.current = null;
+        }
+      }
+    },
+  });
+
   const closeStreamsAndReset = () => {
     setIsStreaming(false);
     setStreamResult({} as WorkflowStreamResult);
@@ -453,6 +536,14 @@ export const useStreamWorkflow = () => {
       }
       resumeStreamRef.current = null;
     }
+    if (timeTravelStreamRef.current) {
+      try {
+        timeTravelStreamRef.current.releaseLock();
+      } catch (error) {
+        // Reader might already be released, ignore the error
+      }
+      timeTravelStreamRef.current = null;
+    }
   };
 
   return {
@@ -462,6 +553,7 @@ export const useStreamWorkflow = () => {
     observeWorkflowStream,
     closeStreamsAndReset,
     resumeWorkflowStream,
+    timeTravelWorkflowStream,
   };
 };
 