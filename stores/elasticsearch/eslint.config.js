87c5e4d9ec3c1298d15a69cf0604fe61b0afb85d
Abhi Aiyer <abhiaiyer91@gmail.com>
2025-12-02 00:04:13 -0600

    fix: use correct eslint config import

--- stores/elasticsearch/eslint.config.js
+++ stores/elasticsearch/eslint.config.js
@@ -1,3 +1,6 @@
-import config from '@internal/lint';
+import { createConfig } from '@internal/lint/eslint';
 
-export default config;
+const config = await createConfig();
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [...config];
