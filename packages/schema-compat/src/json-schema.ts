fe8ec39136148b460f3e1c3948d80d06c492ab8e
wardpeet <ward@coding-tech.com>
2025-12-10 15:40:35 +0100

    update types-builder to add type embedder fn

--- packages/schema-compat/src/json-schema.ts
+++ packages/schema-compat/src/json-schema.ts
@@ -1,4 +1,4 @@
-export { jsonSchema } from '@internal/ai-sdk-v4/schema';
-export type { Schema } from '@internal/ai-sdk-v4/schema';
+export { jsonSchema } from '@internal/ai-sdk-v4';
+export type { Schema } from '@internal/ai-sdk-v4';
 
 export type { JSONSchema7 } from 'json-schema';
