985dc974233d0050f1aa814a307c02979884a9af
wardpeet <ward@coding-tech.com>
2025-12-10 14:18:38 +0100

    rework internal ai-sdk packages

--- packages/_ai-sdk-v4/src/test.ts
+++ packages/_ai-sdk-v4/src/test.ts
@@ -1,2 +1,1 @@
-export { MockLanguageModelV1 } from 'ai/test';
-export { simulateReadableStream } from 'ai';
+export {};
--- packages/_ai-sdk-v4/src/util.ts
+++
@@ -1,2 +0,0 @@
-export type { DeepPartial } from 'ai';
-export { convertUint8ArrayToBase64 } from '@ai-sdk/provider-utils';
