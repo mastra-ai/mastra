80a3b87c8f9f8d645bf96d9552eb5ad05342241b
intojhanurag <aojharaj2004@gmail.com>
2025-12-23 12:34:09 +0000

    remove typo

--- packages/cli/src/commands/init/mcp-docs-server-install.ts
+++ packages/cli/src/commands/init/mcp-docs-server-install.ts
@@ -16,15 +16,15 @@ const createMcpConfig = (editor: Editor, versionTag?: string) => {
         mastra:
           process.platform === `win32`
             ? {
-                command: 'cmd',
-                args: ['/c', 'npx', ...args],
-                type: 'stdio',
-              }
+              command: 'cmd',
+              args: ['/c', 'npx', ...args],
+              type: 'stdio',
+            }
             : {
-                command: 'npx',
-                args,
-                type: 'stdio',
-              },
+              command: 'npx',
+              args,
+              type: 'stdio',
+            },
       },
     };
   }
