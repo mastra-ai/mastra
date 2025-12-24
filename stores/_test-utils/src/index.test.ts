b12e98707acf1805a0d4cbb005b7938c1220614b
NikAiyer <nick.aiyer@gmail.com>
2025-12-23 18:19:33 -0600

    add tests

--- stores/_test-utils/src/index.test.ts
+++ stores/_test-utils/src/index.test.ts
@@ -1,4 +1,9 @@
 import { MockStore } from '@mastra/core/storage';
 import { createTestSuite } from './factory';
+import { createCompositeStorageTests } from './composite-tests';
 
+// Test InMemoryStore (MockStore)
 createTestSuite(new MockStore());
+
+// Test CompositeStorage with InMemoryStore backing
+createCompositeStorageTests();
