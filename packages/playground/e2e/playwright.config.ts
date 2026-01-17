76d1144fb8397115b25560f47f62774954ec5ac4
mfrachet <marvin@mastra.ai>
2026-01-17 21:26:26 +0100

    easier config

--- packages/playground/e2e/playwright.config.ts
+++ packages/playground/e2e/playwright.config.ts
@@ -23,8 +23,8 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: 0,
+  workers: 1,
   reporter: 'html',
 
   use: {
