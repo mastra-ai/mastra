103e87a07bd8c14454a425e41cf9a6bcae2dbd47
YujohnNattrass <yujohn.natt@gmail.com>
2026-01-20 10:59:13 -0800

    Remove telemetry disabled placeholder from studio
    Remove replacement for %%MASTRA_TELEMETRY_DISABLED%% in the server configuration.

--- packages/cli/src/commands/studio/studio.ts
+++ packages/cli/src/commands/studio/studio.ts
@@ -72,7 +72,6 @@ const createServer = (builtStudioPath: string, options: StudioOptions) => {
     .replaceAll('%%MASTRA_SERVER_PORT%%', String(options.serverPort || 4111))
     .replaceAll('%%MASTRA_SERVER_PROTOCOL%%', options.serverProtocol || 'http')
     .replaceAll('%%MASTRA_CLOUD_API_ENDPOINT%%', '')
-    .replaceAll('%%MASTRA_TELEMETRY_DISABLED%%', '')
     .replaceAll('%%MASTRA_HIDE_CLOUD_CTA%%', '');
 
   const server = http.createServer((req, res) => {
