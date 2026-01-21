9c4c1fffc9c7204e27d988e17eeba50ed56c2178
wardpeet <ward@coding-tech.com>
2026-01-21 09:08:46 +0100

    test: add comment to RAG source to trigger tests

--- packages/rag/src/index.ts
+++ packages/rag/src/index.ts
@@ -5,3 +5,4 @@ export * from './rerank/relevance';
 export { GraphRAG } from './graph-rag';
 export * from './tools';
 export * from './utils/default-settings';
+// Test change to trigger RAG tests - Wed Jan 21 09:08:42 CET 2026
