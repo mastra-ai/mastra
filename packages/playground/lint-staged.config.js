c3bedac1708596e1a776f426104e4a3683798da0
Greg Lobinski <greglobinski@gmail.com>
2026-01-26 16:14:00 +0100

    test 2

--- packages/playground/lint-staged.config.js
+++ packages/playground/lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
-  '*.{ts,tsx}': ['eslint --fix --max-warnings=0', 'prettier --write'],
-  '*.{js,jsx}': ['eslint --fix', 'prettier --write'],
+  '*.{ts,tsx}': ['eslint --fix --max-warnings=0 --rule "react-hooks/exhaustive-deps: off"', 'prettier --write'],
+  '*.{js,jsx}': ['eslint --fix --rule "react-hooks/exhaustive-deps: off"', 'prettier --write'],
   '*.{json,md,yml,yaml}': ['prettier --write'],
 };
