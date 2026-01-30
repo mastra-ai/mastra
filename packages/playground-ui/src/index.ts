62120f127b2f06b23c586ef8a98bbd3d66bff743
Yujohn Nattrass <yujohn.natt@gmail.com>
2026-01-30 11:56:14 -0800

    feat(05-02): export PermissionDenied from package index
    - Public API for playground-ui consumers

--- packages/playground-ui/src/index.ts
+++ packages/playground-ui/src/index.ts
@@ -20,6 +20,7 @@ export * from './ds/components/Button/index';
 export * from './ds/components/CodeEditor/index';
 export * from './ds/components/EmptyState/index';
 export * from './ds/components/Entity/index';
+export * from './ds/components/PermissionDenied';
 export * from './ds/components/Header/index';
 export * from './ds/components/Logo/index';
 export * from './ds/components/Table/index';
