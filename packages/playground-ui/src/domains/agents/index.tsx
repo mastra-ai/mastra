9a4ff5dad8bc47548a68049b29600b5eb8b6c974
mfrachet <marvin@mastra.ai>
2026-02-03 09:01:04 +0100

    wip

--- packages/playground-ui/src/domains/agents/index.tsx
+++ packages/playground-ui/src/domains/agents/index.tsx
@@ -20,6 +20,5 @@ export * from './components/agent-entity-header';
 export * from './components/agent-information/agent-memory';
 export * from './components/agent-layout';
 export * from './components/agent-cms-blocks';
-export * from './components/agent-edit/agent-edit-form';
 export * from './components/agent-edit-page/utils/form-validation';
 export * from './components/agent-edit-page';
