e5487f3089d8c30719ae56b9591648210ccff0a8
wardpeet <ward@coding-tech.com>
2026-02-03 09:29:09 +0100

    self review

--- packages/schema-compat/src/schema.ts
+++ packages/schema-compat/src/schema.ts
@@ -1,8 +1,15 @@
 export type {
   StandardSchemaWithJSON,
   InferOutput as InferStandardSchemaOutput,
+  InferInput as InferStandardSchemaInput,
+  StandardSchemaIssue,
+  StandardSchemaWithJSONProps,
 } from './standard-schema/standard-schema.types';
 
 export type { PublicSchema, InferPublicSchema } from './schema.types';
 
-export { toStandardSchema, isStandardSchemaWithJSON } from './standard-schema/standard-schema';
+export {
+  toStandardSchema,
+  isStandardSchemaWithJSON,
+  standardSchemaToJSONSchema,
+} from './standard-schema/standard-schema';
--- packages/schema-compat/src/standard-schema/index.ts
+++
@@ -1,9 +0,0 @@
-export { standardSchemaToJSONSchema, toStandardSchema, isStandardSchemaWithJSON } from './standard-schema';
-
-export type {
-  StandardSchemaWithJSON,
-  StandardSchemaWithJSONProps,
-  InferInput,
-  InferOutput,
-  StandardSchemaIssue,
-} from './standard-schema.types';
