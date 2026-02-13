7ed79fe7b1871d6e19450f48d5c1e0b8a424b0a9
David <david@useomnia.com>
2026-02-13 19:00:07 +0100

    chore: Remove unused method

--- packages/core/src/tools/index.ts
+++ packages/core/src/tools/index.ts
@@ -1,6 +1,6 @@
 export * from './tool';
 export * from './types';
 export * from './ui-types';
-export { isVercelTool, isProviderDefinedTool, isGatewayTool } from './toolchecks';
+export { isVercelTool, isProviderDefinedTool } from './toolchecks';
 export { ToolStream } from './stream';
 export { type ValidationError } from './validation';
