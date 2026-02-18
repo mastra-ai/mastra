5a5ae9f3cdd161a8d086dfa9cb60fbe7d9a35a38
Greg Lobinski <greglobinski@gmail.com>
2026-02-18 13:41:53 +0100

    fix Select Experimental story

--- packages/playground-ui/src/ds/components/Select/select.stories.tsx
+++ packages/playground-ui/src/ds/components/Select/select.stories.tsx
@@ -30,28 +30,16 @@ export const Default: Story = {
 
 export const Experimental: Story = {
   render: () => (
-    <div className="grid gap-3">
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Select option" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="option1">Option 1</SelectItem>
-          <SelectItem value="option2">Option 2</SelectItem>
-          <SelectItem value="option3">Option 3</SelectItem>
-        </SelectContent>
-      </Select>
-      <Select>
-        <SelectTrigger className="w-[180px]" variant="new" size="default">
-          <SelectValue placeholder="Select option" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="option1">Option 1</SelectItem>
-          <SelectItem value="option2">Option 2</SelectItem>
-          <SelectItem value="option3">Option 3</SelectItem>
-        </SelectContent>
-      </Select>
-    </div>
+    <Select>
+      <SelectTrigger className="w-[180px]" variant="experimental" size="default">
+        <SelectValue placeholder="Select option" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="option1">Option 1</SelectItem>
+        <SelectItem value="option2">Option 2</SelectItem>
+        <SelectItem value="option3">Option 3</SelectItem>
+      </SelectContent>
+    </Select>
   ),
 };
 
