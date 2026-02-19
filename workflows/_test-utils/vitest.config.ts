67eb035b894154b059d61eaa31ba8c5f134eaf0b
Tony Kovanen <rase-@users.noreply.github.com>
2026-02-19 16:29:57 +0200

    move test setup to shared entrypoint

--- workflows/_test-utils/vitest.config.ts
+++ workflows/_test-utils/vitest.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
+    include: [], // This package is a library - tests run from engine packages
   },
 });
