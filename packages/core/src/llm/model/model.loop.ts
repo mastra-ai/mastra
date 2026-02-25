266d7866acd1a0d73901e945113eb472283109f3
Taofeeq Oluder <oluderutaofeeq@gmail.com>
2026-02-25 15:36:07 +0100

    fix stopWhen tests failing

--- packages/core/src/llm/model/model.loop.ts
+++ packages/core/src/llm/model/model.loop.ts
@@ -142,7 +142,7 @@ export class MastraLLMVNext extends MastraBase {
     resumeContext,
     runId,
     stopWhen = stepCountIs(5),
-    maxSteps = 5, //default to 5 steps
+    maxSteps,
     tools = {} as Tools,
     modelSettings,
     toolChoice = 'auto',
