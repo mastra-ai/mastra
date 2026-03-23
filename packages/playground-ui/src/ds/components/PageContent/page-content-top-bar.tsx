c7e5f69c553efb5fb02a008fbdcb337f11a1c725
Greg Lobinski <greglobinski@gmail.com>
2026-03-23 13:32:07 +0100

    remove unused component

--- packages/playground-ui/src/ds/components/PageContent/page-content-top-bar.tsx
+++
@@ -1,6 +0,0 @@
-import type React from 'react';
-import { cn } from '@/lib/utils';
-
-export function PageContentTopBar({ children, className }: { children: React.ReactNode; className?: string }) {
-  return <aside className={cn('flex items-center py-3 min-h-[2rem] justify-end', className)}>{children}</aside>;
-}
--- packages/playground-ui/src/ds/components/PageContent/page-content.tsx
+++
@@ -1,8 +0,0 @@
-import { PageContentMain } from './page-content-main';
-import { PageContentRoot } from './page-content-root';
-import { PageContentTopBar } from './page-content-top-bar';
-
-export const PageContent = Object.assign(PageContentRoot, {
-  TopBar: PageContentTopBar,
-  Main: PageContentMain,
-});
