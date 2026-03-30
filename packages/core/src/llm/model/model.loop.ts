67c1fac7bbb4ba96a7b524fa9cb9b815c34df283
Anurag Ojha <aojharaj2004@gmail.com>
2026-03-30 18:48:05 +0530

    refactor(core): use serializeForSpan() for MODEL_GENERATION span input
    Signed-off-by: Anurag Ojha <aojharaj2004@gmail.com>

--- packages/core/src/llm/model/model.loop.ts
+++ packages/core/src/llm/model/model.loop.ts
@@ -157,7 +157,7 @@ export class MastraLLMVNext extends MastraBase {
       name: `llm: '${firstModel.modelId}'`,
       type: SpanType.MODEL_GENERATION,
       input: {
-        messages: [...messageList.getAllSystemMessages(), ...messages],
+        messages: [...messageList.serializeForSpan().systemMessages, ...messages],
       },
       attributes: {
         model: firstModel.modelId,
