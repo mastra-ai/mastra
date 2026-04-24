2df0a179e88b9fc00b114473a64b8e3f20f38945
Damien Schneider <damien.schneider01@gmail.com>
2026-04-24 15:08:54 +0200

    chore(playground-ui): upgrade Storybook to v10

--- packages/playground-ui/eslint.config.js
+++ packages/playground-ui/eslint.config.js
@@ -1,23 +1,20 @@
 import { createConfig } from '@internal/lint/eslint';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import storybook from 'eslint-plugin-storybook';
 
 const reactHooks = (await import('eslint-plugin-react-hooks')).default;
 
 const config = await createConfig();
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [
-  { ignores: ['**/*.stories.tsx'] },
-  ...config,
-  {
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    },
+export default [{ ignores: ['**/*.stories.tsx'] }, ...config, {
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
   },
-];
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+}, ...storybook.configs["flat/recommended"]];
