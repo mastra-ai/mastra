b9c15ba60f5d76f3425e9a397a4c4be9d6863601
Mohamed BEN HAMDOUNE <mbenhamd@gmail.com>
2026-05-04 16:40:46 +0200

    fix(server): reserve Studio serialization context

--- server-adapters/express/src/index.ts
+++ server-adapters/express/src/index.ts
@@ -112,6 +112,10 @@ export class MastraServer extends MastraServerBase<Application, Request, Respons
       }
 
       const requestContext = this.mergeRequestContext({ paramsRequestContext, bodyRequestContext });
+      this.applyRequestMetadataToContext({
+        requestContext,
+        getHeader: name => req.get(name),
+      });
 
       // Set context in res.locals
       res.locals.requestContext = requestContext;
