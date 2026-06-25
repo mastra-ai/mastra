b3abf25c93dc41c49c885cc90382187ad4b304e4
LekoArts <lekoarts@gmail.com>
2026-06-25 10:26:03 +0200

    remove non-essentials

--- docs/src/theme/Admonition/Types.tsx
+++
@@ -1,9 +0,0 @@
-import DefaultAdmonitionTypes from '@theme-original/Admonition/Types'
-import AdmonitionTypeExperimental from '@theme/Admonition/Type/Experimental'
-
-const admonitionTypes = {
-  ...DefaultAdmonitionTypes,
-  experimental: AdmonitionTypeExperimental,
-}
-
-export default admonitionTypes
