7ba1d09d242788d7e1ead79f74d9cb40f48e67d9
Sergey Nastich <sergey.nastich@iterable.com>
2026-07-15 11:09:17 -0400

    draft emitter changes - cancel to cleanup only this stream's listeners

--- packages/core/src/stream/base/output.ts
+++ packages/core/src/stream/base/output.ts
@@ -1759,6 +1759,8 @@ export class MastraModelOutput<OUTPUT = undefined> extends MastraBase {
 
   #createEventedStream() {
     const self = this;
+    let chunkHandler: ((chunk: ChunkType<OUTPUT>) => void) | undefined;
+    let finishHandler: (() => void) | undefined;
 
     return new ReadableStream<ChunkType<OUTPUT>>({
       start(controller) {
@@ -1774,13 +1776,13 @@ export class MastraModelOutput<OUTPUT = undefined> extends MastraBase {
         }
 
         // Listen for new chunks and stream finish
-        const chunkHandler = (chunk: ChunkType<OUTPUT>) => {
+        chunkHandler = (chunk: ChunkType<OUTPUT>) => {
           safeEnqueue(controller, chunk);
         };
 
-        const finishHandler = () => {
-          self.#emitter.off('chunk', chunkHandler);
-          self.#emitter.off('finish', finishHandler);
+        finishHandler = () => {
+          self.#emitter.off('chunk', chunkHandler!);
+          self.#emitter.off('finish', finishHandler!);
           safeClose(controller);
         };
 
@@ -1796,8 +1798,12 @@ export class MastraModelOutput<OUTPUT = undefined> extends MastraBase {
       },
 
       cancel() {
-        // Stream was cancelled, clean up
-        self.#emitter.removeAllListeners();
+        if (chunkHandler) {
+          self.#emitter.off('chunk', chunkHandler);
+        }
+        if (finishHandler) {
+          self.#emitter.off('finish', finishHandler);
+        }
       },
     });
   }
