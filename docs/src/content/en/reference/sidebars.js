4909c35b905195a437cf685a4a77e2e8ea506fe4
LekoArts <lekoarts@gmail.com>
2026-08-28 09:44:19 +0200

    updates

--- docs/src/content/en/reference/sidebars.js
+++ docs/src/content/en/reference/sidebars.js
@@ -254,7 +254,6 @@ const sidebars = {
       items: [
         { type: 'doc', id: 'deployer/cloudflare', label: 'Cloudflare' },
         { type: 'doc', id: 'deployer/index', label: 'Deployer' },
-        { type: 'doc', id: 'deployer/neon', label: 'Neon' },
         { type: 'doc', id: 'deployer/netlify', label: 'Netlify' },
         { type: 'doc', id: 'deployer/vercel', label: 'Vercel' },
       ],
