a912e8c9677bcf1de2fa8e0e0ac786462de55d46
Damien Schneider <damien.schneider01@gmail.com>
2026-09-14 12:55:07 +0200

    refactor(core): separate the Datadog test fix

--- observability/datadog/vitest.config.ts
+++ observability/datadog/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     name: 'unit:observability/datadog',
+    isolate: false,
     environment: 'node',
     include: ['src/**/*.test.ts'],
   },
